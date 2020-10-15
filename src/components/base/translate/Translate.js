016df2e023f217c3fe42d8ee79ae42276f46b3ae
mtni <mtni@dbc.dk>
2020-10-15 08:50:12 +0200

    ...

--- src/components/base/translate/Translate.js
+++ src/components/base/translate/Translate.js
@@ -8,6 +8,50 @@ export const units = translation.units;
 export const contexts = translation.contexts;
 
 /**
+ * 
+ * Documentaion:
+ * 
+ * Props of the translate component
+ * 
+ * @param {string} props.context
+ *  Context prop could be the name of the main component (Module), where the translate function is called. 
+ *  
+ * Examples: 
+ *  "WorkSlider"
+ *  "Footer"
+ *  "Header"
+ * 
+ * @param {string} props.label
+ *  Label should be the name of the translated text. Label should be uniqu name inside of the context.
+ * 
+ * Examples: 
+ *  "submitButton"
+ *  "submitHelpText"
+ *  "contactName"
+ *  "contactEmail"
+ * 
+ * @param {array} props.vars
+ * Vars is a list of variabels, used to make the translated text more dynamic. 
+ * Variabels in the translated text (%s) will get replaced (in the given order).
+ * Also functions/Translate functions can be passed as a variable
+ * 
+ * Examples:
+ *  [5] => "In cart: %s" = "In cart: 5"
+ *  [3, books] => "Order %s %s" = "Order 3 books"
+ * 
+ * @param {bool} props.renderAsHtml
+ * The translated json text can be rendered as html. 
+ * Html tags can be set in the json translations, 
+ * but also passed as variabels. 
+ * 
+ * Exampels: 
+ *  [1] => "Order <strong>%s<strong> book" = "Order <strong>1<strong> book"
+ *  [<span>3</span>] => Order %s books = "Order <span>3<span> books"
+ * 
+ * 
+ * 
+ * Use of the translate component
+ * 
  * Examples:
  *
  * The componentWay:
@@ -20,7 +64,7 @@ export const contexts = translation.contexts;
  * <Translate
    context=''
    label=''
-   vars={[x, y, T({context: '', label: ''})]}
+   vars={[x, y, Translate({context: '', label: ''})]}
  />
 
  /**
