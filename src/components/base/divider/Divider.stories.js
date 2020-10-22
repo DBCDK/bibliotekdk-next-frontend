8194f5b2dda170f0b66057245f6621d7ddc3ee06
pjo <pjo@dbc.dk>
2020-10-22 08:29:09 +0200

    added stories.js

---
+++ src/components/base/divider/Divider.stories.js
@@ -0,0 +1,18 @@
+import { Divider } from "./Divider";
+import { StoryTitle } from "../storybook";
+
+export default {
+  title: "base: Divider - a <hr>",
+};
+
+/**
+ * Returns bibliographic data component
+ */
+export function BibData() {
+  return (
+    <React.Fragment>
+      <StoryTitle>Divider</StoryTitle>
+      <Divider />
+    </React.Fragment>
+  );
+}
