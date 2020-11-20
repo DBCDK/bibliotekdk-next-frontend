ba22422fdcdc318ba470b6e41c5c206530ff82a9
pjo <pjo@dbc.dk>
2020-11-20 10:36:29 +0100

    removed unused code

--- src/components/work/hero/Hero.js
+++ src/components/work/hero/Hero.js
@@ -29,10 +29,6 @@ const HeroSection = () => {
   );
 };
 
-const HeroImg = () => {
-  return <img src="/img/bibdk-hero-scaled.jpeg" className={styles.heroimg} />;
-};
-
 export default function Hero() {
   return <HeroSection />;
 }
