a23b194c7509324cc19acb77f8c171314b65180d
Kasper F <kfu@dbc.dk>
2020-11-25 13:24:26 +0100

    set default api url

--- src/config.js
+++ src/config.js
@@ -2,14 +2,13 @@
  * @file
  * Application configuration.
  */
-
 const config = {
   port: process.env.PORT || 3000,
   externalBaseUrl: process.env.EXTERNAL_BASE_URL || "https://bibliotek.dk",
   api: {
     url:
       process.env.NEXT_PUBLIC_API_URL ||
-      "http://bibliotekdk-next-api-1.frontend-staging.svc.cloud.dbc.dk/graphql",
+      "https://beta-api.bibliotek.dk/graphql",
     timeout: process.env.API_TIMEOUT_MS || 150,
   },
 };
