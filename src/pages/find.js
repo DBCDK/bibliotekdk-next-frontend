16c338a79c073592dcdecfa616c5342dd2d654a9
kfudbc <kfudbc@users.noreply.github.com>
2020-12-03 11:24:13 +0100

    added matomo script, tag click now trigger replace, not push (#54)

--- src/pages/find.js
+++ src/pages/find.js
@@ -29,20 +29,10 @@ function Find() {
       { shallow: true }
     );
   }
-
-  const pageTitle = "Søg, find og lån fra alle Danmarks biblioteker";
-  const pageDescription =
-    "bibliotek.dk er din indgang til bibliotekernes fysiske og digitale materialer.";
   return (
     <React.Fragment>
       <Head>
-        <title>{pageTitle}</title>
-        <meta name="description" content={pageDescription}></meta>
         <meta property="og:url" content="https://beta.bibliotek.dk/find" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={pageDescription} />
-        <link rel="preconnect" href="https://moreinfo.addi.dk"></link>
       </Head>
       <div style={{ marginTop: 50 }}>
         <QuickFilters
