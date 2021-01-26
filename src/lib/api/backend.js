e55d396eeecb967b00f17cf9e6fe65ce0d75a16a
pjo <pjo@dbc.dk>
2021-01-26 12:08:07 +0100

    use config

--- src/lib/api/backend.js
+++ src/lib/api/backend.js
@@ -3,7 +3,7 @@ import config from "@/config";
 export default async function fetchTranslations() {
   try {
     // this one has its own 'page' to get translations - @see src/pages/api/translate.js
-    const res = await fetch("http://localhost:3000/api/translate", {
+    const res = await fetch(config.backend.internalurl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- src/pages/api/translate.js
+++ src/pages/api/translate.js
@@ -11,17 +11,14 @@ import Translate from "@/components/base/translate/Translate.json";
 export default async function handler(req, res) {
   // status flag
   let ok = true;
-  // @TODO try catch here
-  const response = await fetch(
-    "http://bibdk-backend-www-master.frontend-prod.svc.cloud.dbc.dk/get_translations",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(Translate),
-    }
-  );
+  // @TODO errorhandling
+  const response = await fetch(config.backend.url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Translate),
+  });
 
   const json = await response.json().catch((error) => {
     ok = false;
