a42cdceb96d7565a3c60c60128d5d607255d4db0
mtni <36037828+mtni@users.noreply.github.com>
2021-01-27 15:25:48 +0100

    BIBDK2021-160 - test + adjustments (#75)
    * ...
    
    * cy test

---
+++ e2e/cypress/integration/articlePage.spec.js
@@ -0,0 +1,42 @@
+describe("Article", () => {
+  before(() => {
+    cy.visit("/iframe.html?id=article-page--article-page&viewMode=story");
+  });
+
+  it(`Check if article has parsed body image`, () => {
+    // check for onlye 1 figure
+    cy.get("[data-cy=article-body]").find("figure").should("have.length", 1);
+    // check img attributes
+    cy.get("[data-cy=article-body]")
+      .find("img")
+      .invoke("attr", "src")
+      .should("eq", "/img/bibdk-hero-scaled.jpeg");
+    cy.get("[data-cy=article-body]")
+      .find("img")
+      .invoke("attr", "alt")
+      .should("eq", "Læser bog i hængekøje");
+    cy.get("[data-cy=article-body]")
+      .find("img")
+      .invoke("attr", "title")
+      .should("eq", "Hængekøje hygge med bog");
+    // check for onlye 1 figure caption
+    cy.get("[data-cy=article-body]")
+      .find("figcaption")
+      .should("have.length", 1);
+    // check for figure caption value match
+    cy.get("[data-cy=article-body]")
+      .find("figcaption")
+      .contains("Hængekøje hygge med bog");
+  });
+
+  it("Print article", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "print");
+      cy.get("[data-cy=article-print]")
+        .click()
+        .then(() => {
+          expect(win.print).to.be.calledOnce;
+        });
+    });
+  });
+});
