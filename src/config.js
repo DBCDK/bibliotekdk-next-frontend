77700deb0f6d16ad874fe6a827f5534cdc158b10
pjo <pjo@dbc.dk>
2021-02-02 12:18:27 +0100

    prod backend

--- src/config.js
+++ src/config.js
@@ -12,7 +12,9 @@ const config = {
     timeout: process.env.API_TIMEOUT_MS || 150,
   },
   backend: {
-    url: process.env.NEXT_BACKEND_API_URL || "http://localhost:7070",
+    url:
+      process.env.NEXT_BACKEND_API_URL ||
+      "http://bibdk-backend-www-master.frontend-prod.svc.cloud.dbc.dk/",
     timeout: process.env.API_TIMEOUT_MS || 150,
   },
 };
