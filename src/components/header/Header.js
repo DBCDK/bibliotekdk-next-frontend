0a3e10c8ed402a868c98388828adce3d6ac5dbeb
pjo <pjo@dbc.dk>
2021-02-05 12:08:27 +0100

    back to banner + notifications

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { Container, Row, Col } from "react-bootstrap";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import useHistory from "@/components/hooks/useHistory";
 
@@ -21,6 +21,8 @@ import LoginIcon from "./icons/login";
 import BasketIcon from "./icons/basket";
 import BurgerIcon from "./icons/burger";
 import SearchIcon from "./icons/search";
+import Notifications from "@/components/base/notifications/Notifications";
+import { APIStateContext } from "@/lib/api/api";
 
 export function Banner() {
   return (
@@ -117,6 +119,8 @@ function Header({ className = "", router = null, story = null }) {
 
   return (
     <header className={`${styles.wrap} ${className}`}>
+      <Banner />
+      <Notifications />
       <div className={styles.headerWrap}>
         <Container className={styles.header} fluid>
           <Row>
