5683b09f801a68d6c20f9835e2ae0b3709f129a7
Kasper F <kfu@dbc.dk>
2021-02-23 13:54:19 +0100

    add domain

--- next.config.js
+++ next.config.js
@@ -39,6 +39,7 @@ module.exports = {
     domains: [
       "forfatterweb.dk",
       "bibdk-backend-www-master.frontend-prod.svc.cloud.dbc.dk",
+      "bibdk-backend-www-master.frontend-staging.svc.cloud.dbc.dk",
     ],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 640, 1400],
   },
