4648f515b1321941d92d3c3f536203f795a8373a
pjo <pjo@dbc.dk>
2021-03-02 10:58:55 +0100

    added storiexs + test for banner component

---
+++ e2e/cypress/integration/banner.spec.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Test banner
+ */
+describe("TopBanner", () => {
+  before(function () {
+    cy.visit("/iframe.html?id=banner--top-banner");
+  });
+
+  it(`View in BIG viewport`, () => {
+    cy.viewport(1920, 1080);
+    // verify that banner is shown
+    cy.get("[data-cy=top-banner]").should("be.visible");
+  });
+
+  it(`View in SMALL viewport`, () => {
+    cy.viewport(765, 1080);
+    // verify that banner is NOT shown
+    cy.get("[data-cy=top-banner]").should("not.be.visible");
+  });
+});
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -6,7 +6,7 @@ import Translate from "@/components/base/translate";
 export default function Banner() {
   return (
     <div className={styles.bannerWrap}>
-      <Container className={styles.banner} fluid>
+      <Container className={styles.banner} fluid data-cy="top-banner">
         <Text type="text3">
           {Translate({ context: "header", label: "banner-text" })}
         </Text>
