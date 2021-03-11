bf1947f2ba036c7f8c7a37492bd3c17ef4b00fb0
pjo <pjo@dbc.dk>
2021-03-11 10:45:14 +0100

    in progress

--- src/components/helptexts/HelpText.js
+++ src/components/helptexts/HelpText.js
@@ -1,13 +1,7 @@
 import { useData } from "@/lib/api/api";
-import { publishedHelptexts } from "@/lib/api/helptexts.fragments.js";
+import { publishedHelptexts, helpText } from "@/lib/api/helptexts.fragments.js";
 import Title from "@/components/base/title";
 import Text from "@/components/base/text";
-import styles from "./HelpTexts.module.css";
-import Link from "@/components/base/link";
-import classNames from "classnames/bind";
-import React, { useState } from "react";
-import Icon from "@/components/base/icon";
-import { Row } from "react-bootstrap";
 
 export function getPublishedHelpTexts() {
   const { isLoading, data } = useData(publishedHelptexts());
@@ -29,109 +23,24 @@ export function HelpText({ helptext }) {
   }
 }
 
-export function HelpTextMenu({ helpTextId }) {
-  // @TODO use helptxtId to activate a menu
-  // get all helptexts
-  const { isLoading, data } = getPublishedHelpTexts();
+function helpTextById(allHelpTexts, helpTxtId) {
+  return allHelpTexts.find(({ nid }) => parseInt(helpTxtId, "10") === nid);
+}
 
-  // @TODO datacheck
-  if (isLoading) {
+export default function Wrapper({ helpTextID }) {
+  console.log(helpTextID, "INTITAL");
+
+  const { isLoading, data } = helpText(helpTextID);
+  console.log(data, "DATA");
+  if (!data) {
+    // @TODO skeleton
     return null;
-  } else {
-    const menus = helpTextParseMenu(data.nodeQuery.entities);
-
-    const [showMenus, setShowMenus] = useState(menus);
-    const rowClicked = ({ group }) => {
-      // toggle state of clicked group
-      showMenus[group].open = !showMenus[group].open;
-      // set new state(s)
-      setShowMenus(showMenus);
-    };
-
-    return Object.keys(showMenus).map((group, index) => {
-      console.log(showMenus, "FISK");
-      return (
-        <div className={styles.helpmenu} key={`group-${index}`}>
-          <Text
-            tabIndex="0"
-            type="text1"
-            lines={30}
-            key={`helpmenu-${index}`}
-            className={classNames(
-              showMenus[group].open ? styles.active : styles.helpgroup
-            )}
-            onClick={() => {
-              rowClicked({ group });
-            }}
-          >
-            <span>
-              <Icon size={{ w: 1, h: 1 }} src="arrowrightblue.svg" />
-            </span>
-            <span>{group}</span>
-          </Text>
-          <HelptTextMenuLinks
-            menuItems={showMenus}
-            group={group}
-            helpTextId={helpTextId}
-            key={`links-${index}`}
-          />
-        </div>
-      );
-    });
   }
-}
 
-function HelptTextMenuLinks({ menuItems, group, helpTextId }) {
-  // @TODO style the link
-  return menuItems[group].map((item, index) => (
-    <div>
-      <span>
-        <Icon size={{ w: 1, h: 1 }} src="arrowrightblue.svg" />
-      </span>
-      <Link
-        children={
-          <Text type="text3" lines={2}>
-            {item.title}
-          </Text>
-        }
-        href={{ pathname: `/helptexts/${item.id}`, query: {} }}
-        key={`menulink-${index}`}
-        className={classNames(
-          styles.helplink,
-          menuItems[group][index].id === parseInt(helpTextId.helptxtId, "10")
-            ? styles.active
-            : ""
-        )}
-      />
-    </div>
-  ));
-}
+  console.log(data, "DATA");
 
-function helpTextParseMenu(helptTexts) {
-  // sort helptexts by group
-  const structuredHelpTexts = {};
-  let element = {};
-  let group;
-  helptTexts.forEach((helptext, idx) => {
-    element = setGroupElement(helptext);
-    group = helptext.fieldHelpTextGroup;
-    if (structuredHelpTexts[group]) {
-      structuredHelpTexts[group].push(element);
-    } else {
-      structuredHelpTexts[group] = [element];
-    }
-  });
-  return structuredHelpTexts;
-}
+  const allHelpTexts = data.nodeQuery.entities;
+  const aHelpText = helpTextById(allHelpTexts, helpTextID);
 
-function setGroupElement(heltptext) {
-  return {
-    id: heltptext.nid,
-    title: heltptext.title,
-  };
-}
-
-export default function Wrapper(props) {
-  const { helptexts } = getPublishedHelpTexts();
-  return <HelpText {...props} helptexts={helptexts} />;
+  return <HelpText helptext={aHelpText} />;
 }
--- src/components/helptexts/Page.js
+++ src/components/helptexts/Page.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-import { HelpText, HelpTextMenu } from "./HelpText";
+import HelpText from "./HelpText";
 import { useData } from "@/lib/api/api";
 import styles from "@/components/header/Header.module.css";
 import { Col, Container, Row } from "react-bootstrap";
@@ -8,6 +8,7 @@ import React from "react";
 import { HelpTextHeader } from "./HelpTextHeader";
 import { Title } from "@/components/base/title/Title";
 import { publishedHelptexts } from "@/lib/api/helptexts.fragments";
+import HelpTextMenu from "./HelpTextMenu";
 
 /**
  * HelpText page React component
@@ -17,16 +18,19 @@ import { publishedHelptexts } from "@/lib/api/helptexts.fragments";
  *
  * @returns {component}
  */
-export default function HelpTextPage(helptxtId) {
-  const { isLoading, data } = useData(publishedHelptexts());
+export default function HelpTextPage({ helptxtId }) {
+  /*
+ const { isLoading, data } = useData(publishedHelptexts());
   if (!data || !data.nodeQuery || !data.nodeQuery.entities || data.error) {
     // @TODO skeleton
     return null;
   }
 
+
+
   const allHelpTexts = data.nodeQuery.entities;
   const aHelpText = helpTextById(allHelpTexts, helptxtId);
-
+*/
   return (
     <Container className={styles.header} fluid>
       <Row>
@@ -39,19 +43,13 @@ export default function HelpTextPage(helptxtId) {
       </Row>
       <Row>
         <Col md={{ span: 3 }}>
-          <HelpTextMenu helpTextId={helptxtId} />
+          <HelpTextMenu helpTextID={helptxtId} />
         </Col>
         <Col md={{ span: 9 }}>
-          <HelpText helptext={aHelpText} />
+          <HelpText helpTextID={helptxtId} />
         </Col>
       </Row>
     </Container>
-  );
-}
-
-function helpTextById(allHelpTexts, helpTxtId) {
-  return allHelpTexts.find(
-    ({ nid }) => parseInt(helpTxtId.helptxtId, "10") === nid
   );
 }
 
