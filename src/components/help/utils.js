17208780bb715e7408c0667cc922f29645c18864
mtni <mtni@dbc.dk>
2021-03-19 11:33:50 +0100

    ensure real article

---
+++ src/components/help/utils.js
@@ -0,0 +1,38 @@
+/**
+ * Parse helptexts by groups
+ * @param helpTexts
+ * @return {{}}
+ *  eg. {Søgning:[{id:25, title:fisk}. {id:1,title:hest}]}
+ */
+export function helpTextParseMenu(helpTexts) {
+  // sort helptexts by group
+  const structuredHelpTexts = {};
+  let element = {};
+  let group;
+
+  helpTexts.forEach((helptext) => {
+    if (helptext) {
+      element = setGroupElement(helptext);
+      group = helptext.fieldHelpTextGroup;
+      if (structuredHelpTexts[group]) {
+        structuredHelpTexts[group].push(element);
+      } else {
+        structuredHelpTexts[group] = [element];
+      }
+    }
+  });
+
+  return structuredHelpTexts;
+}
+
+/**
+ * Defines an element in a help group
+ * @param heltptext
+ * @return {{id: (number|string|*), title}}
+ */
+function setGroupElement(heltptext) {
+  return {
+    id: heltptext.nid,
+    title: heltptext.title,
+  };
+}
