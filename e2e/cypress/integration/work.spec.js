2ebc21831e9e295d1c7fd711b285ef5a3860e7bb
Kasper F <kfu@dbc.dk>
2021-04-06 08:11:15 +0200

    fix test

--- e2e/cypress/integration/work.spec.js
+++ e2e/cypress/integration/work.spec.js
@@ -9,9 +9,9 @@ describe("Work", () => {
   it(`renders all data on server`, () => {
     // we make a "request" instead of "visit" to see
     // the actual html returned from the server
-    // set timeout to 10000ms to make sure data is loaded on server
+    // set isBot=true to make sure data is loaded on server
     cy.request(
-      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar/work-of:870970-basis:53033423?timeout=10000`
+      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar/work-of:870970-basis:53033423?isBot=true`
     )
       .its("body")
       .then((html) => {
@@ -22,9 +22,8 @@ describe("Work", () => {
   it(`renders no data on server`, () => {
     // we make a request instead of visit to see
     // the actual html returned from the server
-    // set timeout to 1ms to make sure no data is loaded on server
     cy.request(
-      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar/work-of:870970-basis:53033423?timeout=1`
+      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar/work-of:870970-basis:53033423`
     )
       .its("body")
       .then((html) => {
