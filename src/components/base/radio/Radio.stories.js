d410dec4e58ea01ba1b49a9f2e8b90b194280c9f
kfudbc <kfudbc@users.noreply.github.com>
2021-05-19 13:32:51 +0200

    fix tabbing in pickup (#137)

--- src/components/base/radio/Radio.stories.js
+++ src/components/base/radio/Radio.stories.js
@@ -1,6 +1,7 @@
 import { StoryTitle, StoryDescription } from "@/storybook";
 
 import Radio from "@/components/base/radio";
+import animations from "@/components/base/animation/animations.module.css";
 import { useState } from "react";
 
 export default {
@@ -60,6 +61,38 @@ export function RadioGroupNoInitialSelection() {
           </Radio.Button>
         ))}
       </Radio.Group>
+    </div>
+  );
+}
+
+export function DisabledRadioGroup() {
+  const [selected, setSelected] = useState(rows[1]);
+  return (
+    <div>
+      <StoryTitle>Radio Button Group</StoryTitle>
+      <StoryDescription>
+        One can't tab into group, or check/uncheck radio buttons
+      </StoryDescription>
+      <Radio.Group enabled={false}>
+        {rows.map((row) => (
+          <Radio.Button
+            key={row.title}
+            selected={selected.title === row.title}
+            onSelect={() => setSelected(row)}
+            label={row.title}
+          >
+            <b>{row.title}</b>
+            <p>{row.description}</p>
+          </Radio.Button>
+        ))}
+      </Radio.Group>
+      <div
+        style={{ marginTop: 24, display: "inline-block" }}
+        tabIndex="0"
+        className={`${animations["on-focus"]} ${animations["f-outline"]}`}
+      >
+        I am tabbable
+      </div>
     </div>
   );
 }
