6efb802af9f54ba98ecd556d67878700edcab518
pjo <pjo@dbc.dk>
2021-06-07 13:26:33 +0200

    added some test + sessiontimer

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -8,13 +8,17 @@ import Link from "@/components/base/link";
 import Cookies from "js-cookie";
 
 /**
- *
+ * @param sessioneTime
+ * timer to show full feedback - defaults to 3 minutes
  * @param cookietime
  * timeout for cookie (milliseconds) - defaults to one week
  * @return {JSX.Element}
  * @constructor
  */
-export default function Feedback({ cookietime = 604800000 }) {
+export default function Feedback({
+  cookietime = 604800000,
+  sessioneTime = 180000,
+}) {
   const [feedbackOpen, setFeedbackOpen] = useState(false);
   const [showfeedback, setShowfeedback] = useState(false);
 
@@ -44,12 +48,32 @@ export default function Feedback({ cookietime = 604800000 }) {
     }
   };
 
+  const handleSessionTimer = () => {
+    const timer = sessionStorage.getItem("feedbackTimer");
+    if (!timer) {
+      sessionStorage.setItem("feedbackTimer", `${Date.now()}`);
+    } else {
+      const linkclicked = sessionStorage.getItem("linkclicked");
+      // user already clicked the link - hopefully he/she also answered
+      // the question - hide feedback for {cookietime}
+      if (linkclicked) {
+        handleCookie("click");
+      }
+      if (Date.now() - timer > sessioneTime) {
+        setFeedbackOpen(true);
+        sessionStorage.removeItem("feedbackTimer");
+      }
+    }
+  };
+
   useEffect(() => {
     handleCookie("load");
+    handleSessionTimer();
   });
 
   return (
     <div
+      data-cy="feedback-wrapper"
       className={classNames(
         styles.feedbackwrap,
         !showfeedback ? styles.feedbackhidden : ""
@@ -60,8 +84,12 @@ export default function Feedback({ cookietime = 604800000 }) {
           feedbackOpen ? styles.feedbackopen : styles.feedbackclosed
         )}
       >
-        <div className={styles.feedbackiconandtext}>
+        <div
+          className={styles.feedbackiconandtext}
+          data-cy="feedback-link-text"
+        >
           <Icon
+            data-cy="feedback-cookie-close"
             size={{ w: 4, h: 4 }}
             src="close_white_bold.svg"
             bgColor="var(--blue)"
@@ -81,6 +109,7 @@ export default function Feedback({ cookietime = 604800000 }) {
             href="https://forms.gle/1zgfCoQDmEJ9WQhj9"
             target="_blank"
             border={{ top: false, bottom: { keepVisible: true } }}
+            onClick={() => sessionStorage.setItem("linkclicked", true)}
           >
             <Text type="text3" tag="span">
               {Translate({
@@ -91,22 +120,24 @@ export default function Feedback({ cookietime = 604800000 }) {
           </Link>
         </div>
       </div>
-      <div className={styles.blue}>
-        <Text
-          type="text3"
-          className={styles.textrotate}
-          onClick={() => {
-            setFeedbackOpen(!feedbackOpen);
-          }}
-        >
-          <span className={styles.feedbackicon}>
-            <Icon
-              size={{ w: 1, h: 1 }}
-              src={`${feedbackOpen ? "arrowleft.svg" : "arrowright.svg"}`}
-            />
-          </span>
+      <div
+        data-cy="feedback-toggler"
+        className={styles.blue}
+        onClick={() => {
+          sessionStorage.removeItem("feedbackTimer");
+          setFeedbackOpen(!feedbackOpen);
+        }}
+        data-cy="feedback-toggle"
+      >
+        <Text tag="span" type="text3" className={styles.textrotate}>
           Feedback
         </Text>
+        <span className={styles.feedbackicon}>
+          <Icon
+            size={{ w: 1, h: 1 }}
+            src={`${feedbackOpen ? "arrowright.svg" : "arrowleft.svg"}`}
+          />
+        </span>
       </div>
     </div>
   );
