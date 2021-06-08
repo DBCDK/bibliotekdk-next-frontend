3a330dd917a0ede90dc5fe3c624eb7ee5a56ffd5
Kasper F <kfu@dbc.dk>
2021-06-08 15:23:51 +0200

    fetch pickupAllowed

--- src/lib/api/user.fragments.js
+++ src/lib/api/user.fragments.js
@@ -47,6 +47,7 @@ export function orderPolicy({ pid }) {
               lookUpUrl
             }
           }
+          pickupAllowed
         }
       }
       monitor(name: "bibdknext_orderpolicy")
