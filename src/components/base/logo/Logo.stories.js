8a4c1facae08c0933fb2d26666e8f280845e5cde
pjo <pjo@dbc.dk>
2021-06-14 11:21:08 +0200

    renamed and refactored + storybook

--- src/components/base/logo/Logo.stories.js
+++ src/components/base/logo/Logo.stories.js
@@ -1,6 +1,6 @@
 import { StoryTitle, StoryDescription, StorySpace } from "@/storybook";
 import { useState } from "react";
-import { Logo } from "./Logo";
+import Logo from "./Logo";
 
 export default {
   title: "base/logo",
@@ -10,12 +10,26 @@ export default {
  * Returns Header
  *
  */
-export function Logostory() {
+export function DefaultLogo() {
   return (
-    <div style={{ height: "800px" }}>
+    <div style={{ height: "800px", width: "400px" }}>
       <StoryTitle>Logo component</StoryTitle>
       <StoryDescription>Default logo</StoryDescription>
-      <Logo />
+      <div style={{ padding: "20px" }}>
+        <Logo text="default_logo_text" />
+      </div>
+    </div>
+  );
+}
+
+export function BlueLogo() {
+  return (
+    <div style={{ height: "800px", width: "400px" }}>
+      <StoryTitle>Logo component</StoryTitle>
+      <StoryDescription>Hjælp og vejledninger logo</StoryDescription>
+      <div style={{ backgroundColor: "blue", padding: "20px" }}>
+        <Logo fill="var(--white)" text="help_logo_text" />
+      </div>
     </div>
   );
 }
