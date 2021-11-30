9d8f35cd383ee2247b44aa366cbe332b8ce3b872
pjo <pjo@dbc.dk>
2021-11-30 14:48:14 +0100

    specific pid

--- src/components/_modal/pages/order/Order.page.js
+++ src/components/_modal/pages/order/Order.page.js
@@ -582,11 +582,7 @@ export function Order({
                 onArticleSubmit(pid, pickupBranch.branchId);
               } else {
                 onSubmit &&
-                  onSubmit(
-                    materialsSameType.map((m) => m.pid),
-                    pickupBranch,
-                    context?.periodicaForm
-                  );
+                  onSubmit(orderPids, pickupBranch, context?.periodicaForm);
               }
             } else {
               setHasTry(true);
