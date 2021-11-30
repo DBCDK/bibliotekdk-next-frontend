e12aed4ac9bac7d7fd350db2270567f8ad820465
mtni <36037828+mtni@users.noreply.github.com>
2021-11-30 15:30:26 +0100

    Litteratursiden rating fix (#263)
    * ...
    
    * solve test err
    
    * fix test

--- src/components/work/reviews/Reviews.stories.js
+++ src/components/work/reviews/Reviews.stories.js
@@ -13,8 +13,8 @@ import {
 } from "./types/infomedia/InfomediaReview.js";
 
 import {
-  LitteratursidenReview,
-  LitteratursidenReviewSkeleton,
+  ExternalReview,
+  ExternalReviewSkeleton,
 } from "./types/external/ExternalReview.js";
 
 export default {
@@ -27,60 +27,62 @@ export default {
  */
 
 export function ReviewsSlider() {
-  const data = [
-    {
-      __typename: "ReviewInfomedia",
-      author: "Svend Svendsen",
-      date: "2013-06-25",
-      media: "Jyllandsposten",
-      rating: "4/5",
-      url: "http://",
-    },
-    {
-      __typename: "ReviewExternalMedia",
-      author: "Didrik Pedersen",
-      date: "2013-06-25",
-      media: "",
-      rating: "1/5",
-      url: "http://",
-    },
-    {
-      __typename: "ReviewMatVurd",
-      author: "Svend Svendsen",
-      date: "2013-06-25",
-      all: [
-        {
-          text: "Romanen er Ninni SchuImans anden fritstående krimi om journalisten Magdalene og betjentene Petra og Christer. I den svenske sommervarme er en pyroman løs. En kvinde omkommer i flammerne og nu må politiet, sammen med journalisten Magdalene, forsøge at finde frem til gerningsmanden, inden der bliver begået flere mord. Bag det spinkle plot, som er meget utroværdigt selv inden for genrens rammer, er romanen en skildring af fortabte drømme, kærlighed og livet omkring de 40. Problemet er dog, at de to dele, krimien og hverdagsrealismen, aldrig bliver koblet ordenligt sammen, og så hæmmer det realismen voldsomt, at plottet ikke er troværdigt. Politifolkene fremstår eksempelvis unødvendigt inkompetente. Forfatteren formår dog at komme med fine miljøskildringer og et par enkelte fine karakterer, som hæver sig over det todimensionale og skabelonagtige",
-        },
-        {
-          text: 'Til den store læserskare af "nordiske krimier tilsat hverdagsrealisme". Der findes mange bedre krimier, der ligner Drengen der holdt op med at græde til forveksling',
-        },
-        {
-          text: "Det er oplagt at sammenligne med Liza Marklund og Mari Jungstedt, der også benytter sig af makkerparret journalist/politimand",
-        },
-        {
-          text: "Ordinær krimi, med fine miljøskildringer, hvor plottet dog virker forceret og utroværdigt, og det drukner de ellers fine tilløb til hverdagsrealisme. Der findes mange bedre skandinaviske krimier på markedet, som formår at koble det realistiske sammen med et veludført plot",
-        },
-      ],
-      url: "http://",
-    },
-    {
-      __typename: "ReviewInfomedia",
-      date: "2013-06-25",
-      author: "Didrik Pedersen",
-      media: "Berlingske Tidende",
-      rating: "5/5",
-      url: "http://",
-    },
-    {
-      __typename: "ReviewInfomedia",
-      date: "2013-06-25",
-      author: "Svend Svendsen",
-      media: "Jyllandsposten",
-      rating: "4/5",
-      url: "http://",
-    },
-  ];
+  const data = {
+    reviews: [
+      {
+        __typename: "ReviewInfomedia",
+        author: "Svend Svendsen",
+        date: "2013-06-25",
+        media: "Jyllandsposten",
+        rating: "4/5",
+        url: "http://",
+      },
+      {
+        __typename: "ReviewExternalMedia",
+        author: "Didrik Pedersen",
+        date: "2013-06-25",
+        media: "Litteratursiden.dk online",
+        rating: "1/5",
+        url: "http://",
+      },
+      {
+        __typename: "ReviewMatVurd",
+        author: "Svend Svendsen",
+        date: "2013-06-25",
+        all: [
+          {
+            text: "Romanen er Ninni SchuImans anden fritstående krimi om journalisten Magdalene og betjentene Petra og Christer. I den svenske sommervarme er en pyroman løs. En kvinde omkommer i flammerne og nu må politiet, sammen med journalisten Magdalene, forsøge at finde frem til gerningsmanden, inden der bliver begået flere mord. Bag det spinkle plot, som er meget utroværdigt selv inden for genrens rammer, er romanen en skildring af fortabte drømme, kærlighed og livet omkring de 40. Problemet er dog, at de to dele, krimien og hverdagsrealismen, aldrig bliver koblet ordenligt sammen, og så hæmmer det realismen voldsomt, at plottet ikke er troværdigt. Politifolkene fremstår eksempelvis unødvendigt inkompetente. Forfatteren formår dog at komme med fine miljøskildringer og et par enkelte fine karakterer, som hæver sig over det todimensionale og skabelonagtige",
+          },
+          {
+            text: 'Til den store læserskare af "nordiske krimier tilsat hverdagsrealisme". Der findes mange bedre krimier, der ligner Drengen der holdt op med at græde til forveksling',
+          },
+          {
+            text: "Det er oplagt at sammenligne med Liza Marklund og Mari Jungstedt, der også benytter sig af makkerparret journalist/politimand",
+          },
+          {
+            text: "Ordinær krimi, med fine miljøskildringer, hvor plottet dog virker forceret og utroværdigt, og det drukner de ellers fine tilløb til hverdagsrealisme. Der findes mange bedre skandinaviske krimier på markedet, som formår at koble det realistiske sammen med et veludført plot",
+          },
+        ],
+        url: "http://",
+      },
+      {
+        __typename: "ReviewInfomedia",
+        date: "2013-06-25",
+        author: "Didrik Pedersen",
+        media: "Berlingske Tidende",
+        rating: "5/5",
+        url: "http://",
+      },
+      {
+        __typename: "ReviewInfomedia",
+        date: "2013-06-25",
+        author: "Svend Svendsen",
+        media: "Jyllandsposten",
+        rating: "4/5",
+        url: "http://",
+      },
+    ],
+  };
 
   return (
     <div>
@@ -164,7 +166,7 @@ export function Infomedia() {
   );
 }
 
-export function Litteratursiden() {
+export function ExternalMedia() {
   const data = {
     author: "Svend Svendsen",
     reviewType: "INFOMEDIA",
@@ -173,17 +175,17 @@ export function Litteratursiden() {
   return (
     <div>
       <div>
-        <StoryTitle>Litteratursiden template</StoryTitle>
-        <StoryDescription>Litteratursiden review example</StoryDescription>
-        <LitteratursidenReview data={data} />
+        <StoryTitle>External media template</StoryTitle>
+        <StoryDescription>External media review example</StoryDescription>
+        <ExternalReview data={data} />
       </div>
 
       <StorySpace direction="v" space="3" />
 
       <div>
-        <StoryTitle>Litteratursiden template</StoryTitle>
-        <StoryDescription>Litteratursiden review example</StoryDescription>
-        <LitteratursidenReviewSkeleton />
+        <StoryTitle>External media template</StoryTitle>
+        <StoryDescription>External media review example</StoryDescription>
+        <ExternalReviewSkeleton />
       </div>
     </div>
   );
