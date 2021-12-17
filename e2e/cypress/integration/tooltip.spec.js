3f7556b9579a8a1a1710e68fe547fef6b9b9f59c
pjo <pjo@dbc.dk>
2021-12-17 08:56:24 +0100

    added a test

---
+++ e2e/cypress/integration/tooltip.spec.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Test feedback
+ */
+describe("feedback", () => {
+  before(function () {
+    cy.visit("/iframe.html?id=base-tooltips--simple-tooltip");
+  });
+
+  it(`Tooltip present on click`, () => {
+    cy.get('[data-cy="popover-container"]').should("not.exist");
+    cy.get('[data-cy="tooltip-icon"]').click();
+    cy.get('[data-cy="popover-container"]').should("be.visible");
+  });
+});
