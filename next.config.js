abff71b98dfc5b8af7dd4d0cde7305a9d52cf8cb
Kasper Fuglsang <kfu@dbc.dk>
2021-12-29 12:17:31 +0100

    disable eslint

--- next.config.js
+++ next.config.js
@@ -99,4 +99,9 @@ module.exports = {
       url: process.env.NEXTAUTH_URL,
     },
   },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
