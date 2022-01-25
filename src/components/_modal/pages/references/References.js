f4e443dd0f064493a97c65baae220e7cbdc82fb9
pjo <pjo@dbc.dk>
2022-01-25 08:46:53 +0100

    correct external url

--- src/components/_modal/pages/references/References.js
+++ src/components/_modal/pages/references/References.js
@@ -3,9 +3,10 @@ import Link from "@/components/base/link";
 import Text from "@/components/base/text/Text";
 import Top from "@/components/_modal/pages/base/top";
 import styles from "./References.module.css";
+import config from "@/config";
 
 const onlinelinks = (pid) => {
-  const selfurl = "http://localhost:3000";
+  const selfurl = config.externalBaseUrl;
   return {
     refworks: `http://www.refworks.com/express/ExpressImport.asp?vendor=bibliotek.dk&filter=RefWorks Tagged Format&encoding=28591&url=${selfurl}/api/refworks?pid=${pid}`,
     endnote: `http://www.myendnoteweb.com/EndNoteWeb.html?func=directExport&partnerName=bibliotek.dk&dataIdentifier=1&dataRequestUrl=${selfurl}/api/ris?pid=${pid}`,
