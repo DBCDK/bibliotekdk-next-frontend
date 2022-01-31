200d833f10af1bbb606fa1984cb347a1bb2c3add
pjo <pjo@dbc.dk>
2022-01-31 09:03:05 +0100

    fixed story

--- src/components/_modal/pages/references/References.stories.js
+++ src/components/_modal/pages/references/References.stories.js
@@ -1,17 +1,42 @@
 import { StoryTitle, StoryDescription, StorySpace } from "@/storybook";
 
 import { References } from "@/components/_modal/pages/references/References";
+import { Edition } from "@/components/_modal/pages/order/Order.page";
+import dummywork from "./dummydata.json";
 
 export default {
   title: "modal/References",
 };
 
 export function referenceLinks() {
+  const context = {
+    pids: ["820030-katalog:246833"],
+    work: {},
+    manifestation: {},
+  };
   return (
     <div>
       <StoryTitle>Link reference systems</StoryTitle>
       <StoryDescription>endnote refworks download</StoryDescription>
-      <References pid="820030-katalog:246833" />
+      <References context={context} />
     </div>
   );
+}
+
+export function editionSingleManifestation() {
+  /** NOTE
+   * <div className={styles.item}>
+   *         <Edition
+   *           material={manifestation}
+   *           work={work}
+   *           isLoading={false}
+   *           context={context}
+   *           showOrderTxt={false}
+   *           singleManifestation={true}
+   *         />
+   *       </div>
+   */
+  const work = dummywork.work;
+  const manifestation = dummywork.work.manifestations[0];
+  return <div>fisk</div>;
 }
