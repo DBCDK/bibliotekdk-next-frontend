b9c07c73c932c725159ea9c29096d6794ba99b86
pjo <pjo@dbc.dk>
2022-05-06 11:09:38 +0200

    new hero in og:image also

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,10 +37,7 @@ const Index = () => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
-        <meta
-          property="og:image"
-          content={`${root}/img/bibdk-og-cropped.jpg`}
-        />
+        <meta property="og:image" content={`${root}/img/og-new-hero.jpg`} />
         <link rel="preconnect" href="https://moreinfo.addi.dk"></link>
         <link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml" />
         <link rel="alternate icon" href="favicon.ico" />
