2642e4a7c53827b44ddd1f09edc8082b7d6958a1
pjo <pjo@dbc.dk>
2022-08-05 12:51:23 +0200

    added a module - feedbacklink

---
+++ src/components/feedbacklink/FeedBackLink.js
@@ -0,0 +1,25 @@
+import styles from "@/components/feedbacklink/FeedBackLink.module.css";
+import Text from "@/components/base/text/Text";
+import Translate from "@/components/base/translate";
+import Link from "@/components/base/link";
+
+export default function FeedbackLink() {
+  return (
+    <div data-cy="feedback-wrapper" className={styles.feedbackwrap}>
+      <div className={styles.feedbacklink} data-cy="feedback-link-text">
+        <Link
+          href="https://kundeservice.dbc.dk/bibdk"
+          target="_blank"
+          border={{ top: false, bottom: { keepVisible: true } }}
+        >
+          <Text tag="span" type="text3" className={styles.textrotate}>
+            {Translate({
+              context: "feedback",
+              label: "feed_back_kunderservice_link_text",
+            })}
+          </Text>
+        </Link>
+      </div>
+    </div>
+  );
+}
