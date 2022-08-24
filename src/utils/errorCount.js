96544778d510e15f421cfa4588bbdaa2d6cf81b8
pjo <pjo@dbc.dk>
2022-08-24 13:05:54 +0200

    new errorcount

--- src/utils/errorCount.js
+++ src/utils/errorCount.js
@@ -1,32 +1,81 @@
-import fs from "fs";
-let errorCount = 0;
+/**
+ *
+ */
+import { promises as Fs } from "fs";
+import { log } from "dbc-node-logger";
 const path = "/tmp/errorcount";
 
-console.log("INTIERRORCOUNT");
-
-export function getErrorCount() {
-  console.log("GETERROR -- errorCount");
-  fs.readFile(path, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data, "DATA ERRORCOUNT");
-    return data;
-  });
+/**
+ * Get error count
+ * @returns {Promise<*>}
+ */
+export async function getErrorCount() {
+  if (await exists()) {
+    return await read();
+  } else {
+    return "0";
+  }
 }
 
-export function incErrorCount() {
-  fs.writeFile(path, (errorCount++).toString(), { flag: "w+" }, function (err) {
-    if (err) {
-      console.log(err, "WRITE ERROR");
-      throw err;
-    }
-    console.log("It's saved!");
-  });
+/**
+ * helper function to check if the file is there ?
+ * @returns {Promise<boolean>}
+ */
+async function exists() {
+  try {
+    await Fs.access(path);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
-export function resetErrorCount() {
-  errorCount = 0;
-  incErrorCount();
+/**
+ * helper function to read the file
+ * @returns {Promise<string>}
+ */
+async function read() {
+  try {
+    return await Fs.readFile(path, { encoding: "utf8" });
+  } catch (e) {
+    log.error(`Read errorcount failed:`, {
+      error: String(e),
+      stacktrace: e.stack,
+    });
+  }
+}
+
+/**
+ * helper function to write the file
+ * @param count
+ * @returns {Promise<void>}
+ */
+async function write(count) {
+  try {
+    // write the file - overwrite existing - create if not exists (w+)
+    await Fs.writeFile(path, count, { flag: "w+" });
+  } catch (e) {
+    log.error(`Write errorcount failed:`, {
+      error: String(e),
+      stacktrace: e.stack,
+    });
+  }
+}
+
+/**
+ * Increase error count
+ * @returns {Promise<void>}
+ */
+export async function incErrorCount() {
+  const count = await getErrorCount();
+  let errorCount = parseInt(count);
+
+  await write((++errorCount).toString());
+}
+
+/**
+ * Reset error count
+ */
+export async function resetErrorCount() {
+  await write("0");
 }
