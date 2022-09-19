c2c88b53613d18bcb063cd1afaa5f73810dbce40
Anders Wulff Kringelbach <akri@dbc.dk>
2022-09-19 14:13:26 +0200

    Consistently use describe

--- e2e/cypress/integration/search.spec.js
+++ e2e/cypress/integration/search.spec.js
@@ -1,5 +1,5 @@
 describe("Search", () => {
-  context(`Form`, () => {
+  describe(`Form`, () => {
     it(`Maps query parameters from url to input fields`, () => {
       cy.visit("/iframe.html?id=layout-header--nav-header-prefilled");
 
@@ -160,7 +160,7 @@ describe("Search", () => {
         expect(JSON.parse(el.text())).to.deep.equal({ "q.all": "hest" });
       });
     });
-    context(`Mobile`, () => {
+    describe(`Mobile`, () => {
       it(`Maintains input value when opening mobile suggester`, () => {
         cy.viewport("iphone-6");
         cy.visit("/iframe.html?id=layout-header--nav-header-prefilled");
@@ -217,7 +217,7 @@ describe("Search", () => {
     });
   });
 
-  context(`Result`, () => {
+  describe(`Result`, () => {
     it(`Maps from URL params to a search result`, () => {
       cy.visit("/iframe.html?id=search-result--connected");
       cy.get("[data-cy=router-query]").then((el) => {
