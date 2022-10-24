df8f531dabc6a9c158951beb04514f669ed7a276
kfudbc <kfudbc@users.noreply.github.com>
2022-10-24 09:51:37 +0200

    rewrite some filters tests, now mostly with mocked graphql and the wr… (#429)
    * rewrite some filters tests, now mostly with mocked graphql and the wrapped component
    
    * clean up

--- src/components/_modal/pages/filter/Filter.stories.js
+++ src/components/_modal/pages/filter/Filter.stories.js
@@ -1,17 +1,73 @@
 import { useEffect } from "react";
+import { StoryTitle, StoryDescription } from "@/storybook";
 
-import { Filter } from "./Filter.page";
+import FilterConnected, { Filter } from "./Filter.page";
 import Modal, { useModal } from "@/components/_modal";
 
 import useFilters from "@/components/hooks/useFilters";
 
 import response from "./dummy.data";
+import { useRouter } from "next/router";
 
 const exportedObject = {
   title: "Modal/Filter",
 };
 
 export default exportedObject;
+
+export function Connected() {
+  const modal = useModal();
+  return (
+    <div>
+      <StoryTitle>Filters</StoryTitle>
+      <StoryDescription>
+        Search filters open in a modal. Play around with it to see how it
+        synchronizes with the url (on modal close).
+      </StoryDescription>
+      <button onClick={() => modal.push("filter")}>open filters</button>
+
+      <Modal.Container>
+        <Modal.Page id="filter" component={FilterConnected} />
+      </Modal.Container>
+    </div>
+  );
+}
+Connected.story = {
+  parameters: {
+    graphql: {
+      debug: true,
+      resolvers: {
+        SearchResponse: {
+          // Base the hitcount on the selection of a specific subject
+          hitcount: (args) =>
+            args?.variables?.filters?.subjects?.includes("krimi") ? 8 : 20,
+
+          // Return two facets
+          facets: (args) => [...new Array(3).fill({})],
+        },
+        FacetResult: {
+          // Return the names of the two facets
+          name: ({ getNext }) => getNext(["subjects", "creators", "workTypes"]),
+
+          // Let each facet have two values
+          values: () => [...new Array(3).fill({})],
+        },
+        FacetValue: {
+          term: ({ path, getNext }) =>
+            path.includes("facets[0]")
+              ? getNext(["krimi", "gys", "with, comma"])
+              : getNext(["Hans Hansen", "Jens Jensen", "Ole Olesen"]),
+          score: ({ getNext }) => getNext([8, 12, 2]),
+        },
+      },
+    },
+    nextRouter: {
+      showInfo: true,
+      pathname: "/find",
+      query: { "q.all": "some query" },
+    },
+  },
+};
 
 export function Default() {
   const { setStack } = useModal();
