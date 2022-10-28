167c7780fad88ca8f05963c8811e3a083674128e
pjo <pjo@dbc.dk>
2022-10-28 12:32:47 +0200

    at least check that SetPickupAgency component is active

--- e2e/cypress/integration/old/login.spec.js
+++ e2e/cypress/integration/old/login.spec.js
@@ -20,4 +20,21 @@ describe("Login", () => {
     // Make sure api request with bearer token has been called
     cy.wait("@apiRequest");
   });
+
+  it(`should set user pickupagency from url parameter`, () => {
+    cy.intercept("POST", /.*graphql/, (req) => {
+      console.log(req.headers.authorization);
+      expect(req.headers.authorization).to.match(/bearer .+/i);
+    }).as("apiRequest");
+
+    cy.visit(
+      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar/work-of:870970-basis:53033423?setPickupAgency=999999`
+    );
+
+    // verify that parameter setPickupAgency has been removed by SetPickupBranch component
+    cy.url().should("not.include", "setPickupAgency=999999");
+
+    // Make sure api request with bearer token has been called
+    cy.wait("@apiRequest");
+  });
 });
