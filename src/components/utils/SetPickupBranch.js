26b4ad1471d140d7be6f014f80e180e3f9a6db36
pjo <pjo@dbc.dk>
2022-10-28 10:42:03 +0200

    void component to listen for login

---
+++ src/components/utils/SetPickupBranch.js
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import useUser from "@/components/hooks/useUser";
+
+export default function SetPickupBranch() {
+  // the useeffect below updates user pickupbranch after a login from loanerform.
+  // when user
+  const { updateLoanerInfo } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    if (router.query.setPickupAgency) {
+      updateLoanerInfo({ pickupBranch: router.query.setPickupAgency });
+      // pickup agency has been set
+      let query = { ...router.query };
+      delete query.setPickupAgency;
+      router?.replace({ pathname: router.pathname, query });
+    }
+  }, []);
+
+  return null;
+}
