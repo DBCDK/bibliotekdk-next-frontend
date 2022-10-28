5a5102d17cc952c70e7141f290a969204cf4717b
Anders Wulff Kringelbach <akri@dbc.dk>
2022-10-28 17:27:41 +0200

    Remove unused functions

--- src/components/work/reservationbutton/utils.js
+++ src/components/work/reservationbutton/utils.js
@@ -1,6 +1,5 @@
 // Translate Context
-export const context = { context: "overview" };
-
+import Translate from "@/components/base/translate";
 /**
  * Example:
  *
@@ -10,7 +9,8 @@ export const context = { context: "overview" };
  * @param {string} url
  * @returns {string}
  */
-import { encodeTitleCreator, infomediaUrl } from "@/lib/utils";
+
+export const context = { context: "overview" };
 
 export function getBaseUrl(url) {
   if (!url) {
@@ -54,18 +54,6 @@ export function checkDigitalCopy({ manifestations }) {
 }
 
 /**
- * Check if order of digital copy is an option - if one af the manifestions in work
- * has an issn (International Standard Serial Number) return true, if not return false
- * @param manifestations
- * @return {boolean}
- */
-export function checkDigitalCopy_TempUsingAlfaApi({ manifestations }) {
-  return !!manifestations?.find((manifestation) =>
-    manifestation?.onlineAccess?.find((access) => access?.issn)
-  );
-}
-
-/**
  * Select the manifestations that match the chosen type
  * @param {Array<Object>} manifestations
  * @param {String} type
@@ -80,26 +68,6 @@ export function selectMaterialBasedOnType(manifestations, type) {
     return manifestation.materialTypes.find(
       (materialType) => flattenWord(materialType.specific) === flattenWord(type)
     );
-  });
-}
-
-/**
- * infomedia url is specific for this gui - set an url on the online access object
- * @param onlineAccess
- * @param title
- * @return {*}
- */
-export function addToInfomedia_TempUsingAlfaApi(onlineAccess, title) {
-  return onlineAccess?.map((access) => {
-    if (access.infomediaId) {
-      access.url = infomediaUrl(
-        encodeTitleCreator(title),
-        `work-of:${access.pid}`,
-        access.infomediaId
-      );
-      access.accessType = "infomedia";
-    }
-    return access;
   });
 }
 
@@ -157,42 +125,21 @@ export function selectMaterial(manifestations) {
   return selectedManifestation || manifestationsOnlyLoanIsPossible?.[0] || null;
 }
 
-/**
- * Find and return the manifestation we want on the reservation button.
- * If manifestation has an online url it is the preferred - for easy access
- * @param manifestations
- * @return {*}
- */
-export function selectMaterial_TempUsingAlfaApi(manifestations) {
-  // check if onlineacces. if so get the first manifestation with an online url - if any
-  let selectedManifestation;
-  let url;
-  manifestations?.every((manifest) => {
-    // outer loop -> manifestations
-    if (manifest.onlineAccess?.length > 0) {
-      // inner loop -> onlineaccess
-      manifest.onlineAccess.every((access) => {
-        // special case: "dfi.dk" is not a 'real' onlineaccess
-        if (access.url && access.url.indexOf("dfi.dk") === -1) {
-          url = access.url;
-          // we found an online access -> break inner loop
-          return false;
-        }
-        // continue inner loop
-        return true;
-      });
-      if (url) {
-        // outer loop -> check if url has been set - if so
-        // this is the manifestation we are looking for
-        selectedManifestation = manifest;
-        // break outer loop
-        return false;
-      }
-    }
-    // continue outer loop
-    return true;
+export function openOrderModal(
+  modal,
+  workId,
+  singleManifestation,
+  manifestation
+) {
+  modal.push("order", {
+    title: Translate({ context: "modal", label: "title-order" }),
+    pid: manifestation.pid,
+    workId,
+    type: manifestation.materialTypes.specific,
+    ...(singleManifestation && { orderType: "singleManifestation" }),
   });
-  // if a manifestion with an url has been found it will be returned - if not
-  // return the first manifestation in array
-  return selectedManifestation || manifestations?.[0];
+}
+
+export function onOnlineAccess(url, target = "_blank") {
+  window.open(url, target);
 }
