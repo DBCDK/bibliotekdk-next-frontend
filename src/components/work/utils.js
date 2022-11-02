26cc28628cbb6665bb6fe42eed0299c6c827707e
pjo <pjo@dbc.dk>
2022-11-02 12:44:53 +0100

    added some functions to work/utils

--- src/components/work/utils.js
+++ src/components/work/utils.js
@@ -1,4 +1,6 @@
 import Translate from "@/components/base/translate";
+import { uniq } from "lodash";
+import { uniqueEntries } from "@/lib/utils";
 
 export function openLocalizationsModal(modal, pids, workId, materialType) {
   modal.push("localizations", {
@@ -38,4 +40,74 @@ export function openReferencesModal(modal, pids, work, manifestation) {
 
 export function onOnlineAccess(url, target = "_blank") {
   window.open(url, target);
+}
+
+/**
+ * Generates the work page description
+ * @param {object} work The work
+ * @returns {string}
+ */
+export function getPageDescription({ title, creators, materialTypes }) {
+  const creator = creators && creators[0] && creators[0]?.display;
+  const allowedTypes = ["lydbog", "e-bog", "bog"];
+  const types = uniq(
+    materialTypes
+      .map((entry) => {
+        for (let i = 0; i < allowedTypes.length; i++) {
+          if (entry.toLowerCase().includes(allowedTypes[i])) {
+            return allowedTypes[i];
+          }
+        }
+      })
+      .filter((type) => !!type)
+  );
+
+  let typesString = "";
+  types.forEach((type, idx) => {
+    if (idx > 0) {
+      if (idx === types.length - 1) {
+        // last element
+        typesString += " eller ";
+      } else {
+        // middle element
+        typesString += ", ";
+      }
+    } else {
+      // first element
+      typesString = " som ";
+    }
+    typesString += type;
+  });
+
+  return `Lån ${title}${
+    creator ? ` af ${creator}` : ""
+  }${typesString}. Bestil, reserver, lån fra alle danmarks biblioteker. Afhent på dit lokale bibliotek eller find online.`;
+}
+
+function getMaterialTypes(manifestations) {
+  return uniqueEntries(
+    manifestations?.flatMap((manifestation) => {
+      return manifestation.materialTypes?.map((materialType) => {
+        return materialType.specific;
+      });
+    })
+  );
+}
+
+export function getSeo(work) {
+  const materialTypes = getMaterialTypes(work?.manifestations?.all);
+  //return materialTypes;
+  // Return title and description
+  return {
+    title: `${work?.titles?.main[0]}${
+      work?.creators && work?.creators[0]
+        ? ` af ${work?.creators[0].display}`
+        : ""
+    }`,
+    description: getPageDescription({
+      title: work?.titles?.main[0],
+      creators: work?.creators,
+      materialTypes,
+    }),
+  };
 }
