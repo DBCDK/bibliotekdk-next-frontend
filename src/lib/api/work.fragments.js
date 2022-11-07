c2a89297d04af955f4fa1120aab14f82b64d911d
Anders Wulff Kringelbach <akri@dbc.dk>
2022-11-07 15:16:24 +0100

    Prettier

--- src/lib/api/work.fragments.js
+++ src/lib/api/work.fragments.js
@@ -661,13 +661,43 @@ export function editionWork({ workId }) {
         titles {
           full
         }
-        
         materialTypes {
           specific
         }
         workTypes
       }
       monitor(name: "bibdknext_edition_work")
+    }`,
+    variables: { workId },
+    slowThreshold: 3000,
+  };
+}
+
+export function listOfAllManifestations({ workId }) {
+  return {
+    apiUrl: ApiEnums.FBI_API,
+    query: `
+    query listOfAllManifestations($workId: String!) {
+      work(id: $workId) {
+        manifestations {
+          all {
+            pid
+            volume
+            titles {
+              main
+            }
+            materialTypes {
+              specific
+            }
+            edition {
+              publicationYear {
+                display
+              }
+            }
+          }
+        }
+      }
+      monitor(name: "bibdknext_list_of_all_manifestations")
     }`,
     variables: { workId },
     slowThreshold: 3000,
