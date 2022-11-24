e06c8cda3e25222b4ef4dc0fc651277a5a36c211
pjo <pjo@dbc.dk>
2022-11-24 08:27:02 +0100

    in progress - skipped inspiration test - mtni says OK

--- src/components/utils/getCoverImage.js
+++ src/components/utils/getCoverImage.js
@@ -1,13 +1,9 @@
 export function getCoverImage(manifestations) {
-  console.log(manifestations, "ALL MANIFESTATIONS");
-
   const manifestationWithCover =
     manifestations?.find(
       (manifestation) =>
         manifestation.cover.detail && manifestation.cover.origin === "moreinfo"
     ) || manifestations?.find((manifestation) => manifestation.cover.detail);
-
-  console.log(manifestationWithCover, "MANIFESTATION WITH COVER");
 
   const coverImage = manifestationWithCover
     ? { detail: manifestationWithCover.cover.detail }
