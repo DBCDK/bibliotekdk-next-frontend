5c3c9207d99438849eb9b719289d91033472756e
Anders Wulff Kringelbach <akri@dbc.dk>
2022-11-25 13:15:34 +0100

    Null checks

--- src/components/utils/getCoverImage.js
+++ src/components/utils/getCoverImage.js
@@ -1,13 +1,10 @@
 export function getCoverImage(manifestations) {
   const manifestationWithCover =
     manifestations?.find(
-      (manifestation) =>
-        manifestation.cover.detail && manifestation.cover.origin === "moreinfo"
-    ) || manifestations?.find((manifestation) => manifestation.cover.detail);
+      (manifestation) => manifestation?.cover?.detail?.origin === "moreinfo"
+    ) || manifestations?.find((manifestation) => manifestation?.cover?.detail);
 
-  const coverImage = manifestationWithCover
-    ? { detail: manifestationWithCover.cover.detail }
+  return manifestationWithCover
+    ? { detail: manifestationWithCover?.cover?.detail }
     : { detail: null };
-
-  return coverImage;
 }
