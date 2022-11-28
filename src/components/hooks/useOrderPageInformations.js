625ecf5d0e295a5904bfe1abc8f7ff564d7df81a
Anders Wulff Kringelbach <akri@dbc.dk>
2022-11-28 16:13:40 +0100

    Add BlockedUser to hook

--- src/components/hooks/useOrderPageInformations.js
+++ src/components/hooks/useOrderPageInformations.js
@@ -1,6 +1,7 @@
 import usePickupBranch from "@/components/hooks/usePickupBranch";
 import { useData } from "@/lib/api/api";
 import * as workFragments from "@/lib/api/work.fragments";
+import * as branchesFragments from "@/lib/api/branches.fragments";
 import { inferAccessTypes } from "@/components/_modal/pages/edition/utils";
 import { useMemo } from "react";
 
@@ -34,9 +35,19 @@ export default function useOrderPageInformation(workId, pid, periodicaForm) {
     return inferAccessTypes(workData?.work, periodicaForm, pickupBranch);
   }, [workData?.work, periodicaForm, pickupBranch]);
 
+  const blockedUserResponse = useData(
+    loanerInfo?.pickupBranch &&
+      branchesFragments.checkBlockedUser({
+        branchId: loanerInfo.pickupBranch,
+      })
+  );
+
+  const { isLoading: isBlockedUserLoading } = blockedUserResponse;
+
   const isLoadingBranches =
     isWorkLoading ||
     isPickupBranchLoading ||
+    isBlockedUserLoading ||
     (pickupBranchUser?.name && !pickupBranchUser?.agency);
 
   return {
@@ -62,6 +73,7 @@ export default function useOrderPageInformation(workId, pid, periodicaForm) {
       availableAsPhysicalCopy,
       requireDigitalAccess,
     },
+    blockedUserResponse,
     workResponse,
   };
 }
