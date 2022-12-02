efbfa02f322c8af62a96a7549de88bd09131c203
Anders Wulff Kringelbach <akri@dbc.dk>
2022-12-02 10:17:12 +0100

    Update another jest test

--- src/components/help/faq/__test__/groupSortData.test.js
+++ src/components/help/faq/__test__/groupSortData.test.js
@@ -1,5 +1,5 @@
 import { groupSortData } from "../utils";
-import data from "./mock/group.mock";
+import data from "./__fixtures__/group.mock";
 
 test("Group and sort Data", () => {
   const actual = groupSortData(data.faq.entities);
