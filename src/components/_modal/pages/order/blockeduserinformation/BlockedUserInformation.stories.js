905db6ee29475aa6ed685c422314625a221a206e
Anders Wulff Kringelbach <akri@dbc.dk>
2022-12-08 13:12:11 +0100

    Update tests

--- src/components/_modal/pages/order/blockeduserinformation/BlockedUserInformation.stories.js
+++ src/components/_modal/pages/order/blockeduserinformation/BlockedUserInformation.stories.js
@@ -24,7 +24,7 @@ export function BlockedUserKK() {
     />
   );
 }
-export function BlockedUserNoBranchOrAgencyUrl() {
+export function BlockedUserNoUrl() {
   return (
     <NamedBlockedUserInformation
       agencyName={"BalleRipRapRups biblioteker"}
