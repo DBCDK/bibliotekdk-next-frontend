824e0e12f779f19c24186398fed6cee4313c9c45
Anders Wulff Kringelbach <akri@dbc.dk>
2022-12-14 17:20:52 +0100

    Update to use accessFactory

---
+++ src/components/_modal/pages/options/Options.helper.js
@@ -0,0 +1,97 @@
+import Translate from "@/components/base/translate";
+import { AccessEnum } from "@/lib/enums";
+
+export const getTemplateProps = {
+  [AccessEnum.ACCESS_URL](props) {
+    return templateProps?.propsForOnline?.(props);
+  },
+  [AccessEnum.INFOMEDIA_SERVICE](props) {
+    return templateProps?.propsForInfomedia?.(props);
+  },
+  [AccessEnum.DIGITAL_ARTICLE_SERVICE](props) {
+    return templateProps?.propsForOnlineOrderLink?.(props);
+  },
+  [AccessEnum.EREOL](props) {
+    return templateProps?.propsForOnline?.(props);
+  },
+  [AccessEnum.INTER_LIBRARY_LOAN](props) {
+    return templateProps?.propsForPhysicalOrderLink?.(props);
+  },
+};
+
+const templateProps = {
+  propsForOnline(props) {
+    return {
+      linkProps: { href: props?.url, target: "_blank" },
+      linkText: Translate({
+        context: "options",
+        label: "online-link-title",
+        vars: [props?.materialType?.toLowerCase()],
+      }),
+      descriptionText: Translate({
+        context: "options",
+        label: "online-link-description",
+        vars: [props?.origin],
+      }),
+    };
+  },
+  propsForInfomedia(props) {
+    return {
+      linkProps: { href: props?.url, target: "_self" },
+      linkText: Translate({
+        context: "options",
+        label: "infomedia-link-title",
+      }),
+      descriptionText: Translate({
+        context: "options",
+        label: "infomedia-link-description",
+        vars: ["infomedia"],
+      }),
+    };
+  },
+  propsForOnlineOrderLink(props) {
+    return {
+      linkProps: {
+        onClick: () => {
+          props?.onOrder();
+        },
+      },
+      linkText: Translate({
+        context: "options",
+        label: "digital-copy-link-title",
+      }),
+      descriptionText: Translate({
+        context: "options",
+        label: "digital-copy-link-description",
+      }),
+    };
+  },
+  propsForPhysicalOrderLink(props) {
+    return {
+      linkProps: {
+        onClick: () => {
+          props?.onOrder();
+        },
+      },
+      linkText: Translate({
+        context: "options",
+        label: "order-physical-copy",
+      }),
+      descriptionText: Translate({
+        context: "options",
+        label: "order-physical-copy-description",
+      }),
+    };
+  },
+};
+
+// quickfix dfi is not a 'real' url - @TODO do a proper fix
+export function specialSort(a, b) {
+  // fjernleje should be on top
+  if (b.url && b.url.indexOf("dfi.dk") !== -1) {
+    return -1;
+  } else if (a.url && a.url.indexOf("dfi.dk") !== -1) {
+    return 1;
+  }
+  return 0;
+}
--- src/components/_modal/pages/options/Options.page.js
+++ src/components/_modal/pages/options/Options.page.js
@@ -1,112 +1,38 @@
-// templates
-import Infomedia from "./templates/infomedia";
-import Online from "./templates/online";
-import WebArchive from "./templates/webarchive";
-import OrderLink from "./templates/orderlink";
-
 import styles from "./Options.module.css";
-import sortBy from "lodash/sortBy";
 import Top from "../base/top";
+import { openOrderModal } from "@/components/work/utils";
+import {
+  specialSort,
+  getTemplateProps,
+} from "@/components/_modal/pages/options/Options.helper";
+import Link from "@/components/base/link";
+import Text from "@/components/base/text";
 
-/**
- * Template selection function
- *
- * @param {object} props props for template
- *
- * @returns {JSX.Element}
- */
-function getTemplate(props) {
-  if (props.accessType === "webArchive") {
-    return <WebArchive {...props} key={props.listkey} />;
-  }
-  if (props.accessType === "infomedia") {
-    return <Infomedia props={props} />;
-  }
-  if (props.accessType === "online") {
-    return <Online props={props} key={props.listkey} />;
-  }
-  if (props.accessType === "digitalCopy") {
-    return <OrderLink props={props} key={props.listkey} digitalOnly />;
-  }
-  if (props.accessType === "physical") {
-    return <OrderLink props={props} key={props.listkey} />;
-  }
-  if (props.accessType === "combined") {
-    return <OrderLink props={props} key={props.listkey} combined />;
-  }
-}
+export function OptionsLinkAndDescription({ props, templateProps }) {
+  const { note, className } = { ...props };
 
-function sortorder(onlineaccess) {
-  const realorder = [
-    "online",
-    "webArchive",
-    "infomedia",
-    "digitalCopy",
-    "physical",
-    "combined",
-  ];
-  return sortBy(onlineaccess, function (item) {
-    return realorder.indexOf(item.accessType);
-  });
-}
-
-/**
- * Enrich online access object with additinal info
- * @param onlineAccess Array of onlineaccess objects
- * @param orderPossible Is it possible to order a physical copy?
- * @return {*}
- */
-function addToOnlineAccess(onlineAccess = [], orderPossible) {
-  let addi = onlineAccess?.map((access) => {
-    const copy = { ...access };
-
-    if (copy.__typename === "InfomediaService") {
-      copy.accessType = "infomedia";
-    } else if (copy.issn) {
-      // We combine physical and digital into single entry
-      copy.accessType = orderPossible ? "combined" : "digitalCopy";
-      // TODO: Hvad hedder denne nu?
-      // } else if (copy.type === "webArchive") {
-      //   copy.accessType = "webArchive";
-    } else if (copy.url) {
-      copy.accessType = "online";
-    }
-
-    return copy;
-  });
-  if (
-    orderPossible &&
-    !addi.find((access) => access.accessType === "combined")
-  ) {
-    addi.push({ accessType: "physical" });
-  }
-
-  return addi;
-}
-
-// quickfix dfi is not a 'real' url - @TODO do a proper fix
-function specialSort(a, b) {
-  // fjernleje should be on top
-  if (b.url && b.url.indexOf("dfi.dk") !== -1) {
-    return -1;
-  } else if (a.url && a.url.indexOf("dfi.dk") !== -1) {
-    return 1;
-  }
-  return 0;
+  return (
+    <li className={`${className} ${styles.item}`}>
+      <Link
+        border={{ bottom: { keepVisible: true } }}
+        {...templateProps.linkProps}
+      >
+        <Text type="text1">{templateProps.linkText}</Text>
+      </Link>
+      {note && <Text type="text3">{note}</Text>}
+      <Text type="text3">{templateProps.descriptionText}</Text>
+    </li>
+  );
 }
 
 export function Options({ modal, context }) {
-  const { onlineAccess } = { ...context };
+  const { allowedAccesses } = { ...context };
 
   // no type selected - get the first one
   const type = context.type;
 
-  const addiOnlineAccess = addToOnlineAccess(
-    onlineAccess,
-    context.orderPossible
-  );
   // quickfix - sort links from filmstriben - we want fjernleje on top
-  const orderedOnlineAccess = sortorder(addiOnlineAccess).sort(specialSort);
+  const orderedOnlineAccess = allowedAccesses?.sort(specialSort);
 
   return (
     orderedOnlineAccess && (
@@ -114,15 +40,24 @@ export function Options({ modal, context }) {
         <Top title={context.title} />
         <ul className={styles.list} key="options-ul">
           {orderedOnlineAccess.map((access, index) => {
-            return getTemplate({
+            const props = {
               ...access,
-              materialType: type,
-              title_author: context.title_author,
               className: styles.item,
-              workId: context.workId,
-              modal: modal,
-              listkey: access.accessType + "-" + index,
-            });
+              materialType: type,
+              onOrder: () =>
+                openOrderModal(modal, context.workId, "singleManifestation", {
+                  pid: access?.pid,
+                  materialTypes: { specific: type },
+                }),
+            };
+
+            return (
+              <OptionsLinkAndDescription
+                key={access.accessType + "-" + index}
+                props={props}
+                templateProps={getTemplateProps[access?.__typename](props)}
+              />
+            );
           })}
         </ul>
       </div>
