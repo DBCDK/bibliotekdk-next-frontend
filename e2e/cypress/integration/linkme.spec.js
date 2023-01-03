777673212fbff6be38660f047ae2e3297bf36c75
pjo <pjo@dbc.dk>
2023-01-03 08:38:20 +0100

    some testing

---
+++ e2e/cypress/integration/linkme.spec.js
@@ -0,0 +1,43 @@
+const nextjsBaseUrl = Cypress.env("nextjsBaseUrl");
+
+function getPage(path = "/") {
+  return cy.request({
+    url: `${nextjsBaseUrl}${path}`,
+    // bot
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+    },
+  });
+}
+describe(`linkme`, () => {
+  it(`redirects on no rec.id`, () => {
+    const path = "/linkme.php?isBot=true";
+    const url = `${nextjsBaseUrl}${path}`;
+    cy.visit(url);
+    // we expect a redirect
+    cy.contains("Siden blev ikke fundet");
+  });
+
+  it("redirect on correct rec.id", () => {
+    const path = "/linkme.php?rec.id=874310-katalog%3ADBB0422141";
+    const url = `${nextjsBaseUrl}${path}`;
+    cy.visit(url);
+    // we expect a redirect
+    cy.contains("Grum : Roman fra Sø og Mose");
+  });
+
+  it("serverside redirect", () => {
+    const path = "/linkme.php?rec.id=874310-katalog%3ADBB0422141&isBot=true";
+    getPage(path).then((res) => {
+      console.log(res);
+      const jsonld = JSON.parse(
+        res.body.match(
+          /<script type="application\/ld\+json">(.*?)<\/script>/
+        )?.[1]
+      );
+
+      expect(jsonld.mainEntity.name).to.equal("Grum");
+    });
+  });
+});
