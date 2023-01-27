de6983e4aa8b8728abba919894584211306cdb43
pjo <pjo@dbc.dk>
2023-01-27 13:33:35 +0100

    fixed a test - relatedKeywords

--- src/components/work/related/Related.stories.js
+++ src/components/work/related/Related.stories.js
@@ -1,5 +1,6 @@
 import { StoryTitle, StoryDescription } from "@/storybook";
 import WrappedRelated, { Related } from "./Related";
+import { WrappedDetailsSection } from "@/components/work/details/Details.stories";
 
 const exportedObject = {
   title: "work/RelatedSubjects",
@@ -48,10 +49,40 @@ export function Connected() {
 
 Connected.story = {
   parameters: {
-    nextRouter: {
-      showInfo: true,
-      pathname: "/materiale",
-      query: { workId: "work-of:870970-basis:51701763" },
+    graphql: {
+      debug: true,
+      resolvers: {
+        Subject: {
+          __resolveType: () => "SubjectText",
+        },
+        Work: {
+          subjects: () => ({
+            dbcVerified: [
+              {
+                display: "savn",
+                __typename: "SubjectText",
+                language: {
+                  display: "dansk",
+                  isoCode: "dan",
+                },
+              },
+              {
+                display: "melankoli",
+                __typename: "SubjectText",
+                language: {
+                  display: "dansk",
+                  isoCode: "dan",
+                },
+              },
+            ],
+          }),
+        },
+      },
     },
+  },
+  nextRouter: {
+    showInfo: true,
+    pathname: "/",
+    query: {},
   },
 };
