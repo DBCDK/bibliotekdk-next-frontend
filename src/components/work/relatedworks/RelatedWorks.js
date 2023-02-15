3b168608abdb32da8f2ba9f7b96fe136771d4edc
Anders Wulff Kringelbach <akri@dbc.dk>
2023-02-15 19:43:55 +0100

    Add RelatedWorks

---
+++ src/components/work/relatedworks/RelatedWorks.js
@@ -0,0 +1,42 @@
+import Section from "@/components/base/section";
+import Translate from "@/components/base/translate";
+import SingleRelatedManifestation from "@/components/work/relatedworks/singlerelatedwork/SingleRelatedManifestation";
+import { useData } from "@/lib/api/api";
+import { workForWorkRelationsWorkTypeFactory } from "@/lib/api/work.fragments";
+import { workRelationsWorkTypeFactory } from "@/lib/workRelationsWorkTypeFactoryUtils";
+import ScrollSnapSlider from "@/components/base/scrollsnapslider/ScrollSnapSlider";
+function RelatedWorks({ relations, className, subtitle }) {
+  const context = { context: "relatedworks" };
+  const sliderId = "relatedWorks_slide";
+
+  const relatedWorks = relations?.map((manifestation, index) => (
+    <SingleRelatedManifestation key={index} manifestation={manifestation} />
+  ));
+
+  if (relations?.length === 0) {
+    return null;
+  }
+
+  return (
+    <Section
+      title={Translate({ ...context, label: "title" })}
+      space={{ top: "var(--pt8)", bottom: "var(--pt4)" }}
+      className={`${className}`}
+      subtitle={subtitle}
+    >
+      <ScrollSnapSlider sliderId={sliderId}>{relatedWorks}</ScrollSnapSlider>
+    </Section>
+  );
+}
+
+export default function Wrap({ workId, subtitle = "HEJ" }) {
+  const workForWorkRelationsWorkTypeFactory_response = useData(
+    workId && workForWorkRelationsWorkTypeFactory({ workId: workId })
+  );
+
+  const { flatRelations } = workRelationsWorkTypeFactory(
+    workForWorkRelationsWorkTypeFactory_response?.data?.work
+  );
+
+  return <RelatedWorks relations={flatRelations} subtitle={subtitle} />;
+}
