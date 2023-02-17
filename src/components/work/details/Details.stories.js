d83d2fe9723b527f86aed4c0288c20a2e42615f1
pjo <pjo@dbc.dk>
2023-02-17 14:53:11 +0100

    better storybook for details

--- src/components/work/details/Details.stories.js
+++ src/components/work/details/Details.stories.js
@@ -32,6 +32,7 @@ WrappedDetailsSection.story = {
       resolvers: {
         Work: {
           genreAndForm: () => ["actionfilm", "thriller", "science fiction"],
+          workTypes: () => ["MOVIE"],
         },
 
         Manifestation: {
@@ -41,7 +42,69 @@ WrappedDetailsSection.story = {
               loanIsPossible: true,
             },
           ],
+          creators: () => [
+            {
+              display: "William Steig",
+              roles: [],
+            },
+          ],
+          contributors: () => [],
+          audience: () => ({
+            generalAudience: [
+              "Mærkning: Tilladt for alle men frarådes børn under 7 år",
+            ],
+          }),
+          languages: () => ({
+            spoken: [
+              {
+                display: "dansk",
+              },
+              {
+                display: "norsk",
+              },
+              {
+                display: "finsk",
+              },
+              {
+                display: "svensk",
+              },
+            ],
+            subtitles: [
+              {
+                display: "dansk",
+              },
+              {
+                display: "engelsk",
+              },
+              {
+                display: "norsk",
+              },
+              {
+                display: "finsk",
+              },
+              {
+                display: "svensk",
+              },
+            ],
+            main: [
+              {
+                display: "engelsk",
+              },
+            ],
+          }),
+
+          physicalDescriptions: () => [
+            {
+              summary: "1 dvd-video ca. 50 min.",
+            },
+          ],
+
           materialTypes: () => [{ specific: "Musik (dvd)" }],
+          edition: () => ({
+            publicationYear: {
+              display: "2011",
+            },
+          }),
         },
       },
     },
