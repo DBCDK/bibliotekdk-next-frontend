1f81768b8e824989053390105d2bb752e697e86d
Anders Wulff Kringelbach <akri@dbc.dk>
2023-02-27 17:36:36 +0100

    Test on titles instead of element

--- e2e/cypress/e2e/old/work.cy.js
+++ e2e/cypress/e2e/old/work.cy.js
@@ -11,11 +11,11 @@ describe("Work", () => {
     // the actual html returned from the server
     // set isBot=true to make sure data is loaded on server
     cy.request(
-      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar/work-of:870970-basis:22669885?type=Bog&isBot=true`
+      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar/work-of:870970-basis:22669885?type=bog&isBot=true`
     )
       .its("body")
       .then((html) => {
-        expect(html).to.have.string("1950 High Noon</h1");
+        expect(html).to.have.string("1950 High Noon");
       });
   });
 
@@ -23,11 +23,11 @@ describe("Work", () => {
     // we make a request instead of visit to see
     // the actual html returned from the server
     cy.request(
-      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar-staalesen/work-of%3A870970-basis%3A22669885?type=Bog`
+      `${nextjsBaseUrl}/materiale/1950-high-noon_gunnar-staalesen/work-of%3A870970-basis%3A22669885?type=bog`
     )
       .its("body")
       .then((html) => {
-        expect(html).to.not.have.string("1950 High Noon</h1");
+        expect(html).to.not.have.string("1950 High Noon");
       });
   });
 });
