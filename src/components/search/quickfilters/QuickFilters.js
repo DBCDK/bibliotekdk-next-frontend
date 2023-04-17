155ccc01a98a5b4a33dffe10401d27a45b0c92ae
akri <91877805+akri-dbcdk@users.noreply.github.com>
2023-04-17 11:08:00 +0200

    Features 1406 3 3 mar 22 23 (#648)
    * Small changes
    
    * Bigger changes
    
    * Quick filter changes unless delete

--- src/components/search/quickfilters/QuickFilters.js
+++ src/components/search/quickfilters/QuickFilters.js
@@ -1,12 +1,9 @@
 import PropTypes from "prop-types";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 
 import useFilters from "@/components/hooks/useFilters";
 
 import Text from "@/components/base/text";
-import Link from "@/components/base/link";
+import Link, { LinkOnlyInternalAnimations } from "@/components/base/link";
 import Icon from "@/components/base/icon";
 import Translate from "@/components/base/translate";
 
@@ -24,49 +21,48 @@ export function QuickFilters({ onFiltersClick }) {
   const countFilters = getFiltersCount([FilterTypeEnum.WORK_TYPES]).toString();
 
   return (
-    <div className={styles.section}>
-      <Container className={styles.fullwidth} fluid>
-        <Row>
-          <div className={styles.quickfilters}>
-            <Col xs={{ span: 9, offset: 3 }}>
-              <div>
-                <div className={styles.links}>
-                  <Link
-                    tabIndex="-1"
-                    className={styles.link}
-                    onClick={() => onFiltersClick()}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter" || e.keyCode === 13) {
-                        onFiltersClick();
-                      }
-                    }}
-                    border={false}
-                  >
-                    <Icon src="settings.svg" size={2} />
-                    <Link
-                      dataCy="view-all-filters"
-                      onClick={(e) => e.preventDefault()}
-                      border={{ bottom: { keepVisible: true } }}
-                    >
-                      <Text type="text3">
-                        {Translate({
-                          context: "search",
-                          label:
-                            countFilters === "0"
-                              ? "showAllFilters"
-                              : "showAllFiltersCount",
-                          vars: countFilters === "0" ? null : [countFilters],
-                        })}
-                      </Text>
-                    </Link>
-                  </Link>
-                </div>
-              </div>
-            </Col>
-          </div>
-        </Row>
-      </Container>
-    </div>
+    <LinkOnlyInternalAnimations
+      tabIndex="-1"
+      className={`${styles.section}`}
+      onClick={() => onFiltersClick()}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.keyCode === 13) {
+          onFiltersClick();
+        }
+      }}
+      border={false}
+    >
+      <div className={styles.link}>
+        {/*<div className={styles.section}>*/}
+        {/*<Container className={styles.fullwidth} fluid>*/}
+        {/*  <Row>*/}
+        {/*    <div className={styles.quickfilters}>*/}
+        {/*      <Col xs={{ span: 9, offset: 3 }}>*/}
+        {/*        <div>*/}
+        {/*<div className={styles.links}>*/}
+        <Icon src="settings.svg" size={2} />
+        <Link
+          dataCy="view-all-filters"
+          border={{ bottom: { keepVisible: true } }}
+        >
+          <Text type="text3" tag="span">
+            {Translate({
+              context: "search",
+              label:
+                countFilters === "0" ? "showAllFilters" : "showAllFiltersCount",
+              vars: countFilters === "0" ? null : [countFilters],
+            })}
+          </Text>
+        </Link>
+        {/*</div>*/}
+        {/*        </div>*/}
+        {/*      </Col>*/}
+        {/*    </div>*/}
+        {/*  </Row>*/}
+        {/*</Container>*/}
+        {/*</div>*/}
+      </div>
+    </LinkOnlyInternalAnimations>
   );
 }
 
