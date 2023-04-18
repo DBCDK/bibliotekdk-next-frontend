d9839971d77b1f31a7a7376254f70597b57b3dca
pjohans <pjohans@gmail.com>
2023-04-18 14:05:09 +0200

    css floating columns (#676)
    * css floating columns
    
    * removed unused var
    
    ---------
    
    Co-authored-by: pjo <pjo@dbc.dk>

--- src/components/work/details/Details.js
+++ src/components/work/details/Details.js
@@ -59,28 +59,32 @@ function Details({ className = "", manifestation = {}, work = {} }) {
       subtitle={subtitle}
     >
       <Row className={`${styles.details}`}>
-        {fieldsToShow &&
-          fieldsToShow.map((field, index) => {
-            const fieldName = Object.keys(field)[0];
-            return (
-              !isEmpty(field[fieldName].value) && (
-                /** this is the label **/
-                <Col xs={6} md={3} key={`details__${fieldName}_${index}`}>
-                  <Text type="text3" className={styles.title} lines={2}>
-                    {field[fieldName].label}
-                  </Text>
-                  {/** some fields has a custom jsx parser .. **/}
-                  {field[fieldName].jsxParser ? (
-                    field[fieldName].jsxParser({
-                      values: field[fieldName].value,
-                    })
-                  ) : (
-                    <DefaultDetailValues values={field[fieldName].value} />
-                  )}
-                </Col>
-              )
-            );
-          })}
+        <Col xs={12}>
+          <div className={styles.container}>
+            {fieldsToShow &&
+              fieldsToShow.map((field, index) => {
+                const fieldName = Object.keys(field)[0];
+                return (
+                  !isEmpty(field[fieldName].value) && (
+                    /** this is the label **/
+                    <div className={styles.item} key={index}>
+                      <Text type="text3" className={styles.title} lines={2}>
+                        {field[fieldName].label}
+                      </Text>
+                      {/** some fields has a custom jsx parser .. **/}
+                      {field[fieldName].jsxParser ? (
+                        field[fieldName].jsxParser({
+                          values: field[fieldName].value,
+                        })
+                      ) : (
+                        <DefaultDetailValues values={field[fieldName].value} />
+                      )}
+                    </div>
+                  )
+                );
+              })}
+          </div>
+        </Col>
       </Row>
     </Section>
   );
