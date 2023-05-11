41b7d1853b900b69f246c4a14611f3b8451dac06
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-05-11 15:47:27 +0200

    layout spacing

--- src/components/profile/Layout/Layout.js
+++ src/components/profile/Layout/Layout.js
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Title } from "@/components/base/title/Title";
-
+import styles from "./Layout.module.css";
 /**
  * Layout to use in /profil subpages
  * Renders a side menu on left side and children on right side
@@ -11,26 +11,20 @@ import { Title } from "@/components/base/title/Title";
  */
 export default function Layout({ title, children }) {
   return (
-    <Container fluid>
+    <Container fluid className={styles.container}>
       <Row>
         <Col md={3}>
           {/**replace with side menu: */}
-
           <div
             style={{ width: "100%", height: "300px", backgroundColor: "coral" }}
           />
         </Col>
-        <Col lg={6} md={9}>
-          <Title type="title3">{title}</Title>
+        <Col md={9}>
+          {/**page content here */}
+          <Title className={styles.title} type="title2">
+            {title}
+          </Title>
           {children}
-          {/**insert page content here */}
-          <div
-            style={{
-              width: "100%",
-              height: "300px",
-              backgroundColor: "burlywood",
-            }}
-          />
         </Col>
       </Row>
     </Container>
