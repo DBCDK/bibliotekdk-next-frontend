c06fd9174aba05f1cf3cb24bfea01f0a851b2a4d
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-05-17 10:16:14 +0200

    remove comment

--- src/components/base/iconButton/IconButton.js
+++ src/components/base/iconButton/IconButton.js
@@ -4,10 +4,6 @@ import Text from "@/components/base/text";
 import Icon from "@/components/base/icon";
 import animations from "@/components/base/animation/animations.module.css";
 
-/**
- * For now can only be used with Close-icon. Can be expanded if other icons are needed.
- *
- */
 function IconButton({
   className,
   onClick,
