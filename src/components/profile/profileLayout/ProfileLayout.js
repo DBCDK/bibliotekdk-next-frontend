312acf96b88d8577718acc7ea40a8fb972d18078
kiscdbc <97035888+kiscdbc@users.noreply.github.com>
2023-05-31 09:17:20 +0200

    Feature bibdk2021 1543 mobile profilemenu (#720)
    * wip
    
    * dropdown working
    
    * stop flashing using useIsMobile
    
    * remove blue link on click
    
    * remove undreline in link
    
    * remove blue on focus/hover on link
    
    * close menu onclick on same element
    
    * clean up
    
    * padding fix
    
    * delelete useIsMobile hook and use windowSize instead
    
    * more space for nav dropdown storybook component
    
    * cx instead of classnames / getTranslatedUrl / css instead of conditional render
    
    * adjust breakpoint add padding
    
    * added cy test
    
    * add reqlang to translate and encode & add getProfileUrl

--- src/components/profile/profileLayout/ProfileLayout.js
+++ src/components/profile/profileLayout/ProfileLayout.js
@@ -3,17 +3,23 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Title } from "@/components/base/title/Title";
 import styles from "./ProfileLayout.module.css";
-import ProfileMenu from "../profilemenu/ProfileMenu";
+import ProfileMenu from "../profilemenu/desktop/ProfileMenu";
+import NavigationDropdown from "@/components/base/dropdown/NavigationDropdown";
+
+const CONTEXT = "profile";
+const MENUITEMS = ["loansAndReservations", "myLibraries"];
 
 /**
  * ProfileLayout to use in /profil subpages
- * Renders a side menu on left side and children on right side
+ * Renders a side menu on left side (ProfileMenu) and children on right side for screen width above 1020px
+ * and renders a dropdown menu (NavigationDropdown) for screen width below 1020px
  *
  * @returns {JSX.Element}
  */
 export default function ProfileLayout({ title, children }) {
   return (
     <Container fluid className={styles.container}>
+      <NavigationDropdown context={CONTEXT} menuItems={MENUITEMS} />
       <Row>
         <Col md={3}>
           <ProfileMenu />
