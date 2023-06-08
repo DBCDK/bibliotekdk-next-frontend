abc97e7dc47c9ebf9d6ba5e3e1161002f6d65d11
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-06-08 13:37:48 +0200

    improved iconButton

--- src/components/base/iconButton/IconButton.js
+++ src/components/base/iconButton/IconButton.js
@@ -3,7 +3,7 @@ import styles from "./iconButton.module.css";
 import Text from "@/components/base/text";
 import Icon from "@/components/base/icon";
 import animations from "@/components/base/animation/animations.module.css";
-import AnimationLine from "@/components/base/animation/line";
+import Link from "@/components/base/link";
 
 /**
  * An animated button that contains a text and an Icon. Pass Icon name that matches an svg file inside public/icons
@@ -28,7 +28,7 @@ function IconButton({
 }) {
   return (
     <button
-      className={`${styles.close} ${animations["on-hover"]} ${animations["on-focus"]} ${className}`}
+      className={`${styles.container} ${animations["on-hover"]} ${animations["on-focus"]} ${className}`}
       onClick={() => onClick && onClick()}
       onKeyDown={(e) => {
         if (e.key === "Enter" || e.keyCode === 13) {
@@ -37,19 +37,19 @@ function IconButton({
       }}
       {...props}
     >
-      <div className={styles.textWrapper}>
+      <Link
+        className={`${animations["on-focus"]} ${styles.textWrapper} `}
+        onClick={(e) => e.preventDefault()}
+        border={{ bottom: { keepVisible: keepUnderline } }}
+        tag="div"
+      >
         <Text
           type={textType}
           className={`${textStyle} ${animations["f-border-bottom"]} ${animations["h-border-bottom"]}`}
         >
           {children}
         </Text>
-        <AnimationLine
-          keepVisible={keepUnderline}
-          className={styles.animationLine}
-        />
-      </div>
-
+      </Link>
       <Icon
         size={{ w: 2, h: "auto" }}
         className={`${styles.icon} ${animations["h-elastic"]} ${animations["f-elastic"]}`}
