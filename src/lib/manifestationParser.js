1d694ea08b793384b810e9adb22825b93349de47
akri <91877805+akri-dbcdk@users.noreply.github.com>
2023-06-21 10:32:30 +0200

    BIBDK2021-1605-vis-ikke-rollen-ophav-i-parentes-efter-navn  (#760)
    * Add ophav to Hidden role functions
    
    * Move parseFunction to centralParsers

--- src/lib/manifestationParser.js
+++ src/lib/manifestationParser.js
@@ -5,6 +5,8 @@ import Text from "@/components/base/text";
 import React, { useEffect, useState } from "react";
 import { FlatSubjectsForFullManifestation } from "@/components/work/keywords/Keywords";
 
+import { parseFunction } from "@/lib/centralParsers.utils";
+
 // fields to handle - add to handle a field eg. subjects or lix or let or ...
 const fields = () => [
   {
@@ -60,7 +62,9 @@ const fields = () => [
     }),
     valueParser: (creators) =>
       creators?.length === 1 && (
-        <ParsedCreatorsOrContributors creatorsOrContributors={creators} />
+        <ParsedAndRenderedCreatorsOrContributors
+          creatorsOrContributors={creators}
+        />
       ),
   },
   {
@@ -71,7 +75,9 @@ const fields = () => [
     }),
     valueParser: (creators) =>
       creators?.length > 1 && (
-        <ParsedCreatorsOrContributors creatorsOrContributors={creators} />
+        <ParsedAndRenderedCreatorsOrContributors
+          creatorsOrContributors={creators}
+        />
       ),
   },
   {
@@ -82,7 +88,9 @@ const fields = () => [
     }),
     valueParser: (contributors) =>
       contributors?.length > 0 && (
-        <ParsedCreatorsOrContributors creatorsOrContributors={contributors} />
+        <ParsedAndRenderedCreatorsOrContributors
+          creatorsOrContributors={contributors}
+        />
       ),
   },
   {
@@ -218,7 +226,7 @@ const fields = () => [
       label: "usedLanguage",
     }),
     valueParser: (languages) =>
-      <ParsedLanguages languages={languages} /> || null,
+      <ParsedAndRenderedLanguages languages={languages} /> || null,
   },
   {
     dataField: "edition",
@@ -424,7 +432,7 @@ export function parseManifestation(manifestation) {
   );
 }
 
-export function ParsedCreatorsOrContributors({
+export function ParsedAndRenderedCreatorsOrContributors({
   creatorsOrContributors,
   Tag = ManifestationLink,
 }) {
@@ -440,10 +448,7 @@ export function ParsedCreatorsOrContributors({
   return creatorsOrContributors?.map((C, idx) => (
     <Text tag={"div"} key={`${C?.display}${idx}`}>
       <Tag>{C?.display}</Tag>
-      {C?.roles?.length > 0 &&
-        ` (${C?.roles
-          ?.map((role) => role?.["function"]?.singular)
-          .join(", ")})`}
+      {parseFunction(C)}
       <br />
     </Text>
   ));
@@ -460,7 +465,7 @@ function ManifestationLink({ children }) {
   );
 }
 
-function ParsedLanguages({ languages }) {
+function ParsedAndRenderedLanguages({ languages }) {
   const languagesNotesExist = languages?.notes?.length > 0;
 
   const languagesExist =
