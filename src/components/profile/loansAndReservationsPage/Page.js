e907f37b6a1803d8b9898c33074b54bf5b1134be
AoschkA <jonaspraem@gmail.com>
2023-06-28 13:38:35 +0200

    Update Page.js

--- src/components/profile/loansAndReservationsPage/Page.js
+++ src/components/profile/loansAndReservationsPage/Page.js
@@ -73,46 +73,6 @@ export const dataReducer = (dataType, data) => {
   }
 };
 
-const sortLoans = (loans) => {
-  if (!loans) {
-    return loans;
-  }
-
-  loans.sort((a, b) => {
-    const dateA = new Date(a.dueDate);
-    const dateB = new Date(b.dueDate);
-    return dateA - dateB;
-  });
-
-  return loans;
-};
-
-const sortOrders = (orders) => {
-  if (!orders) {
-    return orders;
-  }
-
-  orders.sort((a, b) => {
-    if (a.pickUpExpiryDate === null && b.pickUpExpiryDate === null) {
-      // If both items have null pickUpExpiryDate, sort based on holdQueuePosition
-      return Number(a.holdQueuePosition) - Number(b.holdQueuePosition);
-    } else if (a.pickUpExpiryDate === null) {
-      // If only item a has null pickUpExpiryDate, put it after item b
-      return 1;
-    } else if (b.pickUpExpiryDate === null) {
-      // If only item b has null pickUpExpiryDate, put it after item a
-      return -1;
-    } else {
-      // Both items have pickUpExpiryDate set, sort based on the date
-      const dateA = new Date(a.pickUpExpiryDate);
-      const dateB = new Date(b.pickUpExpiryDate);
-      return dateA - dateB;
-    }
-  });
-
-  return orders;
-};
-
 const LoansAndReservations = () => {
   const breakpoint = useBreakpoint();
   const isMobileSize =
