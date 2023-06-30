122d0dccbb80700b85489121976b6a78158c6748
AoschkA <jonaspraem@gmail.com>
2023-06-30 15:12:04 +0200

    comment

--- src/components/base/title/Title.js
+++ src/components/base/title/Title.js
@@ -35,7 +35,7 @@ Container.propTypes = {
     PropTypes.node,
   ]),
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  tag: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6"]).isRequired,
+  tag: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6"]).isRequired, // If not one of these - use Text
   type: PropTypes.oneOf([
     "title1",
     "title2",
