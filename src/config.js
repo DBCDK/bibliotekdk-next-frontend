5bd5dddad2a8ce29366888df06f209a7f545a156
kiscdbc <kisc@dbc.dk>
2023-07-04 09:10:30 +0200

    outcommented prod fbi-api

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ const config = {
   },
   fbi_api: {
     url:
-      "https://fbi-api.dbc.dk/SimpleSearch/graphql" ||
+      // "https://fbi-api.dbc.dk/SimpleSearch/graphql" ||
       process.env.NEXT_PUBLIC_FBI_API_URL ||
       "https://fbi-api-staging.k8s.dbc.dk/SimpleSearch/graphql",
     timeout: process.env.API_TIMEOUT_MS || 150,
