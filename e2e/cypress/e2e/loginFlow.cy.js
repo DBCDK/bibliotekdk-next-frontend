07dbf29171c69213b414056d519deff6eed22c81
kiscdbc <kisc@dbc.dk>
2023-07-31 14:45:54 +0200

    slight cypress tests adjustments

--- e2e/cypress/e2e/loginFlow.cy.js
+++ e2e/cypress/e2e/loginFlow.cy.js
@@ -7,18 +7,13 @@ describe(`Different ways to open login modal with library with borrowerCheck `, 
     cy.get("[data-cy=header-link-login]").should("be.visible").click();
     cy.get("[data-cy=pickup-search-input]").should("be.visible").type("Val");
     cy.contains("Valby Bibliotek").click();
-    cy.get("[data-cy=text-log-ind]").should("be.visible");
+    cy.get("[data-cy=button-log-ind]").should("be.visible");
   });
 
   it("Plain login from login button on profile page", () => {
     cy.visit(nextjsBaseUrl + "/profil");
     cy.consentAllowAll();
-    cy.get("[data-cy=button-log-ind]")
-      .filter((index, element) => {
-        const classes = element.className.split(" ");
-        return classes.some((cls) => cls.includes("primary")); //we can have two login buttons on the page, take one that contains "primary"
-      })
-      .first()
+    cy.get('[class^="ProfileLayout_loginButton__sdKEd"]') //we can have two login buttons on the page, take the one where classname starts with this
       .should("be.visible")
       .click();
     cy.get("[data-cy=text-log-ind]").should("be.visible");
@@ -33,7 +28,7 @@ describe(`Different ways to open login modal with library with borrowerCheck `, 
     cy.get("[data-cy=button-log-ind]")
       .filter((index, element) => {
         const classes = element.className.split(" ");
-        return classes.some((cls) => cls.includes("primary")); //we can have two login buttons on the page, take one that contains "primary"
+        return classes.some((cls) => cls.includes("primary")); //we can have two login buttons on the page, where class contains "primary"
       })
       .first()
       .should("be.visible")
@@ -62,7 +57,7 @@ describe(`Different ways to open login modal with library with borrowerCheck `, 
 });
 
 describe(`Different ways to open login modal with (FFU) library WITHOUT borrowerCheck`, () => {
-  it("Plain login from login button in header not supported", () => {
+  it("Plain login from login button in header not supported & back button leads back to pick up branch search", () => {
     cy.visit(nextjsBaseUrl);
     cy.consentAllowAll();
     cy.get("[data-cy=header-link-login]").should("be.visible").click();
@@ -73,15 +68,17 @@ describe(`Different ways to open login modal with (FFU) library WITHOUT borrower
     cy.get("[data-cy=text-log-ind-via-cbs-bibliotek-understøttes-ikke]").should(
       "be.visible"
     );
+    cy.get("[data-cy=button-tilbage]").should("be.visible").click();
+    cy.get("[data-cy=pickup-search-input]").should("be.visible");
   });
 
-  it("Plain login from login button on profile page not supported", () => {
+  it("Plain login from login button on profile page not supported & back button leads back to pick up branch search", () => {
     cy.visit(nextjsBaseUrl + "/profil");
     cy.consentAllowAll();
     cy.get("[data-cy=button-log-ind]")
       .filter((index, element) => {
         const classes = element.className.split(" ");
-        return classes.some((cls) => cls.includes("primary")); //we can have two login buttons on the page, take one that contains "primary"
+        return classes.some((cls) => cls.includes("primary")); //we can have two login buttons on the page, take one with classname contains "primary"
       })
       .first()
       .should("be.visible")
@@ -93,9 +90,11 @@ describe(`Different ways to open login modal with (FFU) library WITHOUT borrower
     cy.get("[data-cy=text-log-ind-via-cbs-bibliotek-understøttes-ikke]").should(
       "be.visible"
     );
+    cy.get("[data-cy=button-tilbage]").should("be.visible").click();
+    cy.get("[data-cy=pickup-search-input]").should("be.visible");
   });
 
-  it("Infomedia login not supported", () => {
+  it("Infomedia login not supported & back button leads back to pick up branch search", () => {
     cy.visit(
       nextjsBaseUrl +
         "/infomedia/politiken/work-of:870971-avis:87043134/Y4074532" //TODO could this article disappear? should i mock this here?
@@ -117,6 +116,8 @@ describe(`Different ways to open login modal with (FFU) library WITHOUT borrower
     cy.get("[data-cy=text-log-ind-via-cbs-bibliotek-understøttes-ikke]").should(
       "be.visible"
     );
+    cy.get("[data-cy=button-tilbage]").should("be.visible").click();
+    cy.get("[data-cy=pickup-search-input]").should("be.visible");
   });
 
   //TODO add bestil flow ORDER_PHYSICAL & DIGITAL_COPY -->  opens loaner form
