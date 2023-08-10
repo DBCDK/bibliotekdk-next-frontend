7c671aa0d98e62678d8def15e2b92f9c44766c9f
kiscdbc <kisc@dbc.dk>
2023-08-10 14:45:14 +0200

    show that modal is opened in top of storybook story

--- src/components/_modal/pages/login/Login.stories.js
+++ src/components/_modal/pages/login/Login.stories.js
@@ -1,6 +1,7 @@
 import { StoryTitle, StoryDescription } from "@/storybook";
 import Login from "./Login";
 import Translate from "@/components/base/translate";
+import { useModal } from "@/components/_modal";
 
 const exportedObject = {
   title: "modal/Login",
@@ -16,22 +17,25 @@ export function LoginOrder() {
   const context = {
     title: Translate({ context: "login", label: "login-to-order" }),
   };
+  const modal = useModal();
+
   return (
     <div style={{ height: "100vh" }}>
       <StoryTitle>Login Modal</StoryTitle>
       <StoryDescription>
-        Shows the login modal coming from order button
+        Shows the login modal coming from reservation button
       </StoryDescription>
 
-      <Login context={context} />
+      <Login context={context} modal={modal} />
     </div>
   );
 }
 
-export function LoginLogin() {
+export function LoginHeader() {
   const context = {
     title: Translate({ context: "header", label: "login" }),
   };
+  const modal = useModal();
   return (
     <div style={{ height: "100vh" }}>
       <StoryTitle>Login Modal</StoryTitle>
@@ -39,7 +43,39 @@ export function LoginLogin() {
         Shows the login modal coming from login button in header
       </StoryDescription>
 
-      <Login context={context} />
+      <Login context={context} modal={modal} />
     </div>
   );
 }
+
+LoginHeader.story = {
+  parameters: {
+    graphql: {
+      debug: true,
+      resolvers: {
+        Query: {
+          monitor: () => "some-monitor",
+        },
+        BranchResult: {
+          hitcount: () => "10",
+          agencyUrl: () => "some-agencyUrl",
+          result: () => [
+            {
+              name: "Test agency",
+            },
+            { name: "Test agency 2" },
+          ],
+        },
+        Highlight: {
+          key: () => "some-key",
+          value: () => "some-value",
+        },
+      },
+    },
+    nextRouter: {
+      showInfo: true,
+      pathname: "/",
+      query: {},
+    },
+  },
+};
