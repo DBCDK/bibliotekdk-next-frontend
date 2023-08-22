e5b5c33d509636fe203360ce4c2da47bc3b3ee1c
kiscdbc <kisc@dbc.dk>
2023-08-22 18:43:14 +0200

    remove console.log

--- src/components/work/reservationbutton/ReservationButton.js
+++ src/components/work/reservationbutton/ReservationButton.js
@@ -168,7 +168,6 @@ export const ReservationButton = ({
     (isOnlineTranslated || workTypeTranslated);
 
   async function handleOpenLoginAndOrderModal() {
-    console.log("INSIDE handleOpenLoginAndOrderModal");
     //add order modal to store, to be able to access when coming back from adgangsplatform/mitid?
     const orderModalProps = {
       pids: pids,
