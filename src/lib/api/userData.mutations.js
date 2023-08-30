99f18c29fda7d1b8fc16f9838e590e7b5506f084
Yousef <yousefmohsen@outlook.dk>
2023-08-30 11:29:41 +0200

    Bibdk2021 1769 persist data consent (#884)
    * orderhistory data consent modal
    
    * mutate
    
    * clean up
    
    * fixed translate

---
+++ src/lib/api/userData.mutations.js
@@ -0,0 +1,24 @@
+/**
+ * updates the users consent to storing their data in userData api for more than 30 days
+ *
+ * @param {object} params
+ * @param {boolean} params.persistUserData
+ */
+
+export function setPersistUserDataValue({ persistUserData, userDataMutation }) {
+  const q = {
+    query: `
+      mutation setPersistUserDataValue($persistUserData: Boolean!) {
+        setPersistUserDataValue(persistUserData: $persistUserData) {
+         success
+         errorMessage
+        }
+      } 
+      `,
+    variables: {
+      persistUserData,
+    },
+  };
+
+  userDataMutation.post(q);
+}
