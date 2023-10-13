45e1b1e98de47157b0e940a6c8ebfcef4d043b71
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-10-13 09:13:50 +0200

    fixed variable name

--- src/components/hooks/useBookmarks.js
+++ src/components/hooks/useBookmarks.js
@@ -8,7 +8,7 @@ import { useSession } from "next-auth/react";
 import useBreakpoint from "@/components/hooks/useBreakpoint";
 
 const KEY_NAME = "bookmarks";
-const itemsPerPage = 20;
+const ITEMS_PER_PAGE = 3;
 
 export const BookmarkSyncProvider = () => {
   const { syncCookieBookmarks } = useBookmarks();
@@ -49,8 +49,8 @@ const useBookmarksCore = ({ isMock = false, session }) => {
     isAuthenticated &&
       bookmarkFragments.fetchAll({
         sortBy,
-        limit: isMobile ? currentPage * itemsPerPage : itemsPerPage,
-        offset: isMobile ? 0 : (currentPage - 1) * itemsPerPage,
+        limit: isMobile ? currentPage * ITEMS_PER_PAGE : ITEMS_PER_PAGE,
+        offset: isMobile ? 0 : (currentPage - 1) * ITEMS_PER_PAGE,
       })
   );
   const bookmarkMutation = useMutate();
@@ -68,7 +68,7 @@ const useBookmarksCore = ({ isMock = false, session }) => {
     hitcount = localBookmarks?.length || 0;
   }
 
-  const totalPages = Math.ceil(hitcount / itemsPerPage);
+  const totalPages = Math.ceil(hitcount / ITEMS_PER_PAGE);
 
   const syncCookieBookmarks = async () => {
     if (!isAuthenticated) return; // Not authenticated
@@ -235,10 +235,10 @@ const useBookmarksCore = ({ isMock = false, session }) => {
    * Returns a of localbookmarks that corresponds to the current page of local bookmarks.
    */
   function currenPageBookmark(bookmarkToPaginate) {
-    const startIdx = isMobile ? 0 : (currentPage - 1) * itemsPerPage;
+    const startIdx = isMobile ? 0 : (currentPage - 1) * ITEMS_PER_PAGE;
     const endIdx = isMobile
-      ? startIdx + itemsPerPage * currentPage
-      : startIdx + itemsPerPage;
+      ? startIdx + ITEMS_PER_PAGE * currentPage
+      : startIdx + ITEMS_PER_PAGE;
     const currentPageBookmarks = bookmarkToPaginate.slice(startIdx, endIdx);
     return currentPageBookmarks;
   }
