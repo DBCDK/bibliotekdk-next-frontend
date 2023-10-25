18d129e8e0e88b1ffad124b418527888fedf5c0c
kiscdbc <kisc@dbc.dk>
2023-10-25 16:35:47 +0200

    fix

--- src/components/feedbacklink/FeedBackLink.js
+++ src/components/feedbacklink/FeedBackLink.js
@@ -4,22 +4,23 @@ import Translate from "@/components/base/translate";
 import Link from "@/components/base/link";
 
 export default function FeedbackLink() {
-  const translation = Translate({
-    context: "general",
-    label: "kundeserviceBibdk",
-  });
-
   return (
     <div data-cy="feedback-wrapper" className={styles.feedbackwrap}>
       <div data-cy="feedback-link-text">
         <Link
-          href={translation}
+          href={Translate({
+            context: "general",
+            label: "kundeserviceBibdk",
+          })}
           target="_blank"
           border={{ top: false, bottom: { keepVisible: true } }}
           dataCy="feedbacklink-to-kundeservice"
         >
           <Text tag="span" type="text3" className={styles.textrotate}>
-            {translation}
+            {Translate({
+              context: "feedback",
+              label: "feed_back_kunderservice_link_text",
+            })}
           </Text>
         </Link>
       </div>
