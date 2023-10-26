95b8937ced3c492afaf356554317bb9857383528
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-10-26 14:45:27 +0200

    onworkclick

--- src/pages/avanceret.js
+++ src/pages/avanceret.js
@@ -3,16 +3,43 @@ import { useRouter } from "next/router";
 import { fetchAll } from "@/lib/api/apiServerOnly";
 
 import Page from "@/components/search/complexSearch/ComplexSearch";
+import { Result } from "@/components/search/result/Result";
+import useQ from "@/components/hooks/useQ";
+import useDataCollect from "@/lib/useDataCollect";
+import useFilters from "@/components/hooks/useFilters";
+import { useRef } from "react";
+
 /**
  * Renders the OrderHistory component
  */
 export default function MyLibraries() {
   const router = useRouter();
+  const q = useQ().getQuery();
+  const filters = useFilters().getQuery();
+  const scrollRef = useRef();
+
+  const dataCollect = useDataCollect();
+
+  const { page = 1 } = router.query;
 
   return (
     <>
+      <div ref={scrollRef} />
       <Header router={router} hideSearchBar />
       <Page />
+
+      {q && (
+        <Result
+          page={parseInt(page, 10)}
+          onWorkClick={(index, work) => {
+            dataCollect.collectSearchWorkClick({
+              search_request: { q, filters },
+              search_query_hit: index + 1,
+              search_query_work: work.workId,
+            });
+          }}
+        />
+      )}
     </>
   );
 }
