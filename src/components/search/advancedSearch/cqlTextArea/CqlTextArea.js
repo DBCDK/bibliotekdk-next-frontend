f29382d36f623f49a0b771d448f7097fa1235a63
pjohans <pjohans@gmail.com>
2023-10-31 12:40:24 +0100

    added a simple component

---
+++ src/components/search/advancedSearch/cqlTextArea/CqlTextArea.js
@@ -0,0 +1,46 @@
+import styles from "./CqlTextArea.module.css";
+
+import React, { useRef, useState } from "react";
+
+import { cyKey } from "@/utils/trim";
+import Translate from "@/components/base/translate";
+
+import { useRouter } from "next/router";
+
+export function CqlTextArea() {
+  const router = useRouter();
+  const textAreaRef = useRef();
+  const doAdvancedSearch = () => {
+    const cql = textAreaRef.current.value;
+    const query = { cql: cql };
+
+    router.push({ pathname: router.pathname, query });
+  };
+
+  return (
+    <div>
+      <textarea
+        className={styles.input}
+        rows="4"
+        cols="20"
+        defaultValue="title=harry AND potter"
+        ref={textAreaRef}
+      />
+      {/*<div className={styles.buttoninline}>*/}
+      <div>
+        <button
+          className={styles.button}
+          type="submit"
+          data-cy={cyKey({
+            name: "searchbutton",
+            prefix: "advenced-search",
+          })}
+          onClick={() => doAdvancedSearch()}
+        >
+          <span>{Translate({ context: "header", label: "search" })}</span>
+          <div className={styles.fill} />
+        </button>
+      </div>
+    </div>
+  );
+}
