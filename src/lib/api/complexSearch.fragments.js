7955b209428db4cded381a2bffaccb24cc92ef7d
pjohans <pjohans@gmail.com>
2023-10-31 11:17:14 +0100

    added fragment for complex search

---
+++ src/lib/api/complexSearch.fragments.js
@@ -0,0 +1,104 @@
+/**
+ * Hitcount
+ *
+ * @param {string} q the query
+ * @param {Object} filters filters for searching
+ */
+export function doComplexSearchAll({ cql, offset, limit }) {
+  return {
+    // delay: 1000, // for debugging
+    query: `
+    query ComplexSearchAll($cql: String!, $offset: Int!, $limit: PaginationLimit!) {
+			complexSearch(cql: $cql) {
+				hitcount
+				errorMessage
+				works(offset: $offset, limit: $limit) {
+					workId
+          mainLanguages {
+            isoCode
+            display
+          }
+          workTypes
+          manifestations {
+            mostRelevant{
+              pid
+              ownerWork {
+                workTypes
+              }
+              cover {
+                detail
+                origin
+              }
+              materialTypes {
+                materialTypeGeneral {
+                  code
+                  display
+                }
+                materialTypeSpecific {
+                  code
+                  display
+                }
+              }
+              hostPublication {
+                title
+              }
+              publisher
+              edition {
+                summary
+                edition
+              }
+            }            
+          }
+          creators {
+            ... on Corporation {
+              __typename
+              display
+              nameSort
+              roles {
+                function {
+                  plural
+                  singular
+                }
+                functionCode
+              }
+            }
+            ... on Person {
+              __typename
+              display
+              nameSort
+              roles {
+                function {
+                  plural
+                  singular
+                }
+                functionCode
+              }
+            }
+          }
+          materialTypes {
+            materialTypeGeneral {
+              code
+              display
+            }
+            materialTypeSpecific {
+              code
+              display
+  }
+          }
+          fictionNonfiction {
+            display
+          }
+          genreAndForm
+          titles {
+            main
+            full
+            parallel
+            sort
+          }
+        }
+			}
+		}`,
+    variables: { cql, offset, limit },
+    slowThreshold: 3000,
+  };
+}
