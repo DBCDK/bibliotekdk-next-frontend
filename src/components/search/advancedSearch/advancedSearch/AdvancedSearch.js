8251e488e88d18aa78002caeeaa50de37b07d231
Yousef <yousefmohsen@outlook.dk>
2023-11-01 14:08:05 +0100

    switch between cql and input views (#1046)
    * swithch between cql and input views
    
    * comment
    
    * remove height

--- src/components/search/advancedSearch/advancedSearch/AdvancedSearch.js
+++ src/components/search/advancedSearch/advancedSearch/AdvancedSearch.js
@@ -3,6 +3,10 @@ import styles from "./AdvancedSearch.module.css";
 import Translate from "@/components/base/translate/Translate";
 import TextInputs from "../fieldInput/TextInputs";
 import { CqlTextArea } from "@/components/search/advancedSearch/cqlTextArea/CqlTextArea";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "@/components/base/link";
+import Text from "@/components/base/text";
 
 /**
  * Contains advanced search fields
@@ -10,14 +14,45 @@ import { CqlTextArea } from "@/components/search/advancedSearch/cqlTextArea/CqlT
  */
 
 export default function AdvancedSearch() {
+  const router = useRouter();
+  const { cql } = router.query;
   const workType = "all";
+  const [showCqlEditor, setShowCqlEditor] = useState(false);
+
+  useEffect(() => {
+    //show CQL editor if there is a cql param in the url
+    setShowCqlEditor(!!cql);
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Title type="title3" className={styles.title}>
-        {Translate({ context: "search", label: "advancedSearch" })}
-      </Title>
-      <TextInputs workType={workType} />
-      <CqlTextArea />
+      <div className={styles.topContainer}>
+        <Title type="title3" className={styles.title}>
+          {Translate({ context: "search", label: "advancedSearch" })}
+        </Title>
+        <div>
+          <Link
+            onClick={() => {
+              setShowCqlEditor(!showCqlEditor);
+            }}
+            border={{
+              top: false,
+              bottom: {
+                keepVisible: true,
+              },
+            }}
+          >
+            <Text type="text3" tag="span">
+              {Translate({
+                context: "search",
+                label: showCqlEditor ? "showInputFields" : "editInCqlEditor",
+              })}
+            </Text>
+          </Link>
+        </div>
+      </div>
+
+      {showCqlEditor ? <CqlTextArea /> : <TextInputs workType={workType} />}
     </div>
   );
 }
