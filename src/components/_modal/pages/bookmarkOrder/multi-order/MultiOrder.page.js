1248fb6697c1f6f86572bdf6760cd249a3c4a073
AoschkA <jonaspraem@gmail.com>
2023-11-02 14:48:39 +0100

    cleanup

--- src/components/_modal/pages/bookmarkOrder/multi-order/MultiOrder.page.js
+++ src/components/_modal/pages/bookmarkOrder/multi-order/MultiOrder.page.js
@@ -2,8 +2,6 @@ import Title from "@/components/base/title";
 import Top from "../../base/top";
 import styles from "./MultiOrder.module.css";
 import Translate from "@/components/base/translate";
-import { useData } from "@/lib/api/api";
-import * as branchesFragments from "@/lib/api/branches.fragments";
 import CheckoutForm from "./checkoutForm/MultiOrderCheckoutForm";
 import Material from "./Material/Material";
 import { useEffect, useRef, useState } from "react";
@@ -11,22 +9,6 @@ import { useModal } from "@/components/_modal/Modal";
 import { StatusEnum } from "@/components/base/materialcard/materialCard.utils";
 
 const CONTEXT = "bookmark-order";
-
-const OrderPolicyWrapper = ({ material, ...props }) => {
-  const pids =
-    material.pid ??
-    material.manifestations.mostRelevant.map((mani) => mani.pid); // TODO filter by material type
-  const { data: orderPolicyData, isLoading: orderPolicyIsLoading } = useData(
-    pids &&
-      pids.length > 0 &&
-      branchesFragments.checkOrderPolicy({ pids: pids, branchId: "726500" })
-  );
-  if (orderPolicyIsLoading) return null;
-
-  const { orderPossible } = orderPolicyData?.branches?.result?.[0]?.orderPolicy;
-
-  return <article data-order-possible={orderPossible} {...props} />;
-};
 
 const MultiOrder = ({ context }) => {
   const modal = useModal();
@@ -64,16 +46,6 @@ const MultiOrder = ({ context }) => {
     const materialsDigital = elements
       .filter(
         (element) => element.getAttribute("data-status") === StatusEnum.DIGITAL
-      )
-      .map((element) =>
-        materials.find(
-          (mat) => mat.key === element.getAttribute("data-material-key")
-        )
-      );
-
-    const materialsReadyToOrder = elements
-      .filter(
-        (element) => element.getAttribute("data-status") === StatusEnum.NONE
       )
       .map((element) =>
         materials.find(
