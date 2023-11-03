8652b7d939bf0576f5e722f964bc0ecd09e2e76a
kiscdbc <97035888+kiscdbc@users.noreply.github.com>
2023-11-03 15:06:12 +0100

    Renew delete loans and reservations (#1056)
    * delete working and shows error if not on mobile
    
    * renew loan fixed
    
    * added renew test
    
    * added desktop bestilling fejlede test

--- src/components/profile/materialRow/versions/MaterialRowLoan.js
+++ src/components/profile/materialRow/versions/MaterialRowLoan.js
@@ -149,6 +149,7 @@ const MaterialRowLoan = (props) => {
     library,
     dataCy,
     isMobileSize,
+    agencyId,
   } = props;
   const [hasRenewError, setHasRenewError] = useState(false);
   const modal = useModal();
@@ -198,7 +199,7 @@ const MaterialRowLoan = (props) => {
         onKeyDown={(e) => {
           e.key === "Enter" && onMobileItemClick();
         }}
-        data-cy={dataCy}
+        data-cy={`articleRow-${dataCy}`}
       >
         <div>{!!image && <Cover src={image} size="fill-width" />}</div>
         <div className={sharedStyles.textContainer}>
@@ -245,7 +246,7 @@ const MaterialRowLoan = (props) => {
           [sharedStyles.materialRow_red]: status === "RED",
         }
       )}
-      data-cy={dataCy}
+      data-cy={`articleRow-${dataCy}`}
     >
       <div className={sharedStyles.materialInfo}>
         {!!image && (
