73727b13c7b05fa5e238d68a676998717d01ff94
akri <akri@dbc.dk>
2023-11-07 12:01:29 +0100

    Comment in story

--- src/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown.stories.js
+++ src/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown.stories.js
@@ -90,31 +90,38 @@ export function AdvancedSearchDropdownCheckboxesAndRadioButtons() {
     </div>
   );
 }
-// export function AdvancedSearchDropdownCheckboxesAndDividerAndRadioButtons() {
-//   const indices = [
-//     { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
-//     { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
-//     { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
-//     { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
-//     { formType: FormTypeEnum.DIVIDER },
-//     { name: "giraf", value: "giraf", formType: FormTypeEnum.RADIO_BUTTON },
-//     { name: "løve", value: "løve", formType: FormTypeEnum.RADIO_BUTTON },
-//     { name: "elefant", value: "elefant", formType: FormTypeEnum.RADIO_BUTTON },
-//     { name: "søløve", value: "søløve", formType: FormTypeEnum.RADIO_BUTTON },
-//   ];
-//
-//   const storyTitle = "Checkboxes and divider and radio buttons";
-//
-//   return (
-//     <div>
-//       <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
-//       <StoryDescription>
-//         AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
-//       </StoryDescription>
-//       <AdvancedSearchDropdown indexName={storyTitle} menuItems={indices} />
-//     </div>
-//   );
-// }
+
+export function AdvancedSearchDropdownCheckboxesAndDividerAndRadioButtons() {
+  const indices = [
+    { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
+    { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
+    { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
+    { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
+    { formType: FormTypeEnum.DIVIDER },
+    { name: "giraf", value: "giraf", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "løve", value: "løve", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "elefant", value: "elefant", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "søløve", value: "søløve", formType: FormTypeEnum.RADIO_BUTTON },
+  ];
+
+  const updateIndex = () => {};
+
+  const storyTitle = "Checkboxes and divider and radio buttons";
+
+  return (
+    <div>
+      <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
+      <StoryDescription>
+        AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
+      </StoryDescription>
+      <AdvancedSearchDropdown
+        indexName={storyTitle}
+        menuItems={indices}
+        updateIndex={updateIndex}
+      />
+    </div>
+  );
+}
 
 export function AdvancedSearchDropdownRadioButtonsAndRadioLink() {
   const indices = [
