2a85ae4dbe8c0e66efe68bcf37dd10b0f26be2ca
akri <akri@dbc.dk>
2023-11-07 11:41:31 +0100

    Update advanced search dropdown

---
+++ src/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown.js
@@ -0,0 +1,115 @@
+import { useEffect, useReducer, useState } from "react";
+import List from "@/components/base/forms/list";
+import isEmpty from "lodash/isEmpty";
+import { DialogForPublicationYear } from "@/components/search/advancedSearch/advancedSearchHelpers/dialogForPublicationYear/DialogForPublicationYear";
+import {
+  CheckboxItem,
+  FormTypeEnum,
+  RadioButtonItem,
+  RadioLinkItem,
+  Toggler,
+} from "@/components/search/advancedSearch/advancedSearchHelpers/helperComponents/HelperComponents";
+import {
+  initializeMenuItem,
+  reducerForToggleMenuItemsState,
+} from "@/components/search/advancedSearch/advancedSearchHelpers/dropdownReducerFunctions";
+import styles from "./AdvancedSearchDropdown.module.css";
+
+export function useMenuItemsState(menuItems, updateIndex) {
+  const [menuItemsState, toggleMenuItemsState] = useReducer(
+    (currentMenuItems, itemUpdate) =>
+      reducerForToggleMenuItemsState({
+        currentMenuItems: currentMenuItems,
+        itemUpdate: itemUpdate,
+      }),
+    menuItems,
+    undefined
+  );
+
+  useEffect(() => {
+    updateIndex(menuItemsState);
+  }, [JSON.stringify(menuItemsState)]);
+
+  return { menuItemsState, toggleMenuItemsState };
+}
+
+export default function AdvancedSearchDropdown({
+  indexName,
+  indexPlaceholder,
+  menuItems = [],
+  updateIndex,
+}) {
+  menuItems = menuItems.map(initializeMenuItem);
+
+  const [expandMenu, setExpandMenu] = useState(false);
+
+  const { menuItemsState, toggleMenuItemsState } = useMenuItemsState(
+    menuItems,
+    updateIndex
+  );
+
+  return (
+    <nav className={styles.nav_element}>
+      <Toggler
+        setExpandMenu={setExpandMenu}
+        expandMenu={expandMenu}
+        indexName={indexName}
+        indexPlaceholder={indexPlaceholder}
+      />
+      {expandMenu && (
+        <List.Group
+          enabled={true}
+          label={indexName}
+          className={styles.dropdown_items}
+          disableGroupOutline={false}
+        >
+          {menuItemsState.map((item) => {
+            if (item?.formType === FormTypeEnum.CHECKBOX) {
+              return (
+                <List.Select
+                  key={item.name}
+                  onSelect={() => toggleMenuItemsState(item)}
+                  label={item.name}
+                >
+                  <CheckboxItem item={item} />
+                </List.Select>
+              );
+            } else if (item?.formType === FormTypeEnum.RADIO_BUTTON) {
+              return (
+                <List.Radio
+                  key={item.name}
+                  selected={item?.isSelected}
+                  moveItemRightOnFocus={true}
+                  onSelect={() => toggleMenuItemsState(item)}
+                  label={item.name}
+                >
+                  <RadioButtonItem item={item} />
+                </List.Radio>
+              );
+            } else if (item?.formType === FormTypeEnum.RADIO_LINK) {
+              return (
+                <List.Radio
+                  key={item.name}
+                  selected={item?.isSelected}
+                  moveItemRightOnFocus={true}
+                  onSelect={() =>
+                    !isEmpty(item?.value) && toggleMenuItemsState(item)
+                  }
+                  label={item.name}
+                >
+                  <RadioLinkItem
+                    item={item}
+                    toggleMenuItemsState={toggleMenuItemsState}
+                    DialogBox={DialogForPublicationYear}
+                  />
+                </List.Radio>
+              );
+            } else if (item?.formType === FormTypeEnum.DIVIDER) {
+              return <List.Divider key={Math.random()} />;
+            }
+          })}
+        </List.Group>
+      )}
+    </nav>
+  );
+}
---
+++ src/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown.stories.js
@@ -0,0 +1,144 @@
+import { StoryDescription, StoryTitle } from "@/storybook";
+import AdvancedSearchDropdown from "@/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown";
+import { FormTypeEnum } from "@/components/search/advancedSearch/advancedSearchHelpers/helperComponents/HelperComponents";
+
+const exportedObject = {
+  title: "AdvancedSearch/AdvancedSearchDropdown",
+};
+
+export default exportedObject;
+
+export function AdvancedSearchDropdownOnlyCheckboxes() {
+  const indices = [
+    { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
+    { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
+    { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
+    { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
+  ];
+  const updateIndex = () => {};
+
+  const storyTitle = "Only checkboxes";
+
+  return (
+    <div>
+      <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
+      <StoryDescription>
+        AdvancedSearch dropdown - {storyTitle}, aka a simple test
+      </StoryDescription>
+      <AdvancedSearchDropdown
+        indexName={storyTitle}
+        menuItems={indices}
+        updateIndex={updateIndex}
+      />
+    </div>
+  );
+}
+export function AdvancedSearchDropdownOnlyRadioButtons() {
+  const indices = [
+    { name: "kat", value: "kat", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "hund", value: "hund", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "hest", value: "hest", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "gris", value: "gris", formType: FormTypeEnum.RADIO_BUTTON },
+  ];
+
+  const updateIndex = () => {};
+
+  const storyTitle = "Only radio buttons";
+
+  return (
+    <div>
+      <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
+      <StoryDescription>
+        AdvancedSearch dropdown - {storyTitle}, aka a simple test
+      </StoryDescription>
+      <AdvancedSearchDropdown
+        indexName={storyTitle}
+        menuItems={indices}
+        updateIndex={updateIndex}
+      />
+    </div>
+  );
+}
+
+export function AdvancedSearchDropdownCheckboxesAndRadioButtons() {
+  const indices = [
+    { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
+    { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
+    { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
+    { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
+    { name: "giraf", value: "giraf", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "løve", value: "løve", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "elefant", value: "elefant", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "søløve", value: "søløve", formType: FormTypeEnum.RADIO_BUTTON },
+  ];
+
+  const updateIndex = () => {};
+
+  const storyTitle = "Checkboxes and radio buttons";
+
+  return (
+    <div>
+      <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
+      <StoryDescription>
+        AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
+      </StoryDescription>
+      <AdvancedSearchDropdown
+        indexName={storyTitle}
+        menuItems={indices}
+        updateIndex={updateIndex}
+      />
+    </div>
+  );
+}
+// export function AdvancedSearchDropdownCheckboxesAndDividerAndRadioButtons() {
+//   const indices = [
+//     { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
+//     { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
+//     { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
+//     { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
+//     { formType: FormTypeEnum.DIVIDER },
+//     { name: "giraf", value: "giraf", formType: FormTypeEnum.RADIO_BUTTON },
+//     { name: "løve", value: "løve", formType: FormTypeEnum.RADIO_BUTTON },
+//     { name: "elefant", value: "elefant", formType: FormTypeEnum.RADIO_BUTTON },
+//     { name: "søløve", value: "søløve", formType: FormTypeEnum.RADIO_BUTTON },
+//   ];
+//
+//   const storyTitle = "Checkboxes and divider and radio buttons";
+//
+//   return (
+//     <div>
+//       <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
+//       <StoryDescription>
+//         AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
+//       </StoryDescription>
+//       <AdvancedSearchDropdown indexName={storyTitle} menuItems={indices} />
+//     </div>
+//   );
+// }
+
+export function AdvancedSearchDropdownRadioButtonsAndRadioLink() {
+  const indices = [
+    { name: "kat", value: "kat", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "hund", value: "hund", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "hest", value: "hest", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "gris", value: "gris", formType: FormTypeEnum.RADIO_BUTTON },
+    { name: "giraf", value: {}, formType: FormTypeEnum.RADIO_LINK },
+  ];
+  const updateIndex = () => {};
+
+  const storyTitle = "Radio buttons and radio link";
+
+  return (
+    <div>
+      <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
+      <StoryDescription>
+        AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
+      </StoryDescription>
+      <AdvancedSearchDropdown
+        indexName={storyTitle}
+        menuItems={indices}
+        updateIndex={updateIndex}
+      />
+    </div>
+  );
+}
