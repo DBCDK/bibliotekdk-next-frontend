4e2f33cb9ec6559caba38bd5a4d653b17d3ea603
kiscdbc <kisc@dbc.dk>
2023-11-09 13:40:18 +0100

    desable loans and reservations test

--- e2e/cypress/e2e/loans-and-reservations.cy.js
+++ e2e/cypress/e2e/loans-and-reservations.cy.js
@@ -1,4 +1,4 @@
-describe(`Loans and reservations`, () => {
+describe.skip(`Loans and reservations`, () => {
   it("Renders properly", () => {
     cy.visit(
       "iframe.html?args=&id=profile-loans-and-reservations--loans-and-reservations-story&viewMode=story"
