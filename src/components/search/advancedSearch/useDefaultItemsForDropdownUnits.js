4848df195c707c446055bbb8d6490e244aee4d8b
akri <akri@dbc.dk>
2023-11-22 14:23:20 +0100

    Fixes from review

--- src/components/search/advancedSearch/useDefaultItemsForDropdownUnits.js
+++ src/components/search/advancedSearch/useDefaultItemsForDropdownUnits.js
@@ -4,6 +4,7 @@ import {
 } from "@/components/search/advancedSearch/advancedSearchHelpers/dummy__default_advanced_search_fields";
 import { convertToDropdownInput } from "@/components/search/advancedSearch/advancedSearchHelpers/convertToDropdownInput";
 import { FormTypeEnum } from "@/components/search/advancedSearch/advancedSearchHelpers/helperComponents/HelperComponents";
+import isEmpty from "lodash/isEmpty";
 
 export const DropdownIndicesEnum = {
   LANGUAGES: "phrase.mainlanguage",
@@ -12,33 +13,41 @@ export const DropdownIndicesEnum = {
 };
 
 /**
- *
+ * If there is a fieldSearch.dropdownSearchIndices, this function
+ *   enriches the dropdownUnit with its isSelected
  * @param {Array.<DropdownSearchIndex>} initDropdowns
  * @param {DropdownUnit} dropdownUnit
  * @returns {DropdownUnit}
  */
 function getDropdownFromUrl({ initDropdowns, dropdownUnit }) {
+  // Get the single dropdownUnit from initial value (be it from url or as a new initial dropdown value)
   const actualSearchIndex = initDropdowns.find(
     (initDropdown) => initDropdown?.searchIndex === dropdownUnit.indexName
   );
 
+  // If the actualSearchIndex is not the same as dropdownUnit's indexName
+  //   we expect the dropdownUnit to be as default value (where no items has isSelected===true)
   if (!actualSearchIndex) {
     return dropdownUnit;
   }
 
-  const res = dropdownUnit.items.map((singleItem) => {
+  // We enrich the dropdownUnit with the value from initialDropdowns (be it url or a new intial dropdown)
+  const enrichedDropdownUnit = dropdownUnit.items.map((singleItem) => {
     return {
       ...singleItem,
       isSelected:
-        [FormTypeEnum.CHECKBOX, FormTypeEnum.RADIO_BUTTON].includes(
+        ([FormTypeEnum.CHECKBOX, FormTypeEnum.RADIO_BUTTON].includes(
           singleItem.formType
-        ) && actualSearchIndex.value.includes(singleItem?.name),
+        ) ||
+          ([FormTypeEnum.RADIO_LINK].includes(singleItem.formType) &&
+            !isEmpty(singleItem.value))) &&
+        actualSearchIndex.value.includes(singleItem?.name),
     };
   });
 
   return {
     indexName: dropdownUnit.indexName,
-    items: /** @type DropdownInputArray */ res,
+    items: /** @type DropdownInputArray */ enrichedDropdownUnit,
   };
 }
 
