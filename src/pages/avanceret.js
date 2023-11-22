72ee4b9f81aab599c3b2f7959802971c98c7ebe9
akri <akri@dbc.dk>
2023-11-22 12:53:36 +0100

    Use context

--- src/pages/avanceret.js
+++ src/pages/avanceret.js
@@ -45,26 +45,6 @@ export default function AdvancedSearchPage() {
 
         <AdvancedSearch />
         <Container fluid>
-          {showResult && (
-            <AdvancedSearchResult
-              pageNo={parseInt(pageNo, 10)}
-              onPageChange={async (page, scroll) => {
-                scroll = typeof scroll !== "boolean" || scroll !== false;
-                await updateQueryParams({ page });
-                scroll && scrollToRef(scrollRef);
-              }}
-              // .. @TODO .. what to do with the datacollect ??
-              onWorkClick={(index, work) => {
-                dataCollect.collectSearchWorkClick({
-                  search_request: { q, filters },
-                  search_query_hit: index + 1,
-                  search_query_work: work.workId,
-                });
-              }}
-              cql={cql}
-              fieldSearch={fieldSearch}
-            />
-          )}
           <AdvancedSearchResult
             onPageChange={async (page, scroll) => {
               scroll = typeof scroll !== "boolean" || scroll !== false;
