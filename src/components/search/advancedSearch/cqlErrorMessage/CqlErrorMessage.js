600fb13a54bd33c5e43aa6070be6ddedf169cc91
pjohans <pjohans@gmail.com>
2023-12-08 14:43:28 +0100

    syntax checker

--- src/components/search/advancedSearch/cqlErrorMessage/CqlErrorMessage.js
+++ src/components/search/advancedSearch/cqlErrorMessage/CqlErrorMessage.js
@@ -2,8 +2,19 @@ import InfoDropdown from "@/components/base/infoDropdown/InfoDropdown";
 import { useData } from "@/lib/api/api";
 import { doComplexSearchAll } from "@/lib/api/complexSearch.fragments";
 import styles from "./CqlErrorMessage.module.css";
+import Icon from "@/components/base/icon/Icon";
+import { useState } from "react";
+import isEmpty from "lodash/isEmpty";
+import { symbol } from "prop-types";
 
 function parseErrorMessage(errorMessage) {
+  if (isEmpty(errorMessage)) {
+    return {
+      explanation: "Nice work",
+      location: location,
+      full: "Well done",
+    };
+  }
   // first sentence of errormessage is (kind of) explanation
   const explanation = errorMessage.split(",")[0];
   // last part is location of error - starts with at: ---> .. and then the rest
@@ -18,28 +29,47 @@ function parseErrorMessage(errorMessage) {
 }
 
 export function CqlErrorMessage(errormessage) {
-  if (!errormessage) {
-    return null;
+  const [src, setSrc] = useState("status__on_shelf.svg");
+  const [showError, setShowError] = useState(false);
+  if (errormessage) {
+    setTimeout(() => {
+      setSrc("status__not_for_loan.svg");
+    }, 300);
+  } else {
+    setTimeout(() => {
+      setSrc("status__on_shelf.svg");
+    }, 300);
   }
 
   const message = parseErrorMessage(errormessage);
 
   return (
-    <div className={styles.errorWrapper}>
-      <div className={styles.errorMessage}>
-        <div>{message.explanation}</div>
-        <div>{message.location}</div>
-      </div>
-      <div className={styles.errorFull}>
-        <InfoDropdown buttonText="full error message" label="Full error">
-          {message.full}
-        </InfoDropdown>
-      </div>
+    <div className={styles.syntaxContainer}>
+      <Icon
+        src={src}
+        size={{ w: 2, h: "auto" }}
+        onClick={() => setShowError(!showError)}
+        className={styles.action}
+      />
+      {showError && (
+        <div className={styles.errorWrapper}>
+          <div className={styles.errorMessage}>
+            <div>{message.explanation}</div>
+            <div>{message.location}</div>
+          </div>
+          <div className={styles.errorFull}>
+            <InfoDropdown buttonText="full error message" label="Full error">
+              {message.full}
+            </InfoDropdown>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default function Wrap({ cql }) {
   const bigResponse = useData(doComplexSearchAll({ cql, offset: 0, limit: 1 }));
+
   return CqlErrorMessage(bigResponse?.data?.complexSearch?.errorMessage);
 }
