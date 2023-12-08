e62bd1fae85af5a3c623b0d853a8e9d5fba4a422
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-12-08 13:59:56 +0100

    removed   stuff

--- src/components/search/advancedSearch/useInputFields.js
+++ src/components/search/advancedSearch/useInputFields.js
@@ -23,7 +23,6 @@ export function useInputFields({ fieldSearchFromUrl }) {
     }
 
     if (inputFromUrl?.inputFields) {
-      const newState = getInitialInputFields()
       setInputFields(inputFromUrl?.inputFields);
     }
   }, [router?.query?.fieldSearch]);
