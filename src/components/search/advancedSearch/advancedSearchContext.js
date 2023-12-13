eea598ab447d7cc2f7744d560b0c913fb9929f6a
akri <akri@dbc.dk>
2023-12-13 15:26:38 +0100

    Remove miav

--- src/components/search/advancedSearch/advancedSearchContext.js
+++ src/components/search/advancedSearch/advancedSearchContext.js
@@ -124,8 +124,6 @@ export default function AdvancedSearchProvider({ children, router }) {
   //// ---- DONE: parsedCQL ----
 
   function resetObjectState() {
-    console.log("miav");
-
     resetInputFields();
     resetDropdownIndices();
     dispatchResetMenuItemsEvent();
