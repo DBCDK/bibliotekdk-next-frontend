53226672f8bbc5690d44bc6b5e50de2f564ba813
akri <akri@dbc.dk>
2023-12-15 12:27:51 +0100

    Just use separate link and translation

--- src/components/series/seriesHeading/titleBox/TitleBox.js
+++ src/components/series/seriesHeading/titleBox/TitleBox.js
@@ -8,9 +8,8 @@ import cx from "classnames";
 import ThumbnailParade from "@/components/series/seriesHeading/titleBox/thumbnailParade/ThumbnailParade";
 
 import { getUniqueCreatorsDisplay } from "@/components/series/utils";
-import { buildHtmlLink, getUniverseUrl } from "@/lib/utils";
+import { getUniverseUrl } from "@/lib/utils";
 import { getAdvancedUrl } from "@/components/search/advancedSearch/utils";
-import { useRouter } from "next/router";
 
 export function LinkToCreator({ creator, isLoading }) {
   // @TODO .. do we need some refactoring ?? - this inputfield is
@@ -38,8 +37,6 @@ export default function TitleBox({ series, seriesIsLoading, className }) {
   const firstSeriesFirstWork = series?.members?.[0]?.work;
   const description = series?.description;
   const { creators, creatorsToShow } = getUniqueCreatorsDisplay(series);
-
-  const router = useRouter();
 
   return (
     <div
@@ -85,9 +82,6 @@ export default function TitleBox({ series, seriesIsLoading, className }) {
           </Text>
         )}
         {firstSeriesFirstWork?.universes?.map((universe) => {
-          router.prefetch(
-            getUniverseUrl(universe?.title, firstSeriesFirstWork?.workId)
-          );
           return (
             <Text
               skeleton={seriesIsLoading}
@@ -98,18 +92,17 @@ export default function TitleBox({ series, seriesIsLoading, className }) {
               {Translate({
                 context: "series_page",
                 label: "part_of_universe",
-                vars: [
-                  buildHtmlLink(
-                    universe?.title,
-                    getUniverseUrl(
-                      universe?.title,
-                      firstSeriesFirstWork?.workId
-                    ),
-                    "_self"
-                  ),
-                ],
                 renderAsHtml: true,
               })}
+              <Link
+                href={getUniverseUrl(
+                  universe?.title,
+                  firstSeriesFirstWork?.workId
+                )}
+                border={{ bottom: { keepVisible: true } }}
+              >
+                {universe?.title}
+              </Link>
             </Text>
           );
         })}
