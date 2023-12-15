574fe020e1409dfeb68f30ccc7da8655d96380f9
akri <91877805+akri-dbcdk@users.noreply.github.com>
2023-12-15 13:34:47 +0100

    Main into prod (#1226)
    * bugfixes - searchhistory
    
    * skeleton on universe and series (#1214)
    
    * ...
    
    * ...
    
    * removed console.log
    
    * ...
    
    * ...
    
    * ...
    
    * fixed refworks
    
    * optional chaining :)
    
    * and some more  optional chaining
    
    * Fix slow load of universe (#1215)
    
    * textarea styles (#1218)
    
    * Bibdk2021 2253 huskeliste efter multiorder (#1213)
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * orderMutation solution wip
    
    * something working
    
    * remember previous orders in same session
    
    * single order marks something
    
    * single order working
    
    * clean up
    
    * clean up
    
    * Bibdk2021 1652 error label fejl onlineartikel har to links med ens mal (#1217)
    
    * Default to all reservation buttons for online material
    
    * Default to all reservation buttons for online material
    
    * Pretty portrait page
    
    * fix from review
    
    * Bibdk2021 2266 input placeholders (#1216)
    
    * added placeholders
    
    * clean up
    
    * clean up
    
    * clean up
    
    * remove comment
    
    * cql placeholder
    
    * cql area translate
    
    * space
    
    * Add translation (#1221)
    
    * Bibdk2021 2281 prove en ting mere for hurtigere load af series (#1223)
    
    * Add translation
    
    * Try one more thing
    
    * Use router prefetch instead
    
    * Just use separate link and translation (#1225)
    
    ---------
    
    Co-authored-by: pjohans <pjohans@gmail.com>
    Co-authored-by: mtni <36037828+mtni@users.noreply.github.com>
    Co-authored-by: mtni <mtni@dbc.dk>
    Co-authored-by: Yousef <yousefmohsen@outlook.dk>
    Co-authored-by: kiscdbc <97035888+kiscdbc@users.noreply.github.com>
    Co-authored-by: kiscdbc <kisc@dbc.dk>

--- src/components/work/overview/workgroupingsoverview/WorkGroupingsOverview.js
+++ src/components/work/overview/workgroupingsoverview/WorkGroupingsOverview.js
@@ -90,7 +90,11 @@ function getSeriesMap({ series, members, workId }) {
   return (
     members?.length > 0 && {
       partNumber: series?.numberInSeries?.display,
-      description: `Del ${numberInSeries + " "} af `,
+      description: Translate({
+        context: "overview",
+        label: "work_groupings_overview_description",
+        vars: [numberInSeries + " "],
+      }),
       title: series?.title,
       anchorId: getAnchor(AnchorsEnum.SERIES),
       link: getSeriesUrl(series?.title, workId),
@@ -102,7 +106,11 @@ function getContinuationMap(groupedByRelationWorkTypes) {
   return (
     groupedByRelationWorkTypes?.[WorkTypeEnum.ARTICLE] && {
       partNumber: null,
-      description: `Del af `,
+      description: Translate({
+        context: "overview",
+        label: "work_groupings_overview_description",
+        vars: [""],
+      }),
       title: "artikelserie",
       anchorId: getAnchor(AnchorsEnum.RELATED_WORKS),
     }
