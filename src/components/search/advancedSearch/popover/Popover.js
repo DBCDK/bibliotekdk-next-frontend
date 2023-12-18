e3432c5cc8676e2a3508769510008d28212a5ecd
Yousef Mohsen <yousefmohsen@outlook.dk>
2023-12-18 12:25:25 +0100

    console

--- src/components/search/advancedSearch/popover/Popover.js
+++ src/components/search/advancedSearch/popover/Popover.js
@@ -73,7 +73,6 @@ const Popover = ({ className, simpleSearchRef }) => {
     };
   }, []);
 
-  console.log("showPopover", showPopover);
   return (
     <>
       <div className="container" ref={triggerContainerRef}>
