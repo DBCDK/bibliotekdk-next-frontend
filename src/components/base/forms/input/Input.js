38eb9d06bf6c8456e9d07fc385f4d3e53cb5537b
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-01-22 14:59:13 +0100

    fixed tabindex

--- src/components/base/forms/input/Input.js
+++ src/components/base/forms/input/Input.js
@@ -65,8 +65,6 @@ function Input({
   delete props.skeleton;
   delete props.invalidClass;
   delete props.onMount;
-  console.log("tabIndex !== undefined ", tabIndex !== undefined);
-  console.log("tabIndex  ", tabIndex);
 
   return (
     <input
@@ -80,7 +78,7 @@ function Input({
       disabled={disabled}
       readOnly={readOnly}
       data-cy={dataCy}
-      tabIndex={tabIndex !== undefined ? tabIndex : disabled ? "-1" : tabIndex}
+      tabIndex={disabled ? "-1" : tabIndex}
       onBlur={(e) => onBlur && onBlur(e)}
       onChange={(e) => {
         onChange && onChange(e);
