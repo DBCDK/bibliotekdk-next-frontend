3a86e3035fb0d63846aa8f875a92b45b9e798396
mtni <mtni@dbc.dk>
2024-01-24 10:50:47 +0100

    ...

--- src/components/search/advancedSearch/advancedSearch/AdvancedSearch.js
+++ src/components/search/advancedSearch/advancedSearch/AdvancedSearch.js
@@ -34,7 +34,6 @@ export default function AdvancedSearch({ ariaExpanded, className }) {
     cqlFromUrl,
     fieldSearchFromUrl,
     setShowPopover,
-    showPopover,
     stateToString,
     resetObjectState,
   } = useAdvancedSearchContext();
