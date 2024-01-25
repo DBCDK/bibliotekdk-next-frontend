496490bb25a45cf8937181fd6342a1cf7456e347
akri <91877805+akri-dbcdk@users.noreply.github.com>
2024-01-25 13:04:40 +0100

    Bibdk2021 2333 refaktorere dropdown state (#1312)
    * Simplify dropdown state
    
    * Happy linter
    
    * Some prettifications
    
    * Remove console log
    
    * Additional prettifications
    
    * Move dependency
    
    * Remove commented out code

--- src/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown.stories.js
+++ src/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown.stories.js
@@ -1,6 +1,8 @@
 import { StoryDescription, StoryTitle } from "@/storybook";
 import AdvancedSearchDropdown from "@/components/search/advancedSearch/advancedSearchDropdown/AdvancedSearchDropdown";
 import { FormTypeEnum } from "@/components/search/advancedSearch/advancedSearchHelpers/helperComponents/HelperComponents";
+import { getNameForActionLinkContainer } from "@/components/search/advancedSearch/advancedSearchHelpers/dummy__default_advanced_search_fields";
+import { useAdvancedSearchContext } from "@/components/search/advancedSearch/advancedSearchContext";
 
 const exportedObject = {
   title: "AdvancedSearch/AdvancedSearchDropdown",
@@ -9,15 +11,40 @@ const exportedObject = {
 export default exportedObject;
 
 export function AdvancedSearchDropdownOnlyCheckboxes() {
-  const indices = [
-    { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
-    { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
-    { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
-    { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
-  ];
-  const updateIndex = () => {};
-
   const storyTitle = "Only checkboxes";
+
+  const indices = [
+    {
+      name: "kat",
+      key: "kat",
+      value: "kat",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "hund",
+      key: "hund",
+      value: "hund",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "hest",
+      key: "hest",
+      value: "hest",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "gris",
+      key: "gris",
+      value: "gris",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+  ];
+
+  const advancedSearchContext = useAdvancedSearchContext();
 
   return (
     <div>
@@ -26,25 +53,50 @@ export function AdvancedSearchDropdownOnlyCheckboxes() {
         AdvancedSearch dropdown - {storyTitle}, aka a simple test
       </StoryDescription>
       <AdvancedSearchDropdown
-        indexName={storyTitle}
         indexTitle={storyTitle}
+        indexName={storyTitle}
+        indexPlaceholder={storyTitle}
         menuItems={indices}
-        updateIndex={updateIndex}
+        advancedSearchContext={advancedSearchContext}
       />
     </div>
   );
 }
 export function AdvancedSearchDropdownOnlyRadioButtons() {
+  const storyTitle = "Only radio buttons";
+
   const indices = [
-    { name: "kat", value: "kat", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "hund", value: "hund", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "hest", value: "hest", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "gris", value: "gris", formType: FormTypeEnum.RADIO_BUTTON },
+    {
+      name: "kat",
+      key: "kat",
+      value: "kat",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "hund",
+      key: "hund",
+      value: "hund",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "hest",
+      key: "hest",
+      value: "hest",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "gris",
+      key: "gris",
+      value: "gris",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
   ];
 
-  const updateIndex = () => {};
-
-  const storyTitle = "Only radio buttons";
+  const advancedSearchContext = useAdvancedSearchContext();
 
   return (
     <div>
@@ -53,30 +105,79 @@ export function AdvancedSearchDropdownOnlyRadioButtons() {
         AdvancedSearch dropdown - {storyTitle}, aka a simple test
       </StoryDescription>
       <AdvancedSearchDropdown
-        indexName={storyTitle}
         indexTitle={storyTitle}
+        indexName={storyTitle}
+        indexPlaceholder={storyTitle}
         menuItems={indices}
-        updateIndex={updateIndex}
+        advancedSearchContext={advancedSearchContext}
       />
     </div>
   );
 }
 
 export function AdvancedSearchDropdownCheckboxesAndRadioButtons() {
+  const storyTitle = "Checkboxes and radio buttons";
+
   const indices = [
-    { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
-    { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
-    { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
-    { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
-    { name: "giraf", value: "giraf", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "løve", value: "løve", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "elefant", value: "elefant", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "søløve", value: "søløve", formType: FormTypeEnum.RADIO_BUTTON },
+    {
+      name: "kat",
+      key: "kat",
+      value: "kat",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "hund",
+      key: "hund",
+      value: "hund",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "hest",
+      key: "hest",
+      value: "hest",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "gris",
+      key: "gris",
+      value: "gris",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "giraf",
+      key: "giraf",
+      value: "giraf",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "løve",
+      key: "løve",
+      value: "løve",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "elefant",
+      key: "elefant",
+      value: "elefant",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "søløve",
+      key: "søløve",
+      value: "søløve",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
   ];
 
-  const updateIndex = () => {};
-
-  const storyTitle = "Checkboxes and radio buttons";
+  const advancedSearchContext = useAdvancedSearchContext();
 
   return (
     <div>
@@ -85,59 +186,80 @@ export function AdvancedSearchDropdownCheckboxesAndRadioButtons() {
         AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
       </StoryDescription>
       <AdvancedSearchDropdown
-        indexName={storyTitle}
         indexTitle={storyTitle}
+        indexName={storyTitle}
+        indexPlaceholder={storyTitle}
         menuItems={indices}
-        updateIndex={updateIndex}
+        advancedSearchContext={advancedSearchContext}
       />
     </div>
   );
 }
 
 export function AdvancedSearchDropdownCheckboxesAndDividerAndRadioButtons() {
-  const indices = [
-    { name: "kat", value: "kat", formType: FormTypeEnum.CHECKBOX },
-    { name: "hund", value: "hund", formType: FormTypeEnum.CHECKBOX },
-    { name: "hest", value: "hest", formType: FormTypeEnum.CHECKBOX },
-    { name: "gris", value: "gris", formType: FormTypeEnum.CHECKBOX },
-    { formType: FormTypeEnum.DIVIDER },
-    { name: "giraf", value: "giraf", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "løve", value: "løve", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "elefant", value: "elefant", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "søløve", value: "søløve", formType: FormTypeEnum.RADIO_BUTTON },
-  ];
-
-  const updateIndex = () => {};
-
   const storyTitle = "Checkboxes and divider and radio buttons";
 
-  return (
-    <div>
-      <StoryTitle>AdvancedSearchDropdown - {storyTitle}</StoryTitle>
-      <StoryDescription>
-        AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
-      </StoryDescription>
-      <AdvancedSearchDropdown
-        indexName={storyTitle}
-        indexTitle={storyTitle}
-        menuItems={indices}
-        updateIndex={updateIndex}
-      />
-    </div>
-  );
-}
-
-export function AdvancedSearchDropdownRadioButtonsAndRadioLink() {
   const indices = [
-    { name: "kat", value: "kat", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "hund", value: "hund", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "hest", value: "hest", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "gris", value: "gris", formType: FormTypeEnum.RADIO_BUTTON },
-    { name: "giraf", value: {}, formType: FormTypeEnum.RADIO_LINK },
+    {
+      name: "kat",
+      key: "kat",
+      value: "kat",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "hund",
+      key: "hund",
+      value: "hund",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "hest",
+      key: "hest",
+      value: "hest",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    {
+      name: "gris",
+      key: "gris",
+      value: "gris",
+      formType: FormTypeEnum.CHECKBOX,
+      indexName: storyTitle,
+    },
+    { formType: FormTypeEnum.DIVIDER, indexName: storyTitle },
+    {
+      name: "giraf",
+      key: "giraf",
+      value: "giraf",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "løve",
+      key: "løve",
+      value: "løve",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "elefant",
+      key: "elefant",
+      value: "elefant",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
+    {
+      name: "søløve",
+      key: "søløve",
+      value: "søløve",
+      formType: FormTypeEnum.RADIO_BUTTON,
+      indexName: storyTitle,
+    },
   ];
-  const updateIndex = () => {};
 
-  const storyTitle = "Radio buttons and radio link";
+  const advancedSearchContext = useAdvancedSearchContext();
 
   return (
     <div>
@@ -146,53 +268,68 @@ export function AdvancedSearchDropdownRadioButtonsAndRadioLink() {
         AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
       </StoryDescription>
       <AdvancedSearchDropdown
-        indexName={storyTitle}
         indexTitle={storyTitle}
+        indexName={storyTitle}
+        indexPlaceholder={storyTitle}
         menuItems={indices}
-        updateIndex={updateIndex}
+        advancedSearchContext={advancedSearchContext}
       />
     </div>
   );
 }
 
 export function AdvancedSearchDropdownActionLink() {
+  const storyTitle = "Action Link";
   const indices = [
     {
       name: "I år (2023)",
+      key: "I år (2023)",
       value: { lower: 2023, upper: 2024 },
       formType: FormTypeEnum.ACTION_LINK,
+      indexName: storyTitle,
     },
     {
       name: "Seneste to år",
+      key: "Seneste to år",
       value: { lower: 2021, upper: 2024 },
       formType: FormTypeEnum.ACTION_LINK,
+      indexName: storyTitle,
     },
     {
       name: "Seneste tre år",
+      key: "Seneste tre år",
 
       value: { lower: 2020, upper: 2024 },
       formType: FormTypeEnum.ACTION_LINK,
+      indexName: storyTitle,
     },
     {
       name: "Seneste fem år",
+      key: "Seneste fem år",
       value: { lower: 2018, upper: 2024 },
       formType: FormTypeEnum.ACTION_LINK,
+      indexName: storyTitle,
     },
     {
       name: "Seneste 10 år",
+      key: "Seneste 10 år",
       value: { lower: 2013, upper: 2024 },
       formType: FormTypeEnum.ACTION_LINK,
+      indexName: storyTitle,
     },
     {
-      name: "container",
+      name: getNameForActionLinkContainer(
+        FormTypeEnum.ACTION_LINK_CONTAINER,
+        storyTitle
+      ),
+      key: "container",
       value: {},
       formType: FormTypeEnum.ACTION_LINK_CONTAINER,
+      indexName: storyTitle,
     },
   ];
 
-  const updateIndex = () => {};
-
-  const storyTitle = "Action Link";
+  const advancedSearchContext = useAdvancedSearchContext();
 
   return (
     <div>
@@ -201,10 +338,11 @@ export function AdvancedSearchDropdownActionLink() {
         AdvancedSearch dropdown - {storyTitle}, aka a more complicated test
       </StoryDescription>
       <AdvancedSearchDropdown
-        indexName={storyTitle}
         indexTitle={storyTitle}
+        indexName={storyTitle}
+        indexPlaceholder={storyTitle}
         menuItems={indices}
-        updateIndex={updateIndex}
+        advancedSearchContext={advancedSearchContext}
       />
     </div>
   );
