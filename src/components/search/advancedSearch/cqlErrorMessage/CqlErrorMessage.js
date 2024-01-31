39b3b865dc999ecd605dd771b37f20cbd1582605
akri <akri@dbc.dk>
2024-01-31 18:17:16 +0100

    Prettier cql error message

--- src/components/search/advancedSearch/cqlErrorMessage/CqlErrorMessage.js
+++ src/components/search/advancedSearch/cqlErrorMessage/CqlErrorMessage.js
@@ -1,10 +1,18 @@
-import InfoDropdown from "@/components/base/infoDropdown/InfoDropdown";
 import { useData } from "@/lib/api/api";
 import { doComplexSearchAll } from "@/lib/api/complexSearch.fragments";
 import styles from "./CqlErrorMessage.module.css";
-import Icon from "@/components/base/icon/Icon";
 import { useState } from "react";
 import isEmpty from "lodash/isEmpty";
+import Text from "@/components/base/text";
+import isEqual from "lodash/isEqual";
+import cx from "classnames";
+import { IconLink } from "@/components/base/iconlink/IconLink";
+import animations from "css/animations";
+import Link from "@/components/base/link";
+import CloseSvg from "@/public/icons/close_grey.svg";
+import RedSvg from "@/public/icons/status__not_for_loan.svg";
+import GreenSvg from "@/public/icons/status__on_shelf.svg";
+import Translate, { hasTranslation } from "@/components/base/translate";
 
 function parseErrorMessage(errorMessage) {
   // first sentence of errormessage is (kind of) explanation
@@ -29,40 +37,91 @@ function parseErrorMessage(errorMessage) {
 }
 
 export function CqlErrorMessage(errormessage) {
-  const [src, setSrc] = useState("status__on_shelf.svg");
+  const [svg, setSvg] = useState("red");
   const [showError, setShowError] = useState(false);
   if (errormessage) {
     setTimeout(() => {
-      setSrc("status__not_for_loan.svg");
+      setSvg("red");
     }, 300);
   } else {
     setTimeout(() => {
-      setSrc("status__on_shelf.svg");
+      setSvg("green");
     }, 300);
   }
 
   const message = parseErrorMessage(errormessage);
 
+  const link_translation = {
+    context: "advanced_search_cql",
+    label: "cql_error_link_href",
+  };
+
   return (
-    <div className={styles.syntaxContainer}>
-      <Icon
-        src={src}
-        size={{ w: 2, h: "auto" }}
-        onClick={() => setShowError(!showError)}
-        className={styles.action}
-      />
+    <div className={styles.syntaxContainer} aria-expanded={showError}>
+      {!showError && (
+        <Link
+          border={false}
+          className={cx(styles.action_bubble)}
+          onClick={() => setShowError(!showError)}
+        >
+          {svg === "green" && (
+            <GreenSvg
+              className={cx(animations["h-elastic"], animations["f-elastic"])}
+            />
+          )}
+          {svg === "red" && (
+            <RedSvg
+              className={cx(animations["h-elastic"], animations["f-elastic"])}
+            />
+          )}
+        </Link>
+      )}
       {showError && (
-        <div className={styles.errorWrapper}>
-          <div className={styles.errorMessage}>
-            <div>{message.explanation}</div>
-            <div>{message.location}</div>
-          </div>
-          <div className={styles.errorFull}>
-            <InfoDropdown buttonText="full error message" label="Full error">
-              {message.full}
-            </InfoDropdown>
-          </div>
-        </div>
+        <Text
+          type={"text3"}
+          className={cx(styles.errorMessage, {
+            [styles.noError]: !errormessage,
+          })}
+        >
+          <Link
+            border={false}
+            className={cx(styles.close)}
+            onClick={() => setShowError((prev) => !prev)}
+          >
+            <CloseSvg
+              className={cx(animations["h-elastic"], animations["f-elastic"])}
+            />
+          </Link>
+          {svg === "green" && (
+            <div>
+              {Translate({
+                context: "advanced_search_cql",
+                label: "cql_error_success",
+              })}
+            </div>
+          )}
+          {svg === "red" && (
+            <>
+              <div className={styles.explanation}>{message.explanation}</div>
+              {!isEqual(message.location, message.explanation) && (
+                <div className={styles.location}>{message.location}</div>
+              )}
+              <IconLink
+                className={styles.link}
+                iconPlacement={"right"}
+                iconStyle={{ marginTop: "var(--pt05)" }}
+                iconOrientation={180}
+                disabled={!hasTranslation(link_translation)}
+                href={Translate(link_translation)}
+              >
+                {Translate({
+                  context: "advanced_search_cql",
+                  label: "cql_error_link_text",
+                })}
+              </IconLink>
+            </>
+          )}
+        </Text>
       )}
     </div>
   );
