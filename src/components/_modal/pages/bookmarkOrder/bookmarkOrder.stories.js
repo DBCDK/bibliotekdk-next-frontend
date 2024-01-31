cd359e3b16417777a8ce8737662221c3c7b7d548
pjohans <pjohans@gmail.com>
2024-01-31 09:25:12 +0100

    lesser margin in mobile + a test

--- src/components/_modal/pages/bookmarkOrder/bookmarkOrder.stories.js
+++ src/components/_modal/pages/bookmarkOrder/bookmarkOrder.stories.js
@@ -12,13 +12,48 @@ const exportedObject = {
 const {
   USER_3,
   BRANCH_3,
+  BRANCH_4,
   WORK_11,
   WORK_12,
   DEFAULT_STORY_PARAMETERS,
+  BORROWER_STATUS_FALSE,
   useMockLoanerInfo,
 } = automock_utils();
 
 export default exportedObject;
+
+const default_context = () => {
+  const Bookmarks = [
+    {
+      key: "work-of:some-pid-8BOOK",
+      materialId: "work-of:some-pid-8",
+      workId: "work-of:some-pid-8",
+      materialType: "BOOK",
+      title: "fisk",
+      createdAt: "2024-01-05T14:03:05.432Z",
+    },
+  ];
+
+  const onlineBookmark = [
+    {
+      key: "work-of:some-pid-7EBOOK",
+      materialId: "work-of:some-pid-7",
+      workId: "work-of:some-pid-7",
+      materialType: "EBOOK",
+      title: "fisk",
+      createdAt: "2024-01-05T14:03:05.432Z",
+    },
+  ];
+  const handleOrderFinished = () => null;
+  const context = {
+    sortType: "title",
+    bookmarksToOrder: Bookmarks,
+    handleOrderFinished,
+    bookmarksOnlineAvailable: onlineBookmark,
+  };
+
+  return context;
+};
 
 export function MaterialsToOrderDouble() {
   const Bookmarks = [
@@ -92,6 +127,44 @@ MaterialsToOrderDouble.story = merge({}, DEFAULT_STORY_PARAMETERS, {
           failedAtCreation: () => [],
           successfullyCreated: () => ["work-of:some-pid-8BOOK"],
           ok: () => true,
+        },
+      },
+    },
+  },
+});
+
+export function BlockedUser() {
+  const context = default_context();
+  return (
+    <div>
+      <StoryTitle>Materials to order - user is blocked</StoryTitle>
+      <StoryDescription>
+        A list of bookmarked materials to order
+      </StoryDescription>
+
+      <EMaterialFilter context={context} active={true} />
+      <Modal.Container>
+        <Modal.Page id="multiorder" component={Pages.MultiOrder} />
+        <Modal.Page id="multireceipt" component={Pages.MultiReceipt} />
+      </Modal.Container>
+    </div>
+  );
+}
+
+BlockedUser.story = merge({}, DEFAULT_STORY_PARAMETERS, {
+  parameters: {
+    graphql: {
+      resolvers: {
+        Query: {
+          user: () => USER_3,
+          works: () => [WORK_11, WORK_12],
+          checkorderpolicy: () => ({ orderPossible: true }),
+          branches: () => {
+            return {
+              borrowerStatus: BORROWER_STATUS_FALSE,
+              result: [BRANCH_4],
+            };
+          },
         },
       },
     },
