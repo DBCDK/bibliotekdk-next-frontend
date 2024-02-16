85e78e795e1383f9ffa238b75b56efe82cb5b550
Kasper Fuglsang <kfu@dbc.dk>
2024-02-16 15:34:45 +0100

    docs

--- src/components/hooks/useGlobalState.js
+++ src/components/hooks/useGlobalState.js
@@ -1,5 +1,10 @@
 import useSWR from "swr";
 let globalState = {};
+
+/**
+ * Custom hook for managing global state using SWR.
+ * Changes are broadcasted to all components that use the same key
+ */
 export function useGlobalState({ key, initial }) {
   const { data, mutate } = useSWR(key, () => globalState[key], {
     fallbackData: initial,
