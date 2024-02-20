4b9e50d55e34e71c7ab0f036a8da75f3eaee2ec3
pjohans <pjohans@gmail.com>
2024-02-20 08:08:06 +0100

    in progress

--- src/components/search/advancedSearch/facets/advancedFacets.js
+++ src/components/search/advancedSearch/facets/advancedFacets.js
@@ -1,18 +1,37 @@
 import { AdvFacetsTypeEnum } from "@/lib/enums";
 import Accordion, { Item } from "@/components/base/accordion/Accordion";
-import mockedFacets from "./mockedFacets.json";
+
 import styles from "./advancedFacets.module.css";
 import { Checkbox } from "@/components/base/forms/checkbox/Checkbox";
 import { useState } from "react";
 import { useFacets } from "@/components/search/advancedSearch/useFacets";
 
-function AdvancedFacets(facets) {
-  // filter out facets NOT found in request
+/**
+ *
+ * @param facets - facet from search result
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export function AdvancedFacets({ facets }) {
+  // filter out facets NOT found in response
   const filteredFacets = Object.values(AdvFacetsTypeEnum).filter((val) =>
     facets.find((facet) => {
       return facet.name.includes(val);
     })
   );
+
+  const { addFacet, removeFacet } = useFacets();
+
+  // @TODO - use query -> setQuery when checked or unchecked and do a live search
+  const onItemClick = (checked, name, facetName) => {
+    if (checked) {
+      // selected -> add to list
+      addFacet(name, facetName);
+    } else {
+      // deselected - remove from list
+      removeFacet(name, facetName);
+    }
+  };
 
   return (
     <Accordion>
@@ -22,49 +41,35 @@ function AdvancedFacets(facets) {
           index={index}
           facets={facets}
           key={`${facetName}-${index}`}
+          onItemClick={onItemClick}
         />
       ))}
     </Accordion>
   );
 }
 
-function AccordianItem({ facetName, facets, index }) {
-  const [selectedItems, setSelectedItems] = useState([]);
-
-  const { addFacet } = useFacets();
-  // @TODO - use query -> setQuery when checked or unchecked and do a live search
-  const onItemClick = (checked, name) => {
-    if (checked) {
-      addFacet(name, facetName);
-      // selected -> add to list
-      const fisk = selectedItems.concat([name]);
-      setSelectedItems(fisk);
-    } else {
-      // not selected -> remove from list
-      const indx = selectedItems.findIndex((sel) => sel === name);
-      const fisk = [...selectedItems];
-      fisk.splice(indx, 1);
-      setSelectedItems(fisk);
-    }
-  };
+function AccordianItem({ facetName, facets, index, onItemClick }) {
+  // const [selectedItems, setSelectedItems] = useState([]);
 
   return (
     <Item
       title={facetName}
-      subTitle={selectedItems.length}
+      // subTitle={selectedItems.length}
       eventKey={index.toString()}
+      key={`${index}-${facetName}`}
     >
       <ListItem
         onItemClick={onItemClick}
         facet={facets.find((facet) => {
           return facet.name.includes(facetName);
         })}
+        facetName={facetName}
       />
     </Item>
   );
 }
 
-function ListItem({ facet, onItemClick }) {
+function ListItem({ facet, onItemClick, facetName }) {
   return (
     <ul>
       {facet.values.map((facet, index) => (
@@ -72,7 +77,7 @@ function ListItem({ facet, onItemClick }) {
           <Checkbox
             ariaLabel={facet.key}
             className={styles.checkbox}
-            onChange={(checked) => onItemClick(checked, facet.key)}
+            onChange={(checked) => onItemClick(checked, facet.key, facetName)}
           />
           <span>{facet.key}</span>
           <span className={styles.score}>{facet.score}</span>
@@ -84,5 +89,5 @@ function ListItem({ facet, onItemClick }) {
 
 export default function wrap() {
   // @TODO useData to get REAL data
-  return AdvancedFacets(mockedFacets.facets);
+  return AdvancedFacets({});
 }
