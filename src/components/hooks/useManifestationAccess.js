a6bfc0c92d08e736f1f47f575cc9b9509f98ac0f
Yousef <yousefmohsen@outlook.dk>
2024-02-28 12:55:42 +0100

    Deploy 27. feb (#1393)
    * Prettier cql error message (#1355) (#1363)
    
    * Prettier cql error message (#1355)
    
    * Bibdk2021 2237 2 nice to have cql error message skal gores endnu mere laekker (#1376)
    
    * Prettier cql error message
    
    * Cql text area and error
    
    * new field search (#1377)
    
    * Feature insecure cookies fix (#1366)
    
    * dbcdk/login-nextjs new version
    
    * clear cookies
    
    * cypress 134
    
    * fixed cypress test
    
    * node ev
    
    * disable secure on cypress
    
    * secure false if cypress
    
    * clean up
    
    * default pickup agency (#1379)
    
    * default pickup agency
    
    * clean up
    
    * // with protocol + ?scrollToEdition (#1381)
    
    * profile button when FFU (#1382)
    
    * validate input before sending request (#1384)
    
    * validate input before sending request
    
    * validate language before send
    
    * added hostpublication to advanced search (#1383)
    
    * added order hooks (#1388)
    
    * added order hooks
    
    * docs
    
    * fix doc
    
    * fixed skeleton (#1387)
    
    * added missing function
    
    * fix
    
    * fix
    
    * lint fix
    
    * rumænsk (#1392)
    
    ---------
    
    Co-authored-by: akri <91877805+akri-dbcdk@users.noreply.github.com>
    Co-authored-by: pjohans <pjohans@gmail.com>
    Co-authored-by: kfudbc <kfudbc@users.noreply.github.com>
    Co-authored-by: Kasper Fuglsang <kfu@dbc.dk>

---
+++ src/components/hooks/useManifestationAccess.js
@@ -0,0 +1,166 @@
+import { useData } from "@/lib/api/api";
+import { accessForManifestations } from "@/lib/api/access.fragments";
+import { AccessEnum } from "@/lib/enums";
+import { useMemo } from "react";
+import useLoanerInfo from "./user/useLoanerInfo";
+
+/**
+ * Sorting entries of the access array
+ *
+ * Prioritize after how fast and easy it is to get the material,
+ * online materials that are accessible on this site, are favored.
+ */
+function sortAccessArray(accessArr) {
+  accessArr = accessArr.map((access) => {
+    let priority = 0;
+    if (access.__typename === AccessEnum.ACCESS_URL) {
+      priority += 5000;
+    }
+    if (access.__typename === AccessEnum.INFOMEDIA_SERVICE) {
+      priority += 4000;
+    }
+    if (access.__typename === AccessEnum.DIGITAL_ARTICLE_SERVICE) {
+      priority += 3000;
+    }
+    if (access.__typename === AccessEnum.EREOL) {
+      priority += 2000;
+    }
+    if (access.__typename === AccessEnum.INTER_LIBRARY_LOAN) {
+      priority += 1000;
+    }
+
+    if (access.loginRequired) {
+      priority -= 1;
+    }
+
+    if (access.origin === "DBC Webarkiv") {
+      priority += 1;
+    }
+
+    if (access.url) {
+      priority += 1;
+    }
+
+    if (access.origin === "Ereolen Go") {
+      priority += 1;
+    }
+
+    if (access.origin === "Ereolen") {
+      priority += 1;
+    }
+
+    if (access.issn) {
+      priority += 1;
+    }
+
+    if (access.loanIsPossible) {
+      priority += 1;
+    }
+
+    return { ...access, priority };
+  });
+
+  // Sort by priority score
+  accessArr.sort((a, b) => b.priority - a.priority);
+
+  return accessArr;
+}
+
+/**
+ * Find unique access entries for all manifestations
+ * and for every representation of the manifestation (same unit)
+ */
+function flattenAccess(manifestations) {
+  // map of access keys -> access objects
+  const accessMap = {};
+
+  // Loop through all manifestations corresponding to the given pids
+  manifestations?.forEach((m) => {
+    // Loop through all representations of that manifestation
+    // (manifestations in the same unit)
+    m?.unit?.manifestations?.forEach((manifestionInUnit) => {
+      // For this representation we loop through access
+      manifestionInUnit?.access?.forEach((accessEntry) => {
+        if (accessEntry?.loanIsPossible === false) {
+          return;
+        }
+
+        // Create a key for this access entry to identify duplicates
+        const keyArr = JSON.stringify(
+          [
+            accessEntry?.__typename,
+            accessEntry?.url,
+            accessEntry?.id,
+            accessEntry?.issn,
+          ]?.filter((part) => !!part)
+        );
+
+        // If this access is not seen before, we create it
+        if (!accessMap[keyArr]) {
+          accessMap[keyArr] = { ...accessEntry, pids: [] };
+        }
+
+        // Push this pid into the pids array in the access
+        // then we have all the pids available with this access entry
+        accessMap[keyArr].pids.push(manifestionInUnit.pid);
+      });
+    });
+  });
+
+  // Convert to array
+  return Object.values(accessMap);
+}
+/**
+ * Takes a list of manifestation pids, and returns
+ * a list of objects of how they can be accessed
+ */
+export function useManifestationAccess({ pids, filter }) {
+  const { loanerInfo, isLoading: loanerInfoIsLoading } = useLoanerInfo();
+  // Fetch data for the pids needed to generate the access array
+  const { data, isLoading: accessIsLoading } = useData(
+    pids?.length > 0 && accessForManifestations({ pids })
+  );
+
+  // Generate the result only when data changes
+  const res = useMemo(() => {
+    if (accessIsLoading || loanerInfoIsLoading || !data) {
+      return {};
+    }
+
+    const flattenedAccess = flattenAccess(data?.manifestations).filter(
+      (entry) => {
+        if (!filter) {
+          return true;
+        }
+        return filter.includes(entry.__typename);
+      }
+    );
+
+    // sort
+    const access = sortAccessArray(flattenedAccess);
+
+    const accessMap = {};
+    access.forEach((entry) => (accessMap[entry.__typename] = entry));
+
+    let workTypesMap = {};
+    data?.manifestations?.forEach((m) =>
+      m?.workTypes?.forEach((workType) => (workTypesMap[workType] = true))
+    );
+
+    return {
+      access,
+      accessMap,
+    };
+  }, [data, loanerInfo]);
+
+  return {
+    ...res,
+    hasDigitalCopy: !!res?.accessMap?.[AccessEnum.DIGITAL_ARTICLE_SERVICE],
+    hasPhysicalCopy: !!res?.accessMap?.[AccessEnum.INTER_LIBRARY_LOAN],
+    digitalCopyPids:
+      res?.accessMap?.[AccessEnum.DIGITAL_ARTICLE_SERVICE]?.pids || [],
+    physicalCopyPids:
+      res?.accessMap?.[AccessEnum.INTER_LIBRARY_LOAN]?.pids || [],
+    isLoading: loanerInfoIsLoading || accessIsLoading,
+  };
+}
