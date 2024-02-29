793bdf34a406e59cd5e256edaf707aa04a2583dc
pjohans <pjohans@gmail.com>
2024-02-29 07:48:30 +0100

    remove unused var

--- src/components/search/advancedSearch/useFacets.js
+++ src/components/search/advancedSearch/useFacets.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { AdvFacetsTypeEnum } from "@/lib/enums";
 
