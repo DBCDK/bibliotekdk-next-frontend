28124d97ff07438ee256c647e91fff42d9ee4803
pjohans <pjohans@gmail.com>
2024-03-19 14:11:21 +0100

    deleted old test

--- src/components/search/advancedSearch/__tests__/getFacetsFromQuery.test.js
+++
@@ -1,9 +0,0 @@
-import { getFacetsFromQuery } from "@/components/search/advancedSearch/utils";
-
-test("parse query to facets", () => {
-  const query = "fisk";
-  const expected = "phrase.language=finsk";
-  const actual = getFacetsFromQuery(query);
-
-  expect(actual).toEqual(expected);
-});
