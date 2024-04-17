b92bb73b4b96a5be91d161b4245f3b9436cbfd05
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-04-17 09:51:29 +0200

    clean up

--- src/components/search/advancedSearch/combinedSearch/CombinedSearch.js
+++ src/components/search/advancedSearch/combinedSearch/CombinedSearch.js
@@ -145,68 +145,6 @@ export default function CombinedSearch({ queries = [], cancelCombinedSearch }) {
     setQueriesItems(newQueriesItems);
   }, [queries]);
 
-  //   useEffect(() => {
-  //     const currentKeys = queriesItems.map((item) => item.key);
-  //     const newKeys = queries.map((query) => query.key);
-
-  //     // Find items that are in queriesItems but not in the new queries (these are to be removed)
-  //     const keysToRemove = currentKeys.filter(key => !newKeys.includes(key));
-
-  //     // Set these items to be exiting
-  //     if (keysToRemove.length > 0) {
-  //       setExitingItems(prev => {
-  //         const newExiting = { ...prev };
-  //         keysToRemove.forEach(key => newExiting[key] = true);
-  //         return newExiting;
-  //       });
-
-  //       // Set a timeout to remove the items after the animation
-  //       setTimeout(() => {
-  //         setQueriesItems(current =>
-  //           current.filter(item => !keysToRemove.includes(item.key))
-  //         );
-  //         setExitingItems(prev => {
-  //           const newExiting = { ...prev };
-  //           keysToRemove.forEach(key => delete newExiting[key]);
-  //           return newExiting;
-  //         });
-  //       }, 300); // This should match the duration of the CSS transition
-  //     }
-
-  //     // Add or update items based on new queries
-  //     let newQueriesItems = [
-  //       ...queriesItems.filter(item => newKeys.includes(item.key))
-  //     ];
-
-  //     // Handling the addition of new items or updating existing ones
-  //     queries.forEach(query => {
-  //       if (newQueriesItems.length >= MAX_ITEMS) return;  // Prevent adding more than MAX_ITEMS
-  //       const existingItem = newQueriesItems.find(item => item.key === query.key);
-  //       if (existingItem) {
-  //         // Update existing item
-  //         existingItem.cql = query.cql;
-  //         existingItem.fieldSearch = Object.keys(query.fieldSearch).length === 0 ? null : query.fieldSearch;
-  //         existingItem.selectedFacets = query.selectedFacets;
-  //       } else {
-  //         // Add new item if it does not exist
-  //         newQueriesItems.push({
-  //           key: query.key,
-  //           cql: query.cql,
-  //           fieldSearch: Object.keys(query.fieldSearch).length === 0 ? null : query.fieldSearch,
-  //           prefixLogicalOperator: newQueriesItems.length === 0 ? null : "AND",
-  //           selectedFacets: query.selectedFacets,
-  //         });
-  //       }
-  //     });
-
-  //     // Ensure the first element does not have a prefixLogicalOperator
-  //     if (newQueriesItems[0]?.prefixLogicalOperator) {
-  //       newQueriesItems[0].prefixLogicalOperator = null;
-  //     }
-
-  //     setQueriesItems(newQueriesItems);
-  //   }, [queries]);
-
   const router = useRouter();
   const updatePrefixLogicalOperator = ({ index, newOperator }) => {
     let newQueriesItems = [...queriesItems];
@@ -215,7 +153,6 @@ export default function CombinedSearch({ queries = [], cancelCombinedSearch }) {
     setQueriesItems(newQueriesItems);
   };
   const facets = useMemo(() => mergeFacets(queriesItems), [queriesItems]);
-  // ${!isFirstRender ? styles.show : ""}
 
   return (
     <div
@@ -240,7 +177,6 @@ export default function CombinedSearch({ queries = [], cancelCombinedSearch }) {
       >
         {queriesItems.map((item, index) => (
           <SearchItem
-            //   className={exitingItems[item.key] ? styles.exiting : ''}
             key={item.key}
             item={item}
             index={index}
