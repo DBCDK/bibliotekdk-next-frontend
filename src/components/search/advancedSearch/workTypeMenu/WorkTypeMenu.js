2edd35ed89cf8dc5c708cc5a7590930bccc6ee6d
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-04-26 12:53:19 +0200

    rename to worktype and fix arrow icon

--- src/components/search/advancedSearch/workTypeMenu/WorkTypeMenu.js
+++ src/components/search/advancedSearch/workTypeMenu/WorkTypeMenu.js
@@ -1,24 +1,27 @@
 /**
  * @file
- * This file render a menu for selection of material type in advanced search popover.
+ * This file render a menu for selection of work type in advanced search popover.
  */
 
 import React from "react";
 import { useAdvancedSearchContext } from "@/components/search/advancedSearch/advancedSearchContext";
 
 import IconButton from "@/components/base/iconButton/IconButton";
+import Link from "@/components/base/link/Link";
+
 import Text from "@/components/base/text";
 import workTypes from "./workTypes.json";
-import styles from "./MaterialTypeMenu.module.css";
+import styles from "./WorkTypeMenu.module.css";
 import Translate from "@/components/base/translate/Translate";
-export default function MaterialTypeMenu() {
+export default function WorkTypeMenu() {
   const { workType, setWorkType } = useAdvancedSearchContext();
   return (
     <div className={styles.dropdownMenu}>
       {workTypes.map((type) => {
         const isSelected = type === workType;
+        const Tag = isSelected ? IconButton : Link;
         return (
-          <IconButton
+          <Tag
             key={type}
             className={styles.menuItem}
             icon={isSelected ? "arrowrightblue" : null}
@@ -34,7 +37,7 @@ export default function MaterialTypeMenu() {
                 label: type,
               })}
             </Text>
-          </IconButton>
+          </Tag>
         );
       })}
     </div>
