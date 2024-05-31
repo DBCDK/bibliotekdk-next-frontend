c32756b8c56ab16d783a6692686ead467d134f01
Kasper Fuglsang <kfu@dbc.dk>
2024-05-31 14:17:29 +0200

    fix lint

--- src/components/search/advancedSearch/advancedSearchContext.js
+++ src/components/search/advancedSearch/advancedSearchContext.js
@@ -228,7 +228,7 @@ export default function AdvancedSearchProvider({ children, router }) {
     resetDropdownIndices,
     resetMenuItemsEvent,
     dispatchResetMenuItemsEvent,
-    dropdownsToRemove,
+    // dropdownsToRemove,
   } = useDropdownSearchIndices({ ...fieldSearchFromUrl }, workType);
 
   //// ---- parsedCQL ----
