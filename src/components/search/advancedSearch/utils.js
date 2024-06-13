7414511c23eaf0edaf6f5eb7fd70f5a10accfe85
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-06-13 09:46:16 +0200

    AND

--- src/components/search/advancedSearch/utils.js
+++ src/components/search/advancedSearch/utils.js
@@ -42,7 +42,7 @@ function getDropdownQuery(dropdownSearchIndices) {
           if (value === NOTA_ENUM.ONLY_NOTA) {
             return 'term.source = "nota"';
           } else if (value === NOTA_ENUM.NOT_NOTA) {
-            return "term.source=(* not nota)";
+            return "term.source=(* AND nota)";
           } else {
             //if all is selected we do nothing
             return;
