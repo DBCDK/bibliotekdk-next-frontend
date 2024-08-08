fa3265bcb39d822701716b52ab9568478b56bbea
pjohans <pjohans@gmail.com>
2024-08-08 13:29:43 +0200

    highlight changes

--- src/components/search/didYouMean/DidYouMean.js
+++ src/components/search/didYouMean/DidYouMean.js
@@ -1,20 +1,26 @@
-import useQ from "@/components/hooks/useQ";
+/**
+ * @file DidYoumean component
+ * Show an alternative to search if relevant - that is if did you mean service finds a good alternative.
+ */
 
+import useQ from "@/components/hooks/useQ";
 import { useData } from "@/lib/api/api";
 import { didYouMean } from "@/lib/api/search.fragments";
-
 import Section from "@/components/base/section";
 import Link from "@/components/base/link";
 import Text from "@/components/base/text";
+import translate from "@/components/base/translate";
+import styles from "./DidYouMean.module.css";
+
 export function DidYouMean({ didyoumean, isLoading }) {
-  const { setQuery } = useQ();
+  const { q, setQuery } = useQ();
 
   if (isLoading) {
     return null;
   }
   // we show limited results (one for now), and only if the score is high
   // definition of high score
-  const highscore = 0.5;
+  const highscore = 0.9;
   // number of results to show
   const numberToShow = 1;
 
@@ -36,30 +42,61 @@ export function DidYouMean({ didyoumean, isLoading }) {
       space={{
         bottom: "var(--pt4)",
       }}
+      // fake the section title - use <span/> to keep the side section
       title={<span />}
     >
+      <Text skeleton={isLoading} lines={1} tag="span">
+        {translate({ context: "search", label: "didyoumean" })}:
+      </Text>
       {results.map((res, index) => (
-        <Link
-          key={index}
-          onClick={(e) => {
-            e.preventDefault();
-            setQuery({
-              include: { all: res.query },
-              query: {
-                workTypes: null,
-              },
-              method: "push",
-            });
-          }}
-          disabled={isLoading}
-          border={{ bottom: { keepVisible: true } }}
-        >
-          <Text skeleton={isLoading} lines={1} tag="span">
-            {res.query}
-          </Text>
-        </Link>
+        <span className={styles.result} key={index}>
+          <Link
+            onClick={(e) => {
+              e.preventDefault();
+              setQuery({
+                include: { all: res.query },
+                query: {
+                  workTypes: null,
+                },
+                method: "push",
+              });
+            }}
+            disabled={isLoading}
+            border={{ bottom: { keepVisible: true } }}
+          >
+            <Text skeleton={isLoading} lines={1} tag="span">
+              <HiglightChanges q={q} suggestion={res.query} />
+            </Text>
+          </Link>
+        </span>
       ))}
     </Section>
+  );
+}
+
+/**
+ * Higlight words in suggestion that are different from querystring.
+ * @param q
+ * @param suggestion
+ * @returns {*}
+ */
+function HiglightChanges({ q, suggestion }) {
+  const suggestionparts = suggestion?.split(" ");
+  const queryparts = q.all.split(" ");
+  return (
+    <span>
+      {suggestionparts?.map((part, index) => {
+        if (!queryparts.includes(part)) {
+          return (
+            <span key={index} className={styles.highlight}>
+              {`${part} `}
+            </span>
+          );
+        } else {
+          return <span key={index}> {part} </span>;
+        }
+      })}
+    </span>
   );
 }
 
@@ -72,7 +109,6 @@ export default function Wrap({ q }) {
   const limit = 5;
   // use the usedata hook to get data
   const { isLoading, data } = useData(didYouMean({ q: q, limit: limit }));
-  console.log(data, "DIDYOUMEAN DATA");
   return (
     <DidYouMean didyoumean={parseDidYouMean(data)} isLoading={isLoading} />
   );
