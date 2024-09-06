5c97476124bc9438f31aa6fd59acf6788c6cdb46
Kasper Fuglsang <kfu@dbc.dk>
2024-09-06 11:56:10 +0200

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -18,6 +18,7 @@ export default function Document() {
           id="crazyegg"
           type="text/javascript"
           src="/crazyegg.js"
+          strategy="beforeInteractive"
           async="async"
         />
         <Script
