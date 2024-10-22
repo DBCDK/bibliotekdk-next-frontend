253839c12b1070ebe36dade5aad95cbcaa243d93
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-10-22 14:26:05 +0200

    series slider title fix

--- src/components/base/materialcard/templates/templates.js
+++ src/components/base/materialcard/templates/templates.js
@@ -100,8 +100,16 @@ function propFunc(textType, lines) {
 
 /**Used in Slider */
 export function templateForVerticalWorkCard({ material }) {
-  const fullTitle =
-    material?.titles?.tvSeries?.title || material?.titles?.full?.join(": ");
+  const tvSeries = material?.titles?.tvSeries;
+  let tvSeriesTitle = tvSeries?.title;
+
+  if (tvSeries?.season && tvSeries?.season?.display) {
+    tvSeriesTitle += ` (${
+      tvSeries.season.display.charAt(0).toUpperCase() +
+      tvSeries.season.display.slice(1)
+    })`;
+  }
+  const fullTitle = tvSeriesTitle || material?.titles?.full?.join(": ");
   const creators = material?.creators;
   const firstCreator =
     extractCreatorsPrioritiseCorporation(creators)?.[0]?.display;
