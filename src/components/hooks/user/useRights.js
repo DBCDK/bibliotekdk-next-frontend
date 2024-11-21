ecd2ed900bc9a79d19f0061a57cc796391b90992
pjohans <pjohans@gmail.com>
2024-11-21 07:54:32 +0100

    added a comment

--- src/components/hooks/user/useRights.js
+++ src/components/hooks/user/useRights.js
@@ -1,3 +1,7 @@
+/**
+ * @file - userRights
+ * Get user rights only - we use them a lot of places
+ */
 import { useData } from "@/lib/api/api";
 import { userRights } from "@/lib/api/user.fragments";
 
