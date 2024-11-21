cc0da7fae8e7be6c8ec5278b7bd7b4b30c9c9cf6
pjohans <pjohans@gmail.com>
2024-11-21 07:53:33 +0100

    removed a comment

--- src/components/hooks/user/useRights.js
+++ src/components/hooks/user/useRights.js
@@ -2,7 +2,6 @@ import { useData } from "@/lib/api/api";
 import { userRights } from "@/lib/api/user.fragments";
 
 export default function useRights() {
-  // Fetch authentication data stored in fbi-api
   const { data, isLoading } = useData(userRights());
 
   const rights = data?.user?.rights;
