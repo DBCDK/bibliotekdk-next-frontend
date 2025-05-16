6cd6b31fa4668fb9bc1d27f9f138ec6543e63e80
pjohans <pjohans@gmail.com>
2025-05-16 08:37:47 +0200

    some more test

--- cypress/e2e/simpleFacets.cy.js
+++ cypress/e2e/simpleFacets.cy.js
@@ -8,4 +8,22 @@ describe("simpleFacets", () => {
     // assert that numbers (facet-score) are NOT in page .. yet
     cy.get("[data-cy=facet-score]").should("not.exist");
   });
+
+  it("check url", () => {
+    cy.visit("/iframe.html?id=search-facets--simple-facets-in-url");
+    cy.get("[data-cy=accordion-item]").first().click().wait(100);
+
+    // assert that numbers (facet-score) are NOT in page .. yet
+    cy.get("[data-cy=facet-score]").should("not.exist");
+
+    // click an  item and verify url is updated
+    cy.get("[data-cy=accordion-item]").contains("bog").click();
+
+    // parse the query - first accordion is materialtypespecific
+    cy.get("[data-cy=router-query]").then((el) => {
+      const fisk = JSON.parse(el.text());
+      const selected = fisk.materialTypesSpecific;
+      assert(selected.includes("bog"), "query includes bog");
+    });
+  });
 });
