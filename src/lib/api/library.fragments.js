d2c35619b9efd714ba2121ab1ede96c4ebd45658
Kasper Røen Fuglsang <kfu@dbc.dk>
2025-06-26 05:54:33 +0200

    fix library sort

--- src/lib/api/library.fragments.js
+++ src/lib/api/library.fragments.js
@@ -23,7 +23,7 @@ export function search({
     delay: 200, // for debugging
     query: `
     query LibraryFragmentsSearch($q: String, $limit: PaginationLimitScalar, $offset: Int, $language: LanguageCodeEnum, $agencyId: String, $agencyTypes: [AgencyTypeEnum!]) {
-      branches(q: $q, agencyid: $agencyId, language: $language, limit: $limit, offset: $offset, bibdkExcludeBranches:true, statuses:AKTIVE, agencyTypes: $agencyTypes) {
+      branches(q: $q, agencyid: $agencyId, language: $language, limit: $limit, offset: $offset, bibdkExcludeBranches:true, statuses:AKTIVE, agencyTypes: $agencyTypes, sortPickupAllowed: true) {
         hitcount
         agencyUrl
         result {
