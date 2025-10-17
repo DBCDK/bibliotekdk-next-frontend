ba2be9776529f8c1baee21a88dad10b9ae1723e2
Kasper Røen Fuglsang <kfu@dbc.dk>
2025-10-17 13:23:06 +0200

    recent searches is now shown for both simple and advanced and cql

--- src/components/hooks/useAdvancedSearchHistory.js
+++ src/components/hooks/useAdvancedSearchHistory.js
@@ -5,6 +5,14 @@
 import { getSessionStorageItem, setSessionStorageItem } from "@/lib/utils";
 import useSWR from "swr";
 import { getLanguage } from "@/components/base/translate";
+import { useMemo } from "react";
+import isEmpty from "lodash/isEmpty";
+import Translate from "@/components/base/translate";
+import { hasTranslation } from "@/components/base/translate";
+import { useFacets } from "../search/advancedSearch/useFacets";
+import { useQuickFilters } from "../search/advancedSearch/useQuickFilters";
+import { useRouter } from "next/router";
+import { useAdvancedSearchContext } from "../search/advancedSearch/advancedSearchContext";
 
 const KEY = "advanced-search-history";
 
@@ -19,6 +27,66 @@ export function getTimeStamp(now) {
   const stamp = new Date(now).toLocaleTimeString("en-GB", options);
   // remove the " AM/PM" part
   return stamp.replace("AM", "").replace("PM", "").replace(":", ".").trim();
+}
+
+function getSearchValueString(item) {
+  // Generate searchValue string based on content
+  let searchValue = "";
+
+  // Simple search: q.all
+  if (item?.q?.all) {
+    searchValue += item.q.all;
+  }
+  const filters = [];
+  if (item?.filters) {
+    Object.values(item?.filters).forEach((value) => {
+      value?.forEach((v) => {
+        filters.push(v);
+      });
+    });
+  }
+  item?.selectedQuickFilters?.forEach((quickFilter) => {
+    filters.push(quickFilter.value);
+  });
+  item?.selectedFacets?.forEach((facet) => {
+    facet?.values?.forEach((v) => {
+      filters.push(v.name);
+    });
+  });
+  item?.fieldSearch?.inputFields?.forEach((inputField) => {
+    const indexTranslation = hasTranslation({
+      context: "search",
+      label: `advanced-dropdown-${
+        inputField?.label || inputField?.searchIndex
+      }`,
+    })
+      ? Translate({
+          context: "search",
+          label: `advanced-dropdown-${
+            inputField?.label || inputField?.searchIndex
+          }`,
+        })
+      : inputField?.label || inputField?.searchIndex;
+    searchValue += indexTranslation + ": " + inputField.value;
+
+    // searchValue += `${indexTranslation}: "${inputField.value}"`;
+  });
+  if (!item?.fieldSearch?.inputFields?.length && item?.cql) {
+    searchValue += item.cql;
+  }
+  if (filters.length > 0) {
+    searchValue += ` (${Translate({
+      context: "search",
+      label: "history-filter-label",
+    })}: ${filters
+      .map((f) =>
+        hasTranslation({ context: "facets", label: "label-" + f })
+          ? Translate({ context: "facets", label: "label-" + f })
+          : f
+      )
+      .join(", ")})`;
+  }
+  return searchValue;
 }
 
 /**
@@ -49,6 +117,11 @@ export const useAdvancedSearchHistory = () => {
   let { data: storedValue, mutate } = useSWR(KEY, (key) =>
     JSON.parse(getSessionStorageItem(key) || "[]")
   );
+  const router = useRouter();
+
+  const { restartFacetsHook } = useFacets();
+  const { resetQuickFilters } = useQuickFilters();
+  const { changeWorkType } = useAdvancedSearchContext();
 
   const setValue = (value) => {
     try {
@@ -106,7 +179,83 @@ export const useAdvancedSearchHistory = () => {
     }
   };
 
-  return { storedValue, setValue, deleteValue, clearValues };
+  const parsedStoredValue = useMemo(() => {
+    return storedValue?.map((item) => {
+      const isSimple = item.mode === "simpel";
+      const isAdvanced = !isSimple && !isEmpty(item.fieldSearch);
+      const type = Translate({
+        context: "improved-search",
+        label: isSimple ? "simple" : isAdvanced ? "advanced" : "cql",
+      });
+
+      const goToItemUrl = () => {
+        if (item.mode === "simpel") {
+          let qKey = "";
+          let qValue = "";
+          if (item?.q?.subject) {
+            qKey = "q.subject";
+            qValue = item?.q?.subject;
+          } else if (item?.q?.creator) {
+            qKey = "q.creator";
+            qValue = item?.q?.creator;
+          } else {
+            qKey = "q.all";
+            qValue = item?.q?.all;
+          }
+          let filtersStr = "";
+          if (item?.filters) {
+            Object.entries(item?.filters).forEach(([key, value]) => {
+              if (value.length > 0) {
+                filtersStr += `&${key}=${encodeURIComponent(value.join(","))}`;
+              }
+            });
+          }
+
+          router.push(
+            `/find/simpel?${qKey}=${encodeURIComponent(qValue)}${filtersStr}`
+          );
+          return;
+        }
+        // restart the useFacets hook - this is a 'new' search
+        restartFacetsHook();
+        resetQuickFilters();
+
+        // set worktype from item
+        changeWorkType(item.fieldSearch?.workType || "all");
+        if (!isEmpty(item.fieldSearch)) {
+          const query = {
+            fieldSearch: JSON.stringify(item.fieldSearch),
+            facets: JSON.stringify(item.selectedFacets || "[]"),
+            quickfilters: JSON.stringify(item.selectedQuickFilters || "[]"),
+          };
+          router.push({
+            pathname: "/avanceret/",
+            query: query,
+          });
+        } else if (item.cql) {
+          router.push({
+            pathname: "/avanceret/",
+            query: {
+              cql: item.cql,
+              facets: JSON.stringify(item.selectedFacets || "[]"),
+              quickfilters: JSON.stringify(item.quickfilters || "[]"),
+            },
+          });
+        }
+      };
+
+      return {
+        ...item,
+        goToItemUrl,
+        translations: {
+          type,
+          searchValue: getSearchValueString(item),
+        },
+      };
+    });
+  }, [storedValue]);
+
+  return { storedValue: parsedStoredValue, setValue, deleteValue, clearValues };
 };
 
 export default useAdvancedSearchHistory;
