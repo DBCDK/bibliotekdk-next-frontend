f1929bef5f2511aa1ccceb45a35f2f88a0f0b49d
Yousef Mohsen <yousefmohsen@outlook.dk>
2025-11-03 17:20:13 +0100

    jenkins error

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reporter: "junit",
   reporterOptions: {
-    mochaFile: "app/e2e/reports/test-result-[hash].xml",
+    mochaFile: "/app/cypress/reports/test-result-[hash].xml",
     toConsole: true,
   },
   env: {
