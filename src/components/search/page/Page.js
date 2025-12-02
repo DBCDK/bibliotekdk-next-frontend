b2c2d3b72a3148bcf4987e967f19f14480af1b3e
Yousef Mohsen <yousefmohsen@outlook.dk>
2025-12-02 07:38:20 +0100

    series and creator boxes in search

--- src/components/search/page/Page.js
+++ src/components/search/page/Page.js
@@ -24,13 +24,16 @@ import {
 } from "@/components/search/advancedSearch/utils";
 
 import * as searchFragments from "@/lib/api/search.fragments";
-import { hitcount as advancedHitcount } from "@/lib/api/complexSearch.fragments";
+import {
+  hitcount as advancedHitcount,
+  extraHits as complexExtraHits,
+} from "@/lib/api/complexSearch.fragments";
 
 import AdvancedSearchSort from "@/components/search/advancedSearch/advancedSearchSort/AdvancedSearchSort";
 import TopBar from "@/components/search/advancedSearch/topBar/TopBar";
 import AdvancedFacets from "@/components/search/facets/advanced/advancedFacets";
 import QuickFilter from "@/components/search/advancedSearch/quickfilter/QuickFilter";
-import FacetTags from "@/components/search/facets/advanced/facetTags";
+import { FacetTags } from "@/components/search/facets/advanced/facetTags/facetTags";
 import { FacetButton } from "@/components/search/facets/advanced/facetButton/facetButton";
 import SimpleFacets from "@/components/search/facets/simple";
 
@@ -41,10 +44,13 @@ import translate from "@/components/base/translate";
 import isEmpty from "lodash/isEmpty";
 import styles from "./Page.module.css";
 import { useRouter } from "next/router";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import SaveSearchBtn from "../save";
 import Related from "../related/Related";
 import DidYouMean from "../didYouMean/DidYouMean";
-
+import SeriesBox from "@/components/search/seriesBox/SeriesBox";
+import CreatorBox from "@/components/search/creatorBox/CreatorBox";
 // -------------------------------
 // UI-komponent: kun rendering
 // -------------------------------
@@ -60,6 +66,9 @@ function Page({
   hasQuery,
   rawcql,
   advancedCql,
+  selectedFacets,
+  creatorHit,
+  seriesHit,
 }) {
   const breakpoint = useBreakpoint();
   const isMobile = ["xs", "sm", "md"].includes(breakpoint);
@@ -79,6 +88,8 @@ function Page({
 
   const shouldShowHistory = !isLoading && !hasActiveSearch;
   const shouldShowNoHits = !isLoading && hasActiveSearch && hitcount === 0;
+  const shouldShowCreator = Boolean(creatorHit);
+  const shouldShowSeries = !shouldShowCreator && Boolean(seriesHit);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -94,6 +105,20 @@ function Page({
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const searchHitComponent = shouldShowCreator ? (
+    <CreatorBox
+      creatorHit={creatorHit}
+      className="search-block"
+      data-cy="search-block"
+    />
+  ) : shouldShowSeries ? (
+    <SeriesBox
+      seriesHit={seriesHit}
+      className="search-block"
+      data-cy="search-block"
+    />
+  ) : null;
+
   return (
     <>
       <TopBar
@@ -108,8 +133,8 @@ function Page({
       <Section
         divider={false}
         colSize={{
-          lg: { offset: 0, span: true },
-          titel: { lg: { offset: 3, span: true } },
+          lg: 10,
+          titel: 2,
         }}
         id="search-result-section"
         className={styles.section}
@@ -151,12 +176,8 @@ function Page({
         subtitle={
           hasActiveSearch &&
           !isMobile && (
-            <div
-              className={`${styles.facetsContainer} ${
-                isSimple ? styles.simple : ""
-              }`}
-            >
-              <FacetTags className={styles.facetTags} />
+            <div className={styles.facetsContainer}>
+              <FacetTags selectedFacets={selectedFacets} />
               <div className={styles.subtitleStyle}>
                 <Text type="text1">
                   {translate({ context: "search", label: "narrow-search" })}
@@ -169,40 +190,62 @@ function Page({
           )
         }
       >
-        {shouldShowHistory && <History />}
-        {shouldShowNoHits && <NoHitSearch isSimpleSearch={isSimple} />}
+        <Row>
+          <Col xs={12} lg={9}>
+            {shouldShowHistory && <History />}
+            {shouldShowNoHits && <NoHitSearch isSimpleSearch={isSimple} />}
 
-        {!isSimple && !isMobile && hitcount > 0 && (
-          <div className={styles.advancedSearchActions}>
-            <AdvancedSearchSort className={styles.sort_container} />
-            <SaveSearchBtn />
-          </div>
-        )}
+            {!isSimple && !isMobile && hitcount > 0 && (
+              <div className={styles.advancedSearchActions}>
+                <AdvancedSearchSort className={styles.sort_container} />
+                <SaveSearchBtn />
+              </div>
+            )}
 
-        {isSimple && !isLoading && hitcount > 0 && (
-          <div className={styles.actions}>
-            {isSimple && <FilterButton className={styles.filterButton} />}
+            {isSimple && !isLoading && hitcount > 0 && (
+              <div className={styles.actions}>
+                {isSimple && <FilterButton className={styles.filterButton} />}
 
-            <div className={styles.supplementary}>
-              <Related />
-              <DidYouMean />
+                <div className={styles.supplementary}>
+                  <Related />
+                  <DidYouMean />
+                </div>
+
+                {isSimple && <SaveSearchBtn />}
+              </div>
+            )}
+          </Col>
+
+          {!isMobile && <Col lg={3} />}
+        </Row>
+
+        <Row className={styles.resultGrid}>
+          {isMobile && searchHitComponent && (
+            <Col xs={12} className={styles.resultsSidebarMobile}>
+              {searchHitComponent}
+            </Col>
+          )}
+
+          <Col xs={12} lg={9} className={styles.resultsMain}>
+            <div className={styles.resultsList}>
+              {Array(isMobile ? currentPage : 1)
+                .fill({})
+                .map((_, index) => (
+                  <Result
+                    key={`result-${index}`}
+                    page={isMobile ? index + 1 : currentPage}
+                    onWorkClick={onWorkClick}
+                  />
+                ))}
             </div>
+          </Col>
 
-            {isSimple && <SaveSearchBtn />}
-          </div>
-        )}
-
-        <div>
-          {Array(isMobile ? currentPage : 1)
-            .fill({})
-            .map((_, index) => (
-              <Result
-                key={`result-${index}`}
-                page={isMobile ? index + 1 : currentPage}
-                onWorkClick={onWorkClick}
-              />
-            ))}
-        </div>
+          {!isMobile && searchHitComponent && (
+            <Col xs={12} lg={3} className={styles.resultsSidebar}>
+              {searchHitComponent}
+            </Col>
+          )}
+        </Row>
       </Section>
       {hitcount > 0 && (
         <Pagination
@@ -226,6 +269,8 @@ Page.propTypes = {
   rawcql: PropTypes.string,
   advancedCql: PropTypes.string,
   selectedFacets: PropTypes.array,
+  creatorHit: PropTypes.object,
+  seriesHit: PropTypes.object,
 };
 
 // -------------------------------
@@ -283,12 +328,30 @@ export default function Wrap({ page = 1, onPageChange, onWorkClick }) {
       advancedHitcount({ cql: advancedCql })
   );
 
+  const shouldFetchExtraHits = isSimple
+    ? hasQuery
+    : hasAdvancedSearch || hasCqlSearch;
+  const extraHitsRes = useData(
+    shouldFetchExtraHits
+      ? isSimple
+        ? searchFragments.extraHits({ q, filters })
+        : complexExtraHits({ cql: advancedCql })
+      : null
+  );
   const hitcount = isAdvanced
     ? advancedRes?.data?.complexSearch?.hitcount || 0
     : simpleRes?.data?.search?.hitcount || 0;
 
   const isLoading = isAdvanced ? advancedRes.isLoading : simpleRes.isLoading;
 
+  const seriesHit = isAdvanced
+    ? extraHitsRes?.data?.complexSearch?.seriesHit
+    : extraHitsRes?.data?.search?.seriesHit || null;
+
+  const creatorHit = isAdvanced
+    ? extraHitsRes?.data?.complexSearch?.creatorHit
+    : extraHitsRes?.data?.search?.creatorHit || null;
+  //  const creatorHit = null;
   // Store the current search history item in the local storage
   useEffect(() => {
     if (currentSearchHistoryItem) {
@@ -310,6 +373,8 @@ export default function Wrap({ page = 1, onPageChange, onWorkClick }) {
       rawcql={rawcql}
       advancedCql={advancedCql}
       selectedFacets={selectedFacets || filters}
+      creatorHit={creatorHit}
+      seriesHit={seriesHit}
     />
   );
 }
