d7db3b7c74a29ba190ab099af8a3c554cadb0bf9
kfudbc <kfudbc@users.noreply.github.com>
2025-12-05 13:14:16 +0100

    Forfatter fixes (#1805)
    * fixed link styling when link is disabled
    
    * fix overview, awards and stuff

--- src/components/base/markdown/AiMarkdown.js
+++ src/components/base/markdown/AiMarkdown.js
@@ -113,7 +113,7 @@ export default function AiMarkdown({
 
           a(props) {
             if (disableLinks) {
-              return <Text type="text2" tag="strong" {...props} />;
+              return <Text type="text2" tag="span" {...props} />;
             }
             return (
               <Link
@@ -167,7 +167,7 @@ export default function AiMarkdown({
         ref={contentRef}
         className={`${styles.content} ${!expanded ? styles.collapsed : ""} ${
           !fadeIn ? styles.noFade : ""
-        }`}
+        } ${!canToggle ? styles.noToggle : ""}`}
         style={{ maxHeight: inlineMaxHeight }}
         onTransitionEnd={(e) => {
           if (e.target !== contentRef.current) return;
