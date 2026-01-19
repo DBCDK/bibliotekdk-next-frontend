ccc3e3fd7e240b9b5a4a96ddda1a7b51d277fbdb
Kasper Røen Fuglsang <kfu@dbc.dk>
2026-01-19 09:15:36 +0100

    prettier

--- src/components/search/advancedSearch/advancedSearchHistory/AdvancedSearchHistory.js
+++ src/components/search/advancedSearch/advancedSearchHistory/AdvancedSearchHistory.js
@@ -299,7 +299,10 @@ export function HistoryHeaderActions({
   return (
     <div className={cx(styles.actionheader)}>
       {selectedCount > 0 && (
-        <Text type="text3" className={styles.selectedCount}>{`${selectedCount} ${Translate({
+        <Text
+          type="text3"
+          className={styles.selectedCount}
+        >{`${selectedCount} ${Translate({
           context: "form",
           label: "icon-label-selected",
         })}`}</Text>
