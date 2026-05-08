5d6e5d2a60e4b8c6583ab9e28d327df7bb20bfaa
mtni <36037828+mtni@users.noreply.github.com>
2026-05-08 21:18:56 +0200

    small UI fixes (#1870)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 import getConfig from "next/config";
+import { normalizeSite } from "@/components/hooks/useSiteConfig";
 
 const COOKIEBOT = getConfig()?.publicRuntimeConfig?.cookiebot;
+const SITE = normalizeSite(getConfig()?.publicRuntimeConfig?.app?.site);
 
 // Cookiebot script added to _document.js def template as recommended by the docs
 // https://nextjs.org/docs/api-reference/next/script#strategy
 
 export default function Document() {
   return (
-    <Html>
+    <Html data-site={SITE}>
       <Head />
       <body>
         <Main />
