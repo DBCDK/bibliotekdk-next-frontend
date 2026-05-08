4b51ebcd46d158255ddaef0a215793ea3ded529e
Morten Thorst Nielsen <mtni@dbc.dk>
2026-05-08 23:24:35 +0200

    bug fix again

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const config = {
     timeout: process.env.API_TIMEOUT_MS || 150,
     cacheKey: process.env.NEXT_BACKEND_CACHE_KEY || "alfa",
   },
-  site: process.env.NEXT_PUBLIC_SITE || "bibliotekDk",
+  site: process.env.NEXT_PUBLIC_SITE || "bibliotekdk",
 };
 
 export default config;
