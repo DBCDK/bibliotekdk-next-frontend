a25e41204cd118db592ac4a4c379b2af534948e8
Kasper Røen Fuglsang <kfu@dbc.dk>
2026-06-17 10:46:22 +0200

    fix tests

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,13 +14,28 @@ import Router from "next/router";
 import { createRouter as createStorybookRouter } from "@storybook/nextjs/router.mock";
 import { RouterContext } from "next/dist/shared/lib/router-context.shared-runtime";
 import { SessionProvider } from "next-auth/react";
-import { SWRConfig } from "swr";
+import { useMemo } from "react";
+import { SWRConfig, useSWRConfig } from "swr";
 import { createMemoryRouter, useMemoryRouter } from "./nextMemoryRouter";
 import AdvancedSearchProvider from "@/components/search/advancedSearch/advancedSearchContext";
 import { UseManyProvider } from "@/components/hooks/useMany";
 
 const memoryRouter = createMemoryRouter();
 Router.router = memoryRouter;
+
+function StorybookSWRReset({ storyId, children }) {
+  const { cache } = useSWRConfig();
+
+  useMemo(() => {
+    for (const key of cache.keys()) {
+      if (typeof key === "string" && key.startsWith('{"apiUrl"')) {
+        cache.delete(key);
+      }
+    }
+  }, [cache, storyId]);
+
+  return children;
+}
 
 export const decorators = [
   (Story, context) => {
@@ -51,14 +66,16 @@ export const decorators = [
         beforeFetch={context?.parameters?.graphql?.urlbeforeFetch}
         debug={context?.parameters?.graphql?.debug}
       >
-        <SWRConfig value={{ provider: () => new Map(), dedupingInterval: 0 }}>
+        <SWRConfig value={{ dedupingInterval: 0 }}>
           <RouterContext.Provider value={memoryRouter}>
             <AdvancedSearchProvider router={memoryRouter}>
               <SessionProvider session={session}>
                 <ModalContextProvider router={memoryRouter}>
                   {showInfo && <StoryRouter router={memoryRouter} />}
-                  <Story />
                   <UseManyProvider />
+                  <StorybookSWRReset storyId={context.id}>
+                    <Story />
+                  </StorybookSWRReset>
                 </ModalContextProvider>
               </SessionProvider>
             </AdvancedSearchProvider>
