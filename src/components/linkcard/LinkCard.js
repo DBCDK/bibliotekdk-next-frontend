258ec1ab0f65e40f9cfd1984c2eed396f681c933
Yousef Mohsen <yousefmohsen@outlook.dk>
2026-09-08 11:42:27 +0200

    linkcard as link

--- src/components/linkcard/LinkCard.js
+++ src/components/linkcard/LinkCard.js
@@ -52,7 +52,7 @@ export default function LinkCard({
             <Link className={styles.title}>{title}</Link>
           </Title>
           <Link a={false} target={target}>
-            <Button type="secondary" size="medium" className={styles.button}>
+            <Button type="secondary" size="medium" className={styles.button} asLink>
               {buttonText}
             </Button>
           </Link>
